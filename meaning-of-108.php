da3161d364b83a63f0d0d2e782c03996235900f9
Pankaj Jain <capankajjain@smilyo.com>
2015-04-11 23:04:18 +0530

    Create meaning-of-108.php

---
+++ meaning-of-108.php
@@ -0,0 +1,85 @@
+<?php include('header.php'); ?>
+
+<!-- start body -->
+<body onunload="" >
+
+	<!-- start dotted pattern -->
+	<div class="bg-overlay"></div>
+	<!-- end dotted pattern -->
+		
+	<!--start menu wrapper -->
+	<div class="menu-wrapper clearfix">
+		<!-- start logo -->
+		<div class="logo">
+			
+		</div>
+		<!-- end logo -->
+		
+		<!-- start navigation -->
+		<div class="main-nav">
+			<ul class="menu">
+				<?php include('menu.php'); ?>
+			</ul>		
+		</div>
+		<!-- end navigation -->
+	</div>
+	<!-- end menu wrapper -->
+	
+	 <!-- start content wrapper -->	
+	
+	<div class="content page-content">
+	
+		<div class="page-title">
+			<h1>Meaning of "108"</h1>
+		</div>
+		
+		<div class="divider clear"></div>
+		
+		<div class="inner-content">	
+			108 represents the 108 qualities in Jain Upadhyay, Munishris and Sadhus.<br><br>
+			<b>There are 108 qualities of praiseworthy souls as elaborated below:</b>
+			<ul><li>Arihant..........12 Qualities</li>
+			<li>Siddh............08 Qualities</li>
+			<li>Acharya........36 Qualities</li>
+			<li>Upadhyay......25 Qualities</li>
+			<li>Sadhu...........27 Qualities</li></ul>
+			<ul><li><b>12 Qualities of Arihant are further sub divided as</b></li>
+			<ul><li>4 Atishay</li>
+			<ol><li>Gyan Atishay</li>
+			<li>Vachn Atishay</li>
+			<li>Puja Atishay</li>
+			<li>Apayapagm Atishay</li></ol>
+			<li>8 Prtiharya</li>
+			<ol><li>Singhasan</li>
+			<li>Bhamandal</li>
+			<li>Chhatra</li>
+			<li>Ashok Vriksh</li>
+			<li>Deva-dundubhi</li>
+			<li>Divya-dhwani</li>
+			<li>Pushp-vrushti</li>
+			<li>Chamar</li></ol></ul>
+			<table border="2">
+			<tr><th>8 Qualities of Siddh</th></tr>
+			<tr><th>S. No.</th><th>Quality (Gun)</th><th>Meaning</th><th>Karm/Dosh</th></tr>		
+			  <tr><td colspan="4">4 by destorying Ghati-Karm</td></tr>
+			    <tr><td>1.</td><td>Anant Gyan</td><td>Infinite Knowledge</td></tr>
+			    <tr><td>2.</td><td>Anant Darshan</td><td>Infinite Perception</td></tr>
+			    <tr><td>3.</td><td>Anant Charitra</td><td>Perfect Conduct</td></tr>
+			    <tr><td>4.</td><td>Anant Virya</td><td>Infinite Energy</td></tr>
+			  <tr><td colspan="4">4 by destroying Aghati-Karm</td></tr>
+			    <tr><td>1.</td><td>Avyabadha-sukh</td><td>Eternal Happiness</td></tr>
+			    <tr><td>2.</td><td>Akshay-stithi</td><td>Immortality</td></tr>
+			    <tr><td>3.</td><td>Arupitv</td><td>Formlessness</td></tr>
+			    <tr><td>4.</td><td>Aguru-Laghotv</td><td>Equality among all Siddh</td></tr>
+			</table>
+			</ul>
+		</div>
+		<div class="sidebar">
+		<img src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" style="width:200px;margin-right:5px">
+		</div>
+		<!-- end widgets -->
+	</div>
+	
+	<!--  end content wrapper  -->
+ 	
+ 	<?php include('footer.php'); ?>
