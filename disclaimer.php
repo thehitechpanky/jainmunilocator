6fecabbbe28f918bfdcd4da21312a199aa5c9ccb
Pankaj Jain <capankajjain@smilyo.com>
2015-04-11 23:08:18 +0530

    Create disclaimer.php

---
+++ disclaimer.php
@@ -0,0 +1,46 @@
+<?php include('header.php'); ?>
+
+<!-- start body -->
+<body onunload="" >
+
+	<!-- start dotted pattern -->
+	<div class="bg-overlay"></div>
+	<!-- end dotted pattern -->
+		
+	<!--start menu wrapper -->
+	<div class="menu-wrapper clearfix">
+		<!-- start logo -->
+		<div class="logo">
+			
+		</div>
+		<!-- end logo -->
+		
+		<!-- start navigation -->
+		<div class="main-nav">
+			<ul class="menu">
+				<?php include('menu.php'); ?>
+			</ul>		
+		</div>
+		<!-- end navigation -->
+	</div>
+	<!-- end menu wrapper -->
+	
+	 <!-- start content wrapper -->	
+<div class="container">
+
+		<div class="page-title">
+			<h1>Disclaimer</h1>
+		</div>
+		
+		<div class="divider clear"></div>
+		
+		<div>
+			<p>The information, materials, products & services included on this website may include inaccuracies or certain errors. Changes are periodically added to the information herein. Jainmunilocator.org may make improvements and/or changes in the information displayed herein at any time. Information received via this website should not be relied upon for personal, legal or financial decisions & you should consult an appropriate person or concern for specific advice as per your situation. This site is presented on an "as is" basis. Jainmunilocator.org makes no representations or warranties regarding the accuracy, reliability or completeness of this site's  content. To the extent permissible by law, Jainmunilocator.org disclaims all warranties, express or implied, including but not limited to, warranties of title, or implied warranties of quality, performances & fitness for a particular purpose. Further, Jainmunilocator.org does not warrant that this web site or the server that makes it available are  free of computer 'viruses'. Jainmunilocator.org does not represent that the use or the results of the use of the service or the materials made available as part of the service will be correct, complete, accurate, current or otherwise reliable.</p>
+<p>Jainmunilocator.org shall not be held liable for any kind of damages, including without limitation, special or consequential damages arising out of your access to, or inability to access this website, or your use of, or reliance upon this site or the content hereof. Further, you agree to indemnify Jainmunilocator.org against any liability and use of the site. Jainmunilocator.org has no duty to update this site or the contents hereof and hence shall not be held liable for any failure to update such information.</p>
+<p>Jainmunilocator.org makes no representation that this site's content is appropriate or available for use in other locations and access to it from territories where the content is illegal and is prohibited. Those who choose to access this site do so on their own initiative & are therefore held responsible for compliance with any applicable local laws. </p>
+		</div>
+		
+	</div>
+	<!--  end content wrapper  -->
+ 	
+ 	<?php include('footer.php'); ?>
