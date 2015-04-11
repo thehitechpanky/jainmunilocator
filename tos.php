fdb42619da88651ea2df6e6f780c9e0c7d8bd2da
Pankaj Jain <capankajjain@smilyo.com>
2015-04-11 23:08:50 +0530

    Create tos.php

---
+++ tos.php
@@ -0,0 +1,44 @@
+<?php include('header.php'); ?>
+
+<!-- start body -->
+<body onunload="" >
+
+	<!-- start dotted pattern -->
+	<div class="bg-overlay"></div>
+	<!-- end dotted pattern -->
+		
+	<!--start menu wrapper -->
+	<div class="menu-wrapper clearfix">
+		<!-- start logo -->
+		<div class="logo">
+			
+		</div>
+		<!-- end logo -->
+		
+		<!-- start navigation -->
+		<div class="main-nav">
+			<ul class="menu">
+				<?php include('menu.php'); ?>
+			</ul>		
+		</div>
+		<!-- end navigation -->
+	</div>
+	<!-- end menu wrapper -->
+	
+	 <!-- start content wrapper -->	
+<div class="container">
+
+		<div class="page-title">
+			<h1>Terms and Conditions</h1>
+		</div>
+		
+		<div class="divider clear"></div>
+		
+		<div>
+			<p>TOS</p>
+		</div>
+		
+	</div>
+	<!--  end content wrapper  -->
+ 	
+ 	<?php include('footer.php'); ?>
