410f3c9e83ff1c8f6db83bb0352d44451b891747
Pankaj Jain <capankajjain@smilyo.com>
2015-04-11 23:00:20 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include('header.php'); ?>
+<!-- start body -->
+<body class="page-template-template-homepage-php" onunload="" >
+
+	<!-- start dotted pattern -->
+		<div class="bg-overlay"></div>
+	<!-- end dotted pattern -->
+		
+	<!--start menu wrapper -->
+	<div class="menu-wrapper clearfix">
+		<!-- start logo -->
+		<div class="logo">
+			
+		</div>
+		<!-- end logo -->
+		
+		<!-- start navigation -->
+		<div class="main-nav">
+			<ul >
+				<?php include('menu.php'); ?>
+			</ul>		
+		</div>
+		<!-- end navigation -->
+	</div>
+	<!--end menu wrapper -->
+ 	<?php include('footer.php'); ?>
