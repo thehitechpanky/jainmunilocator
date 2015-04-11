26dea01a71dcce8b7b221f5edc27eb1010462099
Pankaj Jain <capankajjain@smilyo.com>
2015-04-11 23:15:24 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,62 @@
+	<!-- start footer -->	
+	<div class="space"></div>
+	<div class="sub-footer clearfix">
+		<div id="progress-back" class="load-item">
+			<div id="progress-bar"></div>
+		</div>	
+		<div class="copyright">
+			&copy; 2014 Developed by <a  target='_blank' href='http://dhruvjain.org'>Dhruv Jain</a>	</div>
+		
+		<!-- start footer-menu -->
+		<div class="footer-menu">
+			<ul class="footer_menu">
+				<li>
+					<a href="tos.php">Terms and Conditions</a>
+				</li>
+				<li>
+					<a href="disclaimer.php">Disclaimer</a>
+				</li>
+				
+				<li>
+					<a href="#">Blog</a>
+				</li>
+			</ul>		
+		</div>
+		<!-- end footer-menu -->
+				
+	</div>
+	<!-- end footer -->
+	
+	<div id="slidecaption"></div>
+			
+	<!-- srart footer social -->
+	<div class="social">
+	
+		<a title="Facebook" target="_blank" href="http://www.facebook.com/jainmunilocator">
+			<img width="22" height="22" title="" alt="" src="images/social/facebook.png" />
+		</a>
+		
+		<a title="Follow me" target="_blank" href="#">
+			<img width="22" height="22" title="" alt="" src="images/social/twitter.png" />
+		</a>
+		
+		<a title="Google Plus" target="_blank" href="#">
+			<img width="22" height="22" title="" alt="" src="images/social/google.png" />
+		</a>
+		
+		<a title="Flickr" target="_blank" href="#">
+			<img width="22" height="22" title="" alt="" src="images/social/flickr.png" />
+		</a>
+		<a title="Youtube" target="_blank" href="#">
+			<img width="22" height="22" title="" alt="" src="images/social/youtube.png" />
+		</a>	
+				
+		<a title="Linkedin" target="_blank" href="#">
+			<img width="22" height="22" title="" alt="" src="images/social/linkedin.png" />
+		</a>
+		
+	</div>
+	<!-- end footer social -->
+	
+</body>
+</html>
