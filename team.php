9c82338a41fba12862c14aaf76fdceffc19ec049
Pankaj Jain <capankajjain@smilyo.com>
2015-04-11 23:05:04 +0530

    Create team.php

---
+++ team.php
@@ -0,0 +1,55 @@
+<?php include('header.php'); ?>
+
+<!-- start body -->
+<body onunload="" >
+
+	<!-- start dotted pattern -->
+	<div class="bg-overlay"></div>
+	<!-- end dotted pattern -->
+		
+	<!--start menu wrapper -->
+	<div class="menu-wrapper clearfix">
+		<!-- start logo -->
+		<div class="logo">
+			
+		</div>
+		<!-- end logo -->
+		
+		<!-- start navigation -->
+		<div class="main-nav">
+			<ul class="menu">
+				<?php include('menu.php'); ?>
+			</ul>		
+		</div>
+		<!-- end navigation -->
+	</div>
+	<!-- end menu wrapper -->
+	
+	 <!-- start content wrapper -->	
+	
+	<div class="content page-content">
+	
+		<div class="page-title">
+			<h1>Our Team</h1>
+		</div>
+		
+		<div class="divider clear"></div>
+		
+		<div class="inner-content">
+		<div style="font-size:150%;font-weight:900">	
+			Initiative by - Pankaj Jain<br /><br />
+			Website Designer - Dhruv<br /><br />
+			Facebook Page Admin - Vacant
+		</div>
+		<br /><br /><br />
+		*For Vacant Positions, use the contact form to contact us
+		</div>
+		<div class="sidebar">
+		<img src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" style="width:200px;margin-right:5px">
+		</div>
+		<!-- end widgets -->
+	</div>
+	
+	<!--  end content wrapper  -->
+ 	
+ 	<?php include('footer.php'); ?>
