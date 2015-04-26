7809f466cd9521520ca1fc376ea96e646278187c
Pankaj Jain <capankajjain@smilyo.com>
2015-04-26 10:31:14 +0530

    Update header.php

--- header.php
+++ header.php
@@ -40,7 +40,6 @@
 	<script type='text/javascript' src='js/jquery.prettyPhoto.js'></script>
 	<script type='text/javascript' src='js/jquery.scroll.min.js'></script>
 	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDcnIUTozeOU26CWZRSxQRRoTFeZtvzX6Y"></script>
-	
 	<script type="text/javascript" src='js/googlemaps.min.js'></script>
 	<script type="text/javascript">google.maps.event.addDomListener(window, 'load', initialize);
 	</script>
@@ -50,10 +49,7 @@
 		$r = $db->query('SELECT * FROM quotes ORDER BY RAND() LIMIT 1');
 		$row = $r->fetch(PDO::FETCH_ASSOC);
 	?>
-	<!-- background slides -->	
-	<!--old image url http://enjoyingindia.com/wp-content/uploads/2011/10/Jain-Temple-Delhi.jpg-->
-	<!--old image url Mahavir Ji White http://upload.wikimedia.org/wikipedia/commons/f/f7/Mahavirji_Temple.JPG-->
-	<!--old image url Lal Mandir Ji White http://upload.wikimedia.org/wikipedia/commons/6/6d/Digambar_Jain_Lal_Mandir,_Chandni_Chowk,_Delhi.jpg-->
+	<!-- background slides -->
 	<div id="customcon"><div id="map-canvas" style="z-index:0;"></div></div>
 	<!-- <script type = "text/javascript">
 		jQuery(function ($) {
