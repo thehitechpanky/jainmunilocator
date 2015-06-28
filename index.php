918183988a5848bb1b6d308fcee4072a1dc7d7d9
Pankaj Jain <capankajjain@smilyo.com>
2015-06-28 19:03:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,8 @@
 		<!-- end navigation -->
 	</div>
 	<!--end menu wrapper -->
+	
 	<div id="map" style="width: 100%; height: 570px;"></div>
-<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
-  
+	<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
+	
  	<?php include('footer.php'); ?>
