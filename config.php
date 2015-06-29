83cdb1fd89f6c31e948296830891a6c79a137533
Pankaj Jain <capankajjain@smilyo.com>
2015-06-29 18:07:11 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 //Database Connection
-$db = new PDO('mysql:host=localhost;dbname=database_name;charset=utf8', 'username', 'password', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$link = mysqli_connect("localhost","username","password","database_name"); 
+$db = new PDO('mysql:host=localhost;dbname=databasename;charset=utf8', 'username', 'password', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$link = mysqli_connect("localhost","username","password","databasename"); 
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -52,18 +52,4 @@ function getmuni($id)
 	}
 }
 
-//Code below represents code for markers
-$array = array();
-$sql = "SELECT * FROM muni_location,munishri WHERE mid=id AND lat<>0 ";
-$result=mysqli_query($link,$sql);
-$i=0;
-while($row = mysqli_fetch_assoc($result)){
-if(isset($row)){
-$array[$i][0]='<a href="/munis.php?id='.$row['id'].'">'.getmuni($row['id']).'<img src="'.$row['img'].'" />'.'</a>';
-$array[$i][1]=$row['lat'];
-$array[$i][2]=$row['lng'];
-$i++;
-}
-}
-//$json = json_encode($array);
 ?>
