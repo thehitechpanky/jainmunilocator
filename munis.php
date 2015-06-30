0fc68a8e39a71f83bce5f52dadf6d0ab33be14be
Pankaj Jain <capankajjain@smilyo.com>
2015-07-01 01:11:29 +0530

    Database changes leading to code updation

--- munis.php
+++ munis.php
@@ -31,7 +31,7 @@
 	if(isset($_GET['id']))
 	{
 		$id = (int)$_GET['id'];
-		$t = $db->prepare("SELECT * FROM munishri, upadhis WHERE id = ? AND approved=1 AND uid=upadhi");
+		$t = $db->prepare("SELECT * FROM munishri, upadhis, kshullak, acharya, ailak WHERE id = ? AND approved=1 AND uid=upadhi AND kid=id AND id=acharyaid AND id=ailakid");
 		$t->execute(array($id));
 		if($t->rowCount() == 1)
 		{
@@ -172,18 +172,21 @@ geozcoder.geocode( { 'address': $("#x").val()}, function(results, status) {
 					<tr><td>Chaturmas</td><td><?php echo $getinfo['chaturmas'] ?></td></tr>
 					<tr><td><?php if($getinfo['dos']!="0000-00-00") echo "Date Of Samadhi" ?></td><td><?php if($getinfo['dos']!="0000-00-00") echo $getinfo['dos'] ?></td></tr>
 					<tr><th colspan="2" align="left"><?php if($getinfo['upadhi']=="1") echo "Acharya Pad Details" ?></th></tr>
-					<tr><td><?php if($getinfo['upadhi']=="1") echo "Date" ?></td><td><?php if($getinfo['upadhi']=="1") echo $getinfo['acharyapaddate'] ?></td></tr>
-					<tr><td><?php if($getinfo['upadhi']=="1") echo "Guru" ?></td><td><a href ="munis.php?id=<?php echo $getinfo['acharyapadguru'] ?>"><?php if($getinfo['upadhi']=="1") echo getmuni($getinfo['acharyapadguru']) ?></a></td></tr>
-					<tr><td><?php if($getinfo['upadhi']=="1") echo "Place" ?></td><td><?php if($getinfo['upadhi']=="1") echo getlocation($getinfo['acharyapadplace']);
-					if(getlocation($getinfo['curretplace']) != 'N/A')
-					{
-					  ?> <a href="locate.php?location=<?php echo getlocation($getinfo['acharyapadplace']) ?>"><?php if($getinfo['upadhi']=="1") echo'<img src="/images/icons/map-marker.png" alt="map-marker" height="20" width="20")>'; ?></a><?php } ?></td></tr>
+					<tr><td><?php if($getinfo['upadhi']=="1") echo "Date" ?></td><td><?php if($getinfo['upadhi']=="1") echo $getinfo['adate'] ?></td></tr>
+					<tr><td><?php if($getinfo['upadhi']=="1") echo "Guru" ?></td><td><a href ="munis.php?id=<?php echo $getinfo['aguru'] ?>"><?php if($getinfo['upadhi']=="1") echo getmuni($getinfo['aguru']) ?></a></td></tr>
 					<tr><th colspan="2" align="left">Muni Diksha Details</th></tr>
 					<tr><td>Date</td><td><?php echo $getinfo['munidikshadate'] ?></td></tr>
 					<tr><td>Guru</td><td><a href ="munis.php?id=<?php echo $getinfo['munidikshaguru'] ?>"><?php echo getmuni($getinfo['munidikshaguru']) ?></a></td></tr>
 					<tr><td>Place</td><td><?php echo getlocation($getinfo['munidikshasthal']); if(getlocation($getinfo['curretplace']) != 'N/A')
 					{
 					 ?> <a href="locate.php?location=<?php echo getlocation($getinfo['munidiskhasthal']) ?>"><img src="/images/icons/map-marker.png" alt="map-marker" height="20" width="20"></a><?php } ?></td></tr>
+					<tr><th colspan="2" align="left"><?php if($getinfo['ailakguru']>0) echo "Ailak Pad Details" ?></th></tr>
+					<tr><td><?php if($getinfo['ailakguru']>0) echo "Date" ?></td><td><?php if($getinfo['ailakguru']>0) echo $getinfo['ailakdate'] ?></td></tr>
+					<tr><td><?php if($getinfo['ailakguru']>0) echo "Guru" ?></td><td><a href ="munis.php?id=<?php echo $getinfo['ailakguru'] ?>"><?php if($getinfo['ailakguru']>0) echo getmuni($getinfo['ailakguru']) ?></a></td></tr>
+					<tr><th colspan="2" align="left"><?php if($getinfo['kguru']>0) echo "Kshullak Pad Details" ?></th></tr>
+					<tr><td><?php if($getinfo['kguru']>0) echo "Name" ?></td><td><?php if($getinfo['kguru']>0) echo $getinfo['kname'] ?></td></tr>
+					<tr><td><?php if($getinfo['kguru']>0) echo "Date" ?></td><td><?php if($getinfo['kguru']>0) echo $getinfo['kdate'] ?></td></tr>
+					<tr><td><?php if($getinfo['kguru']>0) echo "Guru" ?></td><td><a href ="munis.php?id=<?php echo $getinfo['kguru'] ?>"><?php if($getinfo['kguru']>0) echo getmuni($getinfo['kguru']) ?></a></td></tr>
 					<tr><th colspan="2" align="left">History</th><th></th></tr>
 					<tr><td>Birthname</td><td><?php echo $getinfo['birthname'] ?></td></tr>
 					<tr><td>Date Of Birth</td><td><?php echo $getinfo['dob'] ?></td></tr>
