521d245961302f5792046879d6b0b7153fba5af9
Pankaj Jain <capankajjain@smilyo.com>
2015-07-02 00:22:12 +0530

    Update config.php

--- config.php
+++ config.php
@@ -43,7 +43,6 @@ function getmuni($id)
 		if($n['prefix'] != "") $t = $t.' '.$n['prefix'];
 		if($n['name'] != "") $t = $t.' '.$n['name'];
 		if($n['suffix'] != "") $t = $t.' '.$n['suffix'];
-		$t = ltrim($t,",");
 		return $t;
 	}
 	else
@@ -52,4 +51,26 @@ function getmuni($id)
 	}
 }
 
+//Function to get the guru details
+function getguru($id)
+{
+	global $db;
+	$m = $db->prepare("SELECT * FROM munishri, kshullak, ailak, upadhyay, ailacharya, acharya WHERE id = ? AND approved=1 AND id=kid AND id=ailakid AND id=upadhyayid AND id=ailacharyaid AND id=acharyaid");
+	$m->execute(array($id));
+	if($m->rowCount() == 1)
+	{
+		$n = $m->fetch(PDO::FETCH_ASSOC);
+		if($n['upadhi'] == 1) $t = $n['aguru'];
+		if($n['upadhi'] == 2) $t = $n['ailacharyaguru'];
+		if($n['upadhi'] == 3) $t = $n['upadhyayguru'];
+		if($n['upadhi'] == 4) $t = $n['munidikshaguru'];
+		if($n['upadhi'] == 5) $t = $n['ailakguru'];
+		if($n['upadhi'] == 6) $t = $n['kguru'];
+		return $t;
+	}
+	else
+	{
+		return "N/A";
+	}
+}
 ?>
