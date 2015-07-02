be098389cbcce0b94c9afea68ca743a497d0920c
Pankaj Jain <capankajjain@smilyo.com>
2015-07-02 18:36:32 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,13 +1,7 @@
-<li>
-					<a href="./">Home</a>
-						<!-- start sub-menu -->
-						
-						<!-- end sub-menu -->
-				</li>
-				<li><a href="./about.php">About</a></li>
-				<li><a href="./munis.php">Munis</a></li>
-				<li><a href="./meaning-of-108.php">Meaning of 108</a></li>
-				<li><a href="./team.php">Team</a></li>
-				<li><a href="./contact.php">Contact</a></li>
-				<li><a href="./donate.php">Donate</a></li>
-				<li><a href="./add.php">Add Information</a></li>
+<li><a href="./">Home</a></li>
+<li><a href="./about.php">About</a></li>
+<li><a href="./munis.php">Munis</a></li>
+<li><a href="./meaning-of-108.php">Meaning of 108</a></li>
+<li><a href="./contact.php">Contact</a></li>
+<li><a href="./donate.php">Donate</a></li>
+<li><a href="./add.php">Add Information</a></li>
