abaff09fb57ced9c2654f7a56af2bbe77b96569e
Pankaj Jain <capankajjain@smilyo.com>
2015-07-03 18:49:01 +0530

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -42,19 +42,20 @@
 				<tr><td>&#8226;</td><td><a href="about.php">About</a></td><td>&#8226;</td><td><a href="references.php">References</a></td></tr>
 				<tr><td>&#8226;</td><td><a href="munis.php">Munis</a></td><td>&#8226;</td><td><a href="sitemap.php">Sitemap</a></td></tr>
 				<tr><td>&#8226;</td><td><a href="meaning-of-108.php">Meaning of 108</a></td><td>&#8226;</td><td><a href="tos.php">Terms and Conditions</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="contact.php">Contact</a></td><td>&#8226;</td><td><a href="disclaimer.php">Disclaimer</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="donate.php">Donate</a></td><td></td></tr>
-				<tr><td>&#8226;</td><td><a href="add.php">Add Information</a></td><td></td></tr>
+				<tr><td>&#8226;</td><td><a href="mahagun.php">Mahagun</a></td><td>&#8226;</td><td><a href="disclaimer.php">Disclaimer</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="contact.php">Contact</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="donate.php">Donate</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="add.php">Add Information</a></td></tr>
 			</table>
 			<p> </p>
 			<table style="width:100%">
-				<tr align="left"><th colspan="2">Guru Links</th><th></th><th></th></tr>
+				<tr align="left"><th colspan="2">Guru Links</th></tr>
 					<?php
 					$sql = 'SELECT approved, id FROM munishri where approved=1';
 					$result = $link->query($sql);
 					if ($result->num_rows > 0) {
     				while($row = $result->fetch_assoc())
-					{echo '<tr><td>&#8226;&nbsp;</td><td>'.getmuni($row["id"]).'</td><td><a href="munis.php?id='.$row["id"].'">Profile</a></td><td><a href="shishyawali.php?id='.$row["id"].'">Shishyawali</a></td></tr>';}
+					{echo '<tr><td>&#8226;&nbsp;</td><td><a href="munis.php?id='.$row["id"].'">'.getmuni($row["id"]).'</a></td></tr>';}
 					}
 					$link->close();
 					?>
