78c8567341d750ee961714e0fc262dec7e33993a
Pankaj Jain <capankajjain@smilyo.com>
2015-07-03 18:48:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -55,7 +55,7 @@ function getmuni($id)
 function getguru($id)
 {
 	global $db;
-	$m = $db->prepare("SELECT * FROM munishri, kshullak, ailak, upadhyay, ailacharya, acharya WHERE id = ? AND approved=1 AND id=kid AND id=ailakid AND id=upadhyayid AND id=ailacharyaid AND id=acharyaid");
+	$m = $db->prepare("SELECT * FROM munishri, kshullak, ailak, muni, upadhyay, ailacharya, acharya WHERE id = ? AND approved=1 AND id=kid AND id=ailakid AND id=muniid AND id=upadhyayid AND id=ailacharyaid AND id=acharyaid");
 	$m->execute(array($id));
 	if($m->rowCount() == 1)
 	{
@@ -63,7 +63,7 @@ function getguru($id)
 		if($n['upadhi'] == 1) $t = $n['aguru'];
 		if($n['upadhi'] == 2) $t = $n['ailacharyaguru'];
 		if($n['upadhi'] == 3) $t = $n['upadhyayguru'];
-		if($n['upadhi'] == 4) $t = $n['munidikshaguru'];
+		if($n['upadhi'] == 4) $t = $n['muniguru'];
 		if($n['upadhi'] == 5) $t = $n['ailakguru'];
 		if($n['upadhi'] == 6) $t = $n['kguru'];
 		return $t;
