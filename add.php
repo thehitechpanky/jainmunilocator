e0e7f1a5eb5f85ec3fd54b5d35729aae4bbd63c3
Pankaj Jain <capankajjain@smilyo.com>
2015-07-06 02:01:22 +0530

    Update add.php

--- add.php
+++ add.php
@@ -1,23 +1,17 @@
 <?php include('header.php'); ?>
-<script src='https://www.google.com/recaptcha/api.js'></script>
+
 <!-- start body -->
 <body onunload="" >
 
 	<!-- start dotted pattern -->
 	<div class="bg-overlay"></div>
 	<!-- end dotted pattern -->
-		
-	    
-	     <!-- start navigation -->
-		
-				<?php include('menu.php'); ?>
-			
-	    <!-- end navigation -->
-		
 	
-		
+	<!-- start navigation -->
+	<?php include('menu.php'); ?>
+	<!-- end navigation -->
 	
-	 <!-- start content wrapper -->	
+	<!-- start content wrapper -->	
 	
 	<div class="content page-content">
 	
@@ -41,8 +35,8 @@
 			$text = file_get_contents($url);
 		if(strpos($text,'true'))
 		{
-			$t = $db->prepare('INSERT INTO munishri (upadhi,name,website,img,munidikshadate,birthname,dob,father,mother) VALUES (?,?,?,?,?,?,?,?,?)');
-			if(!$t->execute(array($upadhi,$name,$website,$photo,$mdate,$birthname,$dob,$father,$mother)))
+			$t = $db->prepare('INSERT INTO munishri (upadhi,name,website,img,birthname,dob,father,mother) VALUES (?,?,?,?,?,?,?,?)');
+			if(!$t->execute(array($upadhi,$name,$website,$photo,$birthname,$dob,$father,$mother)))
 			{
 				echo "<div style='color:red'>Some error encountered.Please Contact the website administrator<br /></div>";
 			}
@@ -61,18 +55,10 @@
 			<fieldset><center>
 				<form method="post" action="">
 					<div>
-						<select name="upadhi">
-							<option value="1">Acharya</option>
-							<option value="2">Upadhyaya</option>
-							<option value="3">Muni</option>
-							<option value="4">Ailak</option>
-							<option value="5">Kshullak</option>
-							<option value="6">Aryika</option>
-						</select><br />
+						<input type="text" value="" placeholder="Upadhi" name="upadhi"><br />
 						<input type="text" value="" placeholder="Name" name="name"><br />
 						<input type="text" value="" placeholder="Website" name="website"><br />
 						<input type="text" value="" placeholder="Photo Link" name="photo"><br />
-						<input type="text" value="" placeholder="Muni Diksha Date in YYYY-MM-DD" name="munidikshadate"><br />
 						<input type="text" value="" placeholder="Birth Name" name="birthname"><br />
 						<input type="text" value="" placeholder="Date of Birth in YYYY-MM-DD" name="dob"><br />
 						<input type="text" value="" placeholder="Father" name="father"><br />
@@ -92,4 +78,4 @@
 	
 	<!--  end content wrapper  -->
  	
- 	<?php include('footer.php'); ?>
+<?php include('footer.php'); ?>
