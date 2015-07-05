7d989a3ca8e493a3e98bf655d7c84e56cf8bd1d8
Pankaj Jain <capankajjain@smilyo.com>
2015-07-05 21:30:03 +0530

    Update header.php

--- header.php
+++ header.php
@@ -6,10 +6,10 @@
 
 	<!-- start title -->
 	<title>Jain Muni Locator</title>
-	
+
 	<!-- start meta -->
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
- 
+
 	<!-- styles -->
  	<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="css/default.css" type="text/css" media="screen" />
@@ -19,23 +19,30 @@
 	<link rel="stylesheet" href="css/supersized.min.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="css/light-style.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="css/googlemaps.min.css" type="text/css" media="screen" />
-	<link rel="stylesheet" type="text/css" media="all" href="css/styles.css">
-	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-	<script type="text/javascript" src="js/slider-menu.js"></script>
+	<link rel="stylesheet" href="css/style.min.css" type="text/css" media="all" >
 	
 	<!--[if lte IE 9]>
 	<link rel='stylesheet' href='css/ie9.css' type='text/css' media='all' />
 	<![endif]-->
-	
+
 	<!-- end styles -->
 	
-	<?php include('config.php'); ?>
+	<!-- start script -->
+	<script type='text/javascript' src='js/slider-menu.min.js'></script>
+	<script src='js/jquery-1.11.1.min.js'></script>
+	<!-- end script -->
+
+	
+<!-- Load Configuration File -->
+<?php include('config.php'); ?>
 	<?php
 		$r = $db->query('SELECT * FROM quotes ORDER BY RAND() LIMIT 1');
 		$row = $r->fetch(PDO::FETCH_ASSOC);
 	?>
+	
 	<!-- background slides -->
 	<div id="customcon"><div id="map-canvas" style="z-index:0;"></div></div>
+	
 	<!-- <script type = "text/javascript">
 		jQuery(function ($) {
 			jQuery.supersized({
@@ -48,7 +55,5 @@
 	</script > -->	
 	
 	<!-- end background slides -->
-<!-- metadata starts -->
-<meta name="description" content="Find out location of a the Digambar Jain Munishri and Sadhugan here. Know about their vows, daily routine, lineage and everything else you would want to know about them.">
 </head>
 <!-- end head -->
