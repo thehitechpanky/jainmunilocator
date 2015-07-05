48d564373a585b7ee106416c6a60b9036a19b1d8
Pankaj Jain <capankajjain@smilyo.com>
2015-07-06 02:02:12 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,4 @@
 <?php include('header.php'); ?>
-<script src='https://www.google.com/recaptcha/api.js'></script>
 
 <!-- start body -->
 <body onunload="" >
@@ -7,26 +6,12 @@
 	<!-- start dotted pattern -->
 	<div class="bg-overlay"></div>
 	<!-- end dotted pattern -->
-		
-	<!--start menu wrapper -->
-	<div class="menu-wrapper clearfix">
-		<!-- start logo -->
-		<div class="logo">
-			
-		</div>
-		<!-- end logo -->
-		
-		<!-- start navigation -->
-		<div class="main-nav">
-			<ul class="menu">
-				<?php include('menu.php'); ?>
-			</ul>		
-		</div>
-		<!-- end navigation -->
-	</div>
-	<!-- end menu wrapper -->
 	
-	 <!-- start content wrapper -->	
+	<!-- start navigation -->
+	<?php include('menu.php'); ?>
+	<!-- end navigation -->
+	
+	<!-- start content wrapper -->
 	
 	<div class="content page-content">
 	
@@ -112,4 +97,4 @@
 	
 	<!--  end content wrapper  -->
  	
- 	<?php include('footer.php'); ?>
+<?php include('footer.php'); ?>
