cc29211f588278a42202279f8ce92cecc050d1f5
Pankaj Jain <capankajjain@smilyo.com>
2015-07-05 19:28:28 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,23 +1,21 @@
-<div id="pgcontainer">
-  <div id="sliderheader">
+<div id="pgcontainer"><!-- @start #pgcontainer -->
+	<div id="sliderheader">
     <div id="navbar">
       <a href="#" class="menubtn">menu</a>
       <!-- use captain icon for toggle menu -->
       <div id="menu">
-        <ul>
-		            <li><a href="./">Home</a></li>
-					<li><a href="./about.php">About</a></li>
-					<li><a href="./munis.php">Munis</a></li>
-					<li><a href="./meaning-of-108.php">Meaning of 108</a></li>
-					<li><a href="./team.php">Team</a></li>
-					<li><a href="./contact.php">Contact</a></li>
-					<li><a href="./donate.php">Donate</a></li>
-					<li><a href="./add.php">Add Information</a></li>
-        </ul>
+        <ul>		            
+			<li><a href="./">Home</a></li>
+			<li><a href="./about.php">About</a></li>
+			<li><a href="./munis.php">Munis</a></li>
+			<li><a href="./meaning-of-108.php">Meaning of 108</a></li>
+			<li><a href="./mahagun.php">Mahagun</a></li>
+			<li><a href="./contact.php">Contact</a></li>
+			<li><a href="./donate.php">Donate</a></li>
+			<li><a href="./add.php">Add Information</a></li>
+		</ul>
       </div>
     </div>
     <div class="overlay"></div>
   </div>
-  
-  
 </div><!-- @end #pgcontainer -->
