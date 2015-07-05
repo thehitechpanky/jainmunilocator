4ca754cbbe72ddf06bb041a9dea48e31a3167ab4
Pankaj Jain <capankajjain@smilyo.com>
2015-07-05 19:43:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,25 +3,11 @@
 <body class="page-template-template-homepage-php" onunload="" >
 
 	<!-- start dotted pattern -->
-		<div class="bg-overlay"></div>
+	<div class="bg-overlay"></div>
 	<!-- end dotted pattern -->
-		
-	<!--start menu wrapper -->
-	<div class="menu-wrapper clearfix">
-		<!-- start logo -->
-		<div class="logo">
-			
-		</div>
-		<!-- end logo -->
-		
-		<!-- start navigation -->
-		<div class="main-nav">
-			<ul >
-				<?php include('menu.php'); ?>
-			</ul>		
-		</div>
-		<!-- end navigation -->
-	</div>
-	<!--end menu wrapper -->
+
+	<!-- start navigation -->
+	<?php include('menu.php'); ?>
+	<!-- end navigation -->	
 	
- 	<?php include('footer.php'); ?>
+<?php include('footer.php'); ?>
