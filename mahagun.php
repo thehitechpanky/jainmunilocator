e264b6b426ed94d37d26a29c51eb221f19745278
Pankaj Jain <capankajjain@smilyo.com>
2015-07-06 02:18:29 +0530

    Create mahagun.php

---
+++ mahagun.php
@@ -0,0 +1,128 @@
+<?php include('header.php'); ?>
+
+<!-- start body -->
+<body onunload="" >
+
+	<!-- start dotted pattern -->
+	<div class="bg-overlay"></div>
+	<!-- end dotted pattern -->
+	
+	<!-- start navigation -->
+	<?php include('menu.php'); ?>
+	<!-- end navigation -->
+	
+	<!-- start content wrapper -->
+	
+	<div class="content page-content">
+	
+		<div class="page-title">
+			<h1>Mahagun</h1>
+		</div>
+		
+		<div class="divider clear"></div>
+		
+		<div class="inner-content">	
+			<p>Digambar Jain Sadhus take a vow to observe twenty eight special virtues at the time of their induction into the saintly order and remain vigilant to observe these without slightest deviation through out their life. These twenty eight special virtues have been divided into five Mahavrat, five Samities (Carefulness) five indriya vijay (Victory over senses), Six Avashyak (essential duties) Seven Vishesh Gun (specialties).</p>
+			<p> </p>
+			<p><b>FIVE MAHAVART</b></p>
+			<ol>
+				<li>AHINSA MAHAVRAT-    Not to cause slightest mental or bodily pain to any microscopic or macro living creature that is a cellular or higher life including plants, insects, human being, animals, birds etc. with their thoughts, speech and body. To have sympathy on all living ones taking them to be equal to his own soul.</li>
+				<li>SATYA MAHAVRAT-  Not to speak words which are false, harsh, hurting and against the interest of self and others. To speak sweet, helpful for self and others according to religious sanction.</li>
+				<li>ACHOURYA MAHAVRAT-  Neither to take or help others to take or praise others taking the physical belongings of others without having their clear permission by mind speech and body.</li>
+				<li>BRAHAMCHARYA MAHAVRAT -  Absolutely free from any type of sexual act with any woman, of the world in the three ways that is thoughts, words, physical acts and remain completely engrossed in his own soul.</li>
+				<li>APRIGRAH MAHAVRAT-TO completely leave internal parigrah that is wrath, pride, wickedness, greed etc and external parigrah that is each and every type of attachment of physical articles including money etc.</li>
+			</ol>
+			<p><b>FIVE SAMITIES</b></p>
+			<ol>
+				<li>ERIA SAMITI- To walk cautiously seeing the foreground so that no living creature however small it may be killed or hurt.</li>
+				<li>BHASHA SAMITI- To speak sweet simple, soft and helpful only in essential words according to relegious scriptures.</li>
+				<li>AISHNA SAMITI- Without causing any sort of trouble to others cat simple, pure, hygienic, perfectly vegetarian food only once in day time in twenty four hours to tread the path of religion.</li>
+				<li>AADAN NIKSHAPAN SAMITI- Carefully lie down or sit down or remove and put down things keeping defense of any creatures in view.</li>
+				<li>PRATISTHAPAN SAMITI- To remain vigilant while making excretions (passing out urine, excreta etc). That no insect is killed or troubled.</li>
+			</ol>
+			<p><b>FIVE INDRIYA VIJAY</b></p>
+			<ol>
+				<li>SPARSHAN INORIYA VIJAY- To discard sense of joy and sorrow created or felt by touch of the body.</li>
+				<li>RASNA INDRIA VIJAY- Not to have liking or disliking for things having different taste like sweet, Sour, salty, spicy  etc.</li>
+				<li>GHRAN INDRYA VIJAY- Not to have desire for fragrance or dislike for foul odour.</li>
+				<li>CHAKSHU INDRIA VIJAY- To leave completely any sense of Joy or sorrow by seeing beautiful or ugly things or scenes.</li>
+				<li>KARAN INDRIA VIJAY- Not to feel pleasure or irritation due to melodious sounds, discordant notes, praise and abusive Language etc.</li>
+			</ol>
+			<p><b>SIX ESSENTIALS</b></p>
+			<ol>
+				<li>SAMTA- Without having an emotion good or bad to keep equanimity, that is medium state of thoughts  in all either friend or enemy, praise or insult, comfort or discomfort, gain or loss birth or death, respect or insult etc.</li>
+				<li>VANDANA- To worship and sing in praises of God or one venerable saint.</li>
+				<li>STUTI-  To worship and praise collectively for a number of saints or Mahapurshas (Super human being) gods.</li>
+				<li>PRATI KRAMAN-To make efforts and repent over his own past mistakes or acts which are not proper or forbidden.</li>
+				<li>PRATYAKHYAN- To make efforts to save himself from future sins and mistakes.</li>
+				<li>KAYAUTSARG-To leave the love and care of his own body to perform acts of hardship like Tapasya, fast, expose to cold heat and rains etc calmly.</li>
+			</ol>
+			<p><b>SEVEN VISHESH GUN (Specialities)</b></p>
+			<ol>
+				<li>ACHAILAKTAV (NAKEDNESS)-To discard all cloths and coverings to attain simple, natural, emotionless  etc. child like state.</li>
+				<li>ASNAN-Not to bath his body with water, only to have the bath of mantra (spiritual words) vow. air and sun rays etc.</li>
+				<li>DANT DHOVAN-Not to shine his teeth with brush or any powder etc.</li>
+				<li>KESH LONCH-TO pull out hairs of head; board and pair of moustaches with hands in two, three or four months once.</li>
+				<li>BHOOMI SHAYAN-TO sleep for little time on clean that is having no ants or insects etc. earth, stone sleep, mat or dry grass etc. without any aid like pillow. cushion, any cloth or bad etc. for removing fatigue.</li>
+				<li>AK BHUKTI-They eat once In day time only in twenty four hours simple and hygienic food without any of the thirty two antrai (disturbances) mentioned as below-If they find hair, leather (dead skin) dead insect or any other creature, meat bone, Nail etc. in their food while eating then they at once leave the food without any ill feeling and go away. If they happen to saw raw meat, dying creature, wine, raging fire etc, then they take it as antrai and leave food. While they are going to have their food and they happen to see corpse, dead animals having all the five senses excreta of some bird falls on their body, if some carnivorous animal, some drunkard or meat eater man, chandal (executioner), or a Lady having menses touches their body then they return back without taking their food on that day At the time of taking their food if they happen to hear pathetic words or sound of loudly weeping or fighting then they assume it as anrai. Moreover if person offering food to them falls or some utensil falls, either lamp or fire gets extinguished, raw seed (having power of germination) gets into their stomach. appearance of ascaris or pin or hook worms etc. then they take it also as antrai.</li>
+				<li>STITH I BHOJAN- Saints do not eat their food sitting in any utensil but instead they stand keeping a distance of about ten centimeters between their feet, without having evil thought, observing perfect silence, without making any indication of yes or no, without having any complex of begging or humbleness they accept their food solid or liquid in their palms only and eat, They always eat less than their hunger.</li>
+			</ol>
+			<p><b>THIRTY FOUR EXTRA VIRTUES (Uttar Gun)</b></p>
+			<p>These thirty four extra virtues are classified into six Bahirang Tap (outward). Six antarang Tap (internal), twenty two parishay jai. Thus these total thirty four extra virtues of Muni.</p>
+			<ul>
+				<li><b>SIX BAHIRANG TAP</b></li>
+				<ol>
+					<li>ANSHAN- To purify the soul, to weaken the pride and evil thoughts and to control the senses voluntarily stop taking completely all type of food and liquid for a certain period.</li>
+					<li>AVMODRYA- TO win laziness, do meditation and study, eat food less than their hunger.</li>
+					<li>VRATI PARISANKHYAN-  TO think about some condition silently and without telling anybody that it it will happen today then I will accept food otherwise not for example-special type of house or. some definite number of human beings or food or any type of event or thing.</li>
+					<li>RAS TYAG-  To have control on liking or thoughts of eating particular food of a particular taste and to limit sensory pride taking a vow not to eat one or two or more type of food belonging to following classes-sweet, salt, oils, Ghee milk and curd. These types are known as six ras.</li>
+					<li>AIKANT SHAYYASAN-  For undisturbed studies, meditation thinking and analysis etc. to live in lonely places where there is no man woman or eunuch.</li>
+					<li>KAYA KLAISH- TO realize self to sit in padamasan (lotus posture) or not to speak for a particular interval of time (Silence) or live near rivers or in mountains and thus bear the bodily discomforts keeping one self normal.</li>
+				</ol>
+				<li><b>SIX ANTARANG TAP</b></li>
+				<ol>
+					<li>PRAYASHCHIT- TO repent on wrong thoughts and doings.</li>
+					<li>VINAY-  To behave respectfully towards virtues and virtuous.</li>
+					<li>VAYA VRAT-  To serve saints without any desire.</li>
+					<li>SWADHYAI- TO purify the soul. to remove the darkness of ignorance to check the tendency of iII will to self and others and to create the tendency of doing good to others study of true religious literature.</li>
+					<li>VUTSARG- TO finish attachment with body and other worldly things.</li>
+					<li>DHYAN-  To stabilize the thoughts in the study of inner self and analysis of six substances that is-Jiva, Pudgal [matter] Dharam [medium of motion of soul and matter] Adharam [medium of rest for soul and matter], Kai [time] and Akash [space]. After getting rid of of all ill instincts.</li>
+				</ol>
+				<li><b>TWENTY TWO PARISHA JAI (Victories of pinches)</b></li>
+				<ol>
+					<li>SHUDHA JAI- To bear hunger keeping normal thoughts.</li>
+					<li>PIPASA JAI-To bear thirst without uneasiness.</li>
+					<li>SHEET JAI-To bear cold with equanimity.</li>
+					<li>USHAN JAI- To tolerate heat with equanimity.</li>
+					<li>DANSH MASAK JAi-To keep normal if mosquito etc. bite.</li>
+					<li>NAGAN PARISH JAI-Not to have only pinch of evil thoughts or shame by being completely naked.</li>
+					<li>ARATI JAI-Even in the face of dissatisfaction not to have dissatisfaction or aversion from the religion.</li>
+					<li>STRI PARISHY JAI-Not to allow turbulence of' thoughts even by seeing young beautiful ladies.</li>
+					<li>CHARYA PARISHY JAI-To be remor seless while walking on uneven. thorny Stanley ground.</li>
+					<li>NISHIDHA PARISHY JAI- To bear the pain calmly which is caused by sitting or standing for long intervals during meditation or tapasya.</li>
+					<li>SHAYYA PARISHY JAI-To bear pain calmly caused by sleeping on hard ground.</li>
+					<li>AAKROSH PARISHY JAI-To tolerate calmly unparliamantry, fifthly and perching words use by others.</li>
+					<li>BADH PARISHY JAI- To remain calm and quit at the time of murderer attack with sword or other arms etc. by others.</li>
+					<li>YACHNA PARISHY JAI-Not to beg food or medicine even at the death bed.</li>
+					<li>ALABH PARISHY JAI-In condition of not getting food for long intervals to consider this loss better than gain.</li>
+					<li>ROG PARISHY JAI-To remain normal and firm while suffering with dreaded and painful disease.</li>
+					<li>TREN SPARSH JAI-Remain call and normal in his thoughts if he gets pricked with thorn etc.</li>
+					<li>MAL PARISHY JAI-To bear the inconvenience quietly if their body gets dusty etc.</li>
+					<li>SATKAR PURSKAR PARISHY JAI-Not to feel inferiority complex and let his thoughts be disturbed if others do not praise him though he is very learned austere and meditative.</li>
+					<li>PARGGYA PARISHY JAI- Not to be proud of his intellect.</li>
+					<li>AGAYAN PARISHY JAI- Not to be remorseful if he does not acquire much knowledge inspite of his best efforts.</li>
+					<li>ADARSHAN PARISHY JAI-Even after tough and prolonged Tapasya (austerity) if he does not attain special knowledge and super natural powers then also he does not loose his faith in the path of Nirvana.</li>
+				</ol>
+			</ul>
+			<p>In addition to above mentioned there are so many other attainment or virtues in Nirgranth Muni.</p>
+		</div>
+		
+		<div class="sidebar">
+		<img src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" style="width:200px;margin-right:5px">
+		</div>
+		<!-- end widgets -->
+	</div>
+	
+	<!--  end content wrapper  -->
+ 	
+<?php include('footer.php'); ?>
