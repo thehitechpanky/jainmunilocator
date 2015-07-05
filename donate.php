859704dab37fdf9146790de3209c30a95bfdced4
Pankaj Jain <capankajjain@smilyo.com>
2015-07-06 02:11:25 +0530

    Update donate.php

--- donate.php
+++ donate.php
@@ -25,12 +25,13 @@
 			<p>Jain Muni Locator puts the interests of Jain Community before profit. If this website is useful to you, take one minute to support the non-profit behind it. If every Jain reading this donates Rs. 1000/-, Our fundraiser would be over within an hour. Thank you.</p>
 			<p> </p>
 			<table>
-				<tr align="left"><th colspan="2">Bank Details</th></tr>
+				<tr align="left"><th colspan="2">BANK DETAILS</th></tr>
 				<tr><td>Bank Name : </td><td>________</td></tr>
 				<tr><td>A/c No. : </td><td>_______________</td></tr>
 				<tr><td>IFS Code : </td><td>___________</td></tr>
-				<tr><td colspan="2">After depositing the donation amount in bank account, please make sure you notify us using the contact form.</td></tr>
 			</table>
+			<p> </p>
+			<p><b>Note: After depositing the donation amount in bank account, please make sure you notify us using the contact form.</b></p>
 		</div>
 
 		<div class="sidebar"><img src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" style="width:200px;margin-right:5px"></div>
