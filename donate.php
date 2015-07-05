a931b490fb015d4c362c0bd4d40952e69ae41f9d
Pankaj Jain <capankajjain@smilyo.com>
2015-07-06 02:08:28 +0530

    Update donate.php

--- donate.php
+++ donate.php
@@ -6,53 +6,37 @@
 	<!-- start dotted pattern -->
 	<div class="bg-overlay"></div>
 	<!-- end dotted pattern -->
-		
-	<!--start menu wrapper -->
-	<div class="menu-wrapper clearfix">
-		<!-- start logo -->
-		<div class="logo">
-			
-		</div>
-		<!-- end logo -->
-		
-		<!-- start navigation -->
-		<div class="main-nav">
-			<ul class="menu">
-				<?php include('menu.php'); ?>
-			</ul>		
-		</div>
-		<!-- end navigation -->
-	</div>
-	<!-- end menu wrapper -->
 	
-	 <!-- start content wrapper -->	
+	<!-- start navigation -->
+	<?php include('menu.php'); ?>
+	<!-- end navigation -->
+	
+	<!-- start content wrapper -->
 	
 	<div class="content page-content">
-	
+
 		<div class="page-title">
 			<h1>Donate</h1>
 		</div>
-		
+
 		<div class="divider clear"></div>
-		
+
 		<div class="inner-content">
-		Jain Muni Locator puts the interests of Jain Community before profit. If this website is useful to you, take one minute to support the non-profit behind it. If every Jain reading this donates Rs. 1000/-, Our fundraiser would be over within an hour. Thank you.
-		<table>
-		<thead><th>Bank Details</th></thead>
-		<tbody>
-		<tr><td>Bank Name : </td><td>________</td></tr>
-		<tr><td>A/c No. : </td><td>_______________</td></tr>
-		<tr><td>IFS Code : </td><td>___________</td></tr>
-		</tbody>
-		<tfoot><td>After depositing the donation amount in bank account, please make sure you notify us using the contact form.</td></tfoot>
-		</table>
+			<p>Jain Muni Locator puts the interests of Jain Community before profit. If this website is useful to you, take one minute to support the non-profit behind it. If every Jain reading this donates Rs. 1000/-, Our fundraiser would be over within an hour. Thank you.</p>
+			<p> </p>
+			<table>
+				<tr align="left"><th colspan="2">Bank Details</th></tr>
+				<tr><td>Bank Name : </td><td>________</td></tr>
+				<tr><td>A/c No. : </td><td>_______________</td></tr>
+				<tr><td>IFS Code : </td><td>___________</td></tr>
+				<tr><td colspan="2">After depositing the donation amount in bank account, please make sure you notify us using the contact form.</td></tr>
+			</table>
 		</div>
-		<div class="sidebar">
-		<img src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" style="width:200px;margin-right:5px">
-		</div>
-		<!-- end widgets -->
+
+		<div class="sidebar"><img src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" style="width:200px;margin-right:5px"></div>
+
 	</div>
-	
+
 	<!--  end content wrapper  -->
- 	
- 	<?php include('footer.php'); ?>
+
+<?php include('footer.php'); ?>
