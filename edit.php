2038aa2fa41da19eb3909cfdc56e502fa0dfd273
Pankaj Jain <capankajjain@smilyo.com>
2015-07-13 23:56:51 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -22,45 +22,10 @@ $birthname = $_POST['birthname'];
 $dob = $_POST['dob'];
 $father = $_POST['father'];
 $mother = $_POST['mother'];
-//$lat = $_POST['lat'];
-//$lng = $_POST['lng'];
-$id = $_POST['ids'];
 $location = $_POST['location'];
 $lat = getlatitude($location);
-$lng = getlongtitude($location);
-function getlatitude($address)
-{
-$address= preg_replace('/\s+/', '+', $address);
-$url = "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=India";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$response = curl_exec($ch);
-curl_close($ch);
-$response_a = json_decode($response);
-return $response_a->results[0]->geometry->location->lat;
-}
-function getlongtitude($address)
-{
-$address= preg_replace('/\s+/', '+', $address);
-$url = "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=India";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$response = curl_exec($ch);
-curl_close($ch);
-$response_a = json_decode($response);
-return $response_a->results[0]->geometry->location->lng;
-
-}
-
-
+$lng = getlongitude($location);
+$id = $_POST['ids'];
 
 // query
 $sqlmunishri = "UPDATE munishri SET name=?, website=?, img=?, dos=?, birthname=?, dob=?, father=?, mother=? WHERE id=?";	
@@ -96,26 +61,5 @@ $q = $db->prepare($sqllocation);
 $q->execute(array($lat,$lng));
 
 header('location: munis.php?id='.$id);
-
-// Muni Location
-//if(isset($_POST['muniid']))
-//{
-//	$id = (int)$_POST['muniid'];
-//	$q = $db->prepare("SELECT * FROM muni_location WHERE mid = ?");
-//	$q->execute(array($id));
-//	if($q->rowCount() == 1)
-//	{
-//		$f = $q->fetch(PDO::FETCH_ASSOC);
-//		echo '<input type="hidden" id="lat" style="display:none" value="'.$f['lat'].'"><input type="hidden" id="lng" style="display:none" value="'.$f['lng'].'"><table><tr><td>Address</td><td><input type="text" id="x"></td></tr>';
-//		echo '</table><input type="button" id="wow" value="Submit"><input type="button" id="cancel" value="Cancel" onclick="history.back();">';
-//	}
-//}
-//if(isset($_POST['mid']))
-//{
-//	$id = (int)$_POST['mid'];
-//	$q = $db->prepare("UPDATE muni_location SET lat = ?,lng = ? WHERE mid=?");
-//	$q->execute(array($_POST['lat'],$_POST['lng'],$id));
-//	echo $_POST['lat'].'-'.$_POST['lng'].'-'.$id;
-//}
 
 ?>
