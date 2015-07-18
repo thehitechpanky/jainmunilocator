c5b89136ed328290b081a8d92d17fafa6a14944a
Pankaj Jain <capankajjain@smilyo.com>
2015-07-19 01:19:26 +0530

    Update add.php

--- add.php
+++ add.php
@@ -35,8 +35,8 @@
 			$text = file_get_contents($url);
 		if(strpos($text,'true'))
 		{
-			$t = $db->prepare('INSERT INTO munishri (upadhi,name,website,img,birthname,dob,father,mother) VALUES (?,?,?,?,?,?,?,?)');
-			if(!$t->execute(array($upadhi,$name,$website,$photo,$birthname,$dob,$father,$mother)))
+			$t = $db->prepare('INSERT INTO munishri (upadhi,name,alias,website,img,birthname,dob,father,mother) VALUES (?,?,?,?,?,?,?,?,?)');
+			if(!$t->execute(array($upadhi,$name,$alias,$website,$photo,$birthname,$dob,$father,$mother)))
 			{
 				echo "<div style='color:red'>Some error encountered.Please Contact the website administrator<br /></div>";
 			}
@@ -52,13 +52,32 @@
 		}
 		}
 		?>
-			<fieldset><center>
+			<fieldset>
 				<form method="post" action="">
 					<div>
-						<input type="text" value="" placeholder="Upadhi" name="upadhi"><br />
-						<input type="text" value="" placeholder="Name" name="name"><br />
+						<select id="upadhi" name="upadhi" class="mediumBox" onchange="showPrefix(this.value); showSuffix(this.value);">
+							<option value="" style='display:none;' disabled selected>Upadhi</option>
+							<option value="1">Acharya</option>
+							<option value="2">Ailacharya</option>
+							<option value="3">Upadhyay</option>
+							<option value="4">Muni</option>
+							<option value="5">Ailak</option>
+							<option value="6">Kshullak</option>
+							<option value="7">Aryika</option>
+						</select>
+						<div id="prefix_here" class="inlineDiv"></div>
+						<input type="text" value="" placeholder="Name" name="name" class="smallBox" />
+						<div id="suffix_here" class="inlineDiv"></div>
+						<input type="text" value="" placeholder="Alias" name="alias" class="verySmallBox" /><br />						
+						<input type="text" value="" placeholder="Current Location" name="location" class="longBox" /><br />						
 						<input type="text" value="" placeholder="Website" name="website"><br />
-						<input type="text" value="" placeholder="Photo Link" name="photo"><br />
+						<input type="text" value="" placeholder="Image Link" name="photo"><br />
+						<input type="hidden" value="" placeholder="Acharya Pad Date in YYYY-MM-DD" id="addacharyadate" name="adate" />
+						<input type="hidden" value="" placeholder="Acharya Pad Given by" id="addacharyaguru" name="aguru" class="longBox" />
+						<input type="hidden" value="" placeholder="Muni Deeksha Date in YYYY-MM-DD" id="addmunidate" name="munidate" />
+						<input type="hidden" value="" placeholder="Muni Deeksha Given by" id="addmuniguru" name="muniguru" class="longBox" />
+						<input type="hidden" value="" placeholder="Aryika Deeksha Date in YYYY-MM-DD" id="addaryikadate" name="aryikadate" />
+						<input type="hidden" value="" placeholder="Aryika Deeksha Given by" id="addaryikaguru" name="aryikaguru" class="longBox" />
 						<input type="text" value="" placeholder="Birth Name" name="birthname"><br />
 						<input type="text" value="" placeholder="Date of Birth in YYYY-MM-DD" name="dob"><br />
 						<input type="text" value="" placeholder="Father" name="father"><br />
@@ -66,12 +85,12 @@
 						<div class="g-recaptcha" data-sitekey="6LcXYP8SAAAAAM8199rOJV8yoCWS4mI5FHb5Q70Q"></div><br />
 						<input type="submit" name="submit" value="Submit" />
 					</div>
-				</form>	</center>
+				</form>
 			</fieldset>
 	
 		</div>
 		<div class="sidebar">
-		<img src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" style="width:200px;margin-right:5px">
+			<img alt="Acharya Kundkund | Jain Muni Locator" src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" style="width:200px;margin-right:5px">
 		</div>
 		<!-- end widgets -->
 	</div>
