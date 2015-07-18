d8238558e72f3dc3b150972a1c281df774792dd8
Pankaj Jain <capankajjain@smilyo.com>
2015-07-19 01:07:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php include('header.php'); ?>
-<!-- start body -->
+
 <body class="page-template-template-homepage-php" onunload="" >
 
-	<!-- start dotted pattern -->
-	<div class="bg-overlay"></div>
-	<!-- end dotted pattern -->
+<!-- start dotted pattern -->
+<div class="bg-overlay"></div>
+<!-- end dotted pattern -->
 
-	<!-- start navigation -->
-	<?php include('menu.php'); ?>
-	<!-- end navigation -->	
+<!-- start navigation -->
+<?php include('menu.php'); ?>
+<!-- end navigation -->	
 	
+	<h1><strong><em><i><cite><blockquote>Jain Muni Locator</blockquote></cite></i></em></strong></h1>
+	<h2>Locate all Digambar Jain Munis</h2>
+	<h3>Get details about digambar jain sadhus and acharya - shris in one website.</h3>
+	<p>
+		The initiative taken is about building a global database to locate all the Digambar Jain Muni-shris in world. The database is freely accessible to all the Jainism followers on this website. As time is eternal and formless, this initiative will help the modern jainism followers to be informed about Munishris just by an instant search which can be done on various smart phones.<br />
+		Jain Muni Locator will expand and reach to Munishri's and Aryika's, who are bondless spiritual souls, will teach us the importance of being self-disciplined.<br />
+		The initiative  is to preserve the beliefs and practices of Jainism and this regularly updated website will help jain community to stay in touch with their religion in this trendy scenario.<br />
+		Thus, advancing will establish oneself in moral values  and will bring spiritual revolution.<br />
+		<b>Note: This is an Opensource Project. To view code files or contribute in coding, please visit <a href="http://github.com/thehitechpanky/jainmunilocator" title="Github Link">http://github.com/thehitechpanky/jainmunilocator</a></b>
+	</p>
+	<ol></ol>
+
 <?php include('footer.php'); ?>
