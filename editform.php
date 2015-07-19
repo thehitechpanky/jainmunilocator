b5815c262c8e5b3f5d30d56e269809df72b13bab
Pankaj Jain <capankajjain@smilyo.com>
2015-07-19 16:11:04 +0530

    Update editform.php

--- editform.php
+++ editform.php
@@ -1,130 +1,198 @@
-<?php include('header.php'); ?>
+<?php
+// configuration
+include('config.php');
 
-<!-- start body -->
-<body onunload="" >
+// fields of munishri
+$upadhi = $_POST['upadhi'];
+$name = $_POST['name'];
+$alias = $_POST['alias'];
+$website = $_POST['website'];
+$img = $_POST['img'];
+$dos = $_POST['dos'];
+$vairagya = $_POST['vairagya'];
+$birthname = $_POST['birthname'];
+$dob = $_POST['dob'];
+$father = $_POST['father'];
+$mother = $_POST['mother'];
+$birthplace = $_POST['birthplace'];
+if($birthplace=="N/A") {
+	$birthlat = 0;
+	$birthlng = 0;
+} else {
+	$birthlat = getlatitude($birthplace);
+	$birthlng = getlongitude($birthplace);
+}
 
-	<!-- start dotted pattern -->
-	<div class="bg-overlay"></div>
-	<!-- end dotted pattern -->
-	
-	<!-- start navigation -->
-	<?php include('menu.php'); ?>
-	<!-- end navigation -->
-	
-	<!-- start content wrapper -->
+// fields of acharya
+$adate = $_POST['adate'];
+$aguru = $_POST['aguru'];
+$aplace = $_POST['aplace'];
+if($aplace=="N/A") {
+	$alat = 0;
+	$alng = 0;
+} else {
+	$alat = getlatitude($aplace);
+	$alng = getlongitude($aplace);
+}
 
-		<div class="container">
+// fields of ailacharya
+$ailacharyaname = $_POST['ailacharyaname'];
+$ailacharyadate = $_POST['ailacharyadate'];
+$ailacharyaguru = $_POST['ailacharyaguru'];
+$ailacharyaplace = $_POST['ailacharyaplace'];
+if($ailacharyaplace=="N/A") {
+	$ailacharyalat = 0;
+	$ailacharyalng = 0;
+} else {
+	$ailacharyalat = getlatitude($ailacharyaplace);
+	$ailacharyalng = getlongitude($ailacharyaplace);
+}
 
-		<div class="page-title">
-			<h1><?php echo 'Edit Guru Details'; ?></h1>
-		</div>
+// fields of upadhyay
+$upadhyayname = $_POST['upadhyayname'];
+$upadhyaydate = $_POST['upadhyaydate'];
+$upadhyayguru = $_POST['upadhyayguru'];
+$upadhyayplace = $_POST['upadhyayplace'];
+if($upadhyayplace=="N/A") {
+	$upadhyaylat = 0;
+	$upadhyaylng = 0;
+} else {
+	$upadhyaylat = getlatitude($upadhyayplace);
+	$upadhyaylng = getlongitude($upadhyayplace);
+}
+
+// fields of muni
+$muniname = $_POST['muniname'];
+$munidate = $_POST['munidate'];
+$muniguru = $_POST['muniguru'];
+$muniplace = $_POST['muniplace'];
+if($muniplace=="N/A") {
+	$munilat = 0;
+	$munilng = 0;
+} else {
+	$munilat = getlatitude($muniplace);
+	$munilng = getlongitude($muniplace);
+}
+
+// fields of ailak
+$ailakname = $_POST['ailakname'];
+$ailakdate = $_POST['ailakdate'];
+$ailakguru = $_POST['ailakguru'];
+$ailakplace = $_POST['ailakplace'];
+if($ailakplace=="N/A") {
+	$ailaklat = 0;
+	$ailaklng = 0;
+} else {
+	$ailaklat = getlatitude($ailakplace);
+	$ailaklng = getlongitude($ailakplace);
+}
+
+// fields of kshullak
+$kname = $_POST['kname'];
+$kdate = $_POST['kdate'];
+$kguru = $_POST['kguru'];
+$kplace = $_POST['kplace'];
+if($kplace=="N/A") {
+	$klat = 0;
+	$klng = 0;
+} else {
+	$klat = getlatitude($kplace);
+	$klng = getlongitude($kplace);
+}
+
+// fields of bhramcharya
+$bhramcharyadate = $_POST['bhramcharyadate'];
+$bhramcharyaguru = $_POST['bhramcharyaguru'];
+
+// fields of aryika
+$aryikadate = $_POST['aryikadate'];
+$aryikaguru = $_POST['aryikaguru'];
+$aryikaplace = $_POST['aryikaplace'];
+if($aryikaplace=="N/A") {
+	$aryikalat = 0;
+	$aryikalng = 0;
+} else {
+	$aryikalat = getlatitude($aryikaplace);
+	$aryikalng = getlongitude($aryikaplace);
+}
+
+// fields of kshullika
+$kshullikadate = $_POST['kshullikadate'];
+$kshullikaguru = $_POST['kshullikaguru'];
+$kshullikaplace = $_POST['kshullikaplace'];
+if($kshullikaplace=="N/A") {
+	$kshullikalat = 0;
+	$kshullikalng = 0;
+} else {
+	$kshullikalat = getlatitude($kshullikaplace);
+	$kshullikalng = getlongitude($kshullikaplace);
+}
+
+// fields of muni_location
+$location = $_POST['location'];
+if($location=="N/A") {
+	$lat = 0;
+	$lng = 0;
+} else {
+	$lat = getlatitude($location);
+	$lng = getlongitude($location);
+}
+
+$id = $_POST['ids'];
+
+// Captcha Check
+$url = "https://www.google.com/recaptcha/api/siteverify?secret=6LcXYP8SAAAAAH7WUPMtiHoEiTnev6ofbzsuRY4U&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR'];
+$text = file_get_contents($url);
+
+	if(strpos($text,'true')) {
 		
-		<div class="divider clear"></div>
+		// Edit Database
+		$sqlmunishri = "UPDATE munishri SET upadhi=?, name=?, alias=?, website=?, img=?, dos=?, vairagya=?, birthname=?, dob=?, father=?, mother=?, birthlat=?, birthlng=? WHERE id=?";	
+		$q = $db->prepare($sqlmunishri);
+		$q->execute(array($upadhi,$name,$alias,$website,$img,$dos,$vairagya,$birthname,$dob,$father,$mother,$birthlat,$birthlng,$id));
 		
-		<div>
-			<?php
-				$id=$_GET['id'];
-				$result = $db->prepare("SELECT * FROM munishri, upadhis, muni_location, acharya, ailacharya, upadhyay, muni, ailak, kshullak, bhramcharya, aryika WHERE id= :userid AND upadhi=uid AND id=mid AND id=acharyaid AND id=ailacharyaid AND id=upadhyayid AND id=muniid AND id=ailakid AND id=kid AND id=bhramcharyaid AND id=aryikaid");
-				$result->bindParam(':userid', $id);
-				$result->execute();
-				for($i=0; $row = $result->fetch(); $i++){
-			?>
-			<div style="float:left;width:60%">				
-				<form action="edit.php" method="POST">
-					
-					<input type="hidden" name="ids" value="<?php echo $id; ?>" />
-					
-					<b>Name</b><br />
-					<select name="upadhi" id="upadhi" class="mediumBox" placeholder="Upadhi" onchange="showPrefix(this.value); showSuffix(this.value);">
-					<option value="1" <?php if($row['upadhi']==1) { echo "selected"; } ?> >Acharya</option>
-					<option value="2" <?php if($row['upadhi']==2) { echo "selected"; } ?> >Ailacharya</option>
-					<option value="3" <?php if($row['upadhi']==3) { echo "selected"; } ?> >Upadhyay</option>
-					<option value="4" <?php if($row['upadhi']==4) { echo "selected"; } ?> >Muni</option>
-					<option value="5" <?php if($row['upadhi']==5) { echo "selected"; } ?> >Ailak</option>
-					<option value="6" <?php if($row['upadhi']==6) { echo "selected"; } ?> >Kshullak</option>
-					<option value="7" <?php if($row['upadhi']==7) { echo "selected"; } ?> >Aryika</option>
-					</select>
-					<div id="prefix_here" class="inlineDiv"></div>
-					<input type="text" name="name" class="smallBox" placeholder="Name" value="<?php echo $row['name']; ?>" />
-					<div id="suffix_here" class="inlineDiv"></div>
-					<input type="text" name="alias" class="verySmallBox" placeholder="Alias" value="<?php echo $row['alias']; ?>" /><br /><br />
-					
-					<b>Current Location</b><br />
-					<input type="text" name="location" class="longBox" value="<?php echo getaddress($row['lat'],$row['lng']); ?>"/><br /><br />
-					
-					Website Link<input type="text" name="website" value="<?php echo $row['website']; ?>" /><br />
-					Image Link<input type="text" name="img" class="longBox" value="<?php echo $row['img']; ?>" /><br />
-					Date of Samadhi (if applicable) in YYYY-MM-DD<input type="text" name="dos" class="smallBox" value="<?php echo $row['dos']; ?>" /><br /><br />
-					
-					<b>Acharya Pad Details</b><br />
-					Date in YYYY-MM-DD<input type="text" name="adate" class="smallBox" value="<?php echo $row['adate']; ?>" /><br />
-					Guru<input type="text" id="aguru" value="<?php echo getmuni($row['aguru']); ?>" list="aguruList"/>
-					<datalist id="aguruList">
-					</datalist>
-					<input type="hidden" name="aguru" id="aguruHidden" value="" />
-					<br /><br />
-					
-					<b>Ailacharya Pad Details</b><br />
-					Name<input type="text" name="ailacharyaname" value="<?php echo $row['ailacharyaname']; ?>" /><br />
-					Date in YYYY-MM-DD<input type="text" name="ailacharyadate" class="smallBox" value="<?php echo $row['ailacharyadate']; ?>" /><br />
-					Guru ID<input type="text" name="ailacharyaguru" value="<?php echo $row['ailacharyaguru']; ?>" /><br /><br />
-					
-					<b>Upadhyay Pad Details</b><br />
-					Name<input type="text" name="upadhyayname" value="<?php echo $row['upadhyayname']; ?>" /><br />
-					Date in YYYY-MM-DD<input type="text" name="upadhyaydate" class="smallBox" value="<?php echo $row['upadhyaydate']; ?>" /><br />
-					Guru ID<input type="text" name="upadhyayguru" value="<?php echo $row['upadhyayguru']; ?>" /><br /><br />
-					
-					<b>Muni Deeksha Details</b><br />
-					Name<input type="text" name="muniname" value="<?php echo $row['muniname']; ?>" /><br />
-					Date in YYYY-MM-DD<input type="text" name="munidate" class="smallBox" value="<?php echo $row['munidate']; ?>" /><br />
-					Guru ID<input type="text" name="muniguru" value="<?php echo $row['muniguru']; ?>" /><br /><br />
-					
-					<b>Ailak Deeksha Details</b><br />
-					Name<input type="text" name="ailakname" value="<?php echo $row['ailakname']; ?>" /><br />
-					Date in YYYY-MM-DD<input type="text" name="ailakdate" class="smallBox" value="<?php echo $row['ailakdate']; ?>" /><br />
-					Guru ID<input type="text" name="ailakguru" value="<?php echo $row['ailakguru']; ?>" /><br /><br />
-					
-					<b>Kshullak Deeksha Details</b><br />
-					Name<input type="text" name="kname" value="<?php echo $row['kname']; ?>" /><br />
-					Date in YYYY-MM-DD<input type="text" name="kdate" class="smallBox" value="<?php echo $row['kdate']; ?>" /><br />
-					Guru ID<input type="text" name="kguru" value="<?php echo $row['kguru']; ?>" /><br /><br />
-					
-					<b>Bhramcharya Vrat Details</b><br />
-					Date in YYYY-MM-DD<input type="text" name="bhramcharyadate" class="smallBox" value="<?php echo $row['bhramcharyadate']; ?>" /><br />
-					Guru ID<input type="text" name="bhramcharyaguru" value="<?php echo $row['bhramcharyaguru']; ?>" /><br /><br />
-					
-					<b>Vairagya Details</b><br />
-					Date in YYYY-MM-DD<input type="text" name="vairagya" class="smallBox" value="<?php echo $row['vairagya']; ?>" /><br /><br />
-					
-					<b>Aryika Deeksha Details</b><br />
-					Date in YYYY-MM-DD<input type="text" name="aryikadate" class="smallBox" value="<?php echo $row['aryikadate']; ?>" /><br />
-					Guru ID<input type="text" name="aryikaguru" value="<?php echo $row['aryikaguru']; ?>" /><br /><br />
-					
-					<b>History</b><br />
-					Birthname<input type="text" name="birthname" value="<?php echo $row['birthname']; ?>" /><br />
-					Date of Birth<input type="text" name="dob" class="smallBox" value="<?php echo $row['dob']; ?>" /><br />
-					Father<input type="text" name="father" value="<?php echo $row['father']; ?>" /><br />
-					Mother<input type="text" name="mother" value="<?php echo $row['mother']; ?>" /><br /><br />
-					
-					<div class="g-recaptcha" data-sitekey="6LcXYP8SAAAAAM8199rOJV8yoCWS4mI5FHb5Q70Q"></div><br />
-					
-					<input type="submit" value="Save" />&nbsp;&nbsp;&nbsp;
-					<input type="reset" value="Reset" />&nbsp;&nbsp;&nbsp;
-					<input type="button" value="Back" onclick="history.back();" />
-					
-				</form>
-			</div>
-			
-			<div class="innerDiv">
-				<img alt="<?php echo getmuni($row['id']); ?>" width="315px" src="<?php echo $row['img']; ?>" />
-			</div>
-			
-			<?php } ?>
-			
-		</div>
+		$sqlacharya = "UPDATE acharya SET adate=?, aguru=? WHERE acharyaid='$id'";
+		$q = $db->prepare($sqlacharya);
+		$q->execute(array($adate,$aguru));
 		
-	</div>
+		$sqlailacharya = "UPDATE ailacharya SET ailacharyaname=?, ailacharyadate=?, ailacharyaguru=? WHERE ailacharyaid='$id'";
+		$q = $db->prepare($sqlailacharya);
+		$q->execute(array($ailacharyaname,$ailacharyadate,$ailacharyaguru));
+		
+		$sqlupadhyay = "UPDATE upadhyay SET upadhyayname=?, upadhyaydate=?, upadhyayguru=? WHERE upadhyayid='$id'";
+		$q = $db->prepare($sqlupadhyay);
+		$q->execute(array($upadhyayname,$upadhyaydate,$upadhyayguru));
+		
+		$sqlmuni = "UPDATE muni SET muniname=?, munidate=?, muniguru=? WHERE muniid='$id'";
+		$q = $db->prepare($sqlmuni);
+		$q->execute(array($muniname,$munidate,$muniguru));
+		
+		$sqlailak = "UPDATE ailak SET ailakname=?, ailakdate=?, ailakguru=? WHERE ailakid='$id'";
+		$q = $db->prepare($sqlailak);
+		$q->execute(array($ailakname,$ailakdate,$ailakguru));
+		
+		$sqlkshullak = "UPDATE kshullak SET kname=?, kdate=?, kguru=? WHERE kid='$id'";
+		$q = $db->prepare($sqlkshullak);
+		$q->execute(array($kname,$kdate,$kguru));
+		
+		$sqlaryika = "UPDATE aryika SET aryikadate=?, aryikaguru=? WHERE aryikaid='$id'";
+		$q = $db->prepare($sqlaryika);
+		$q->execute(array($aryikadate,$aryikaguru));
+		
+		$sqlbhramcharya = "UPDATE bhramcharya SET bhramcharyadate=?, bhramcharyaguru=? WHERE bhramcharyaid='$id'";
+		$q = $db->prepare($sqlbhramcharya);
+		$q->execute(array($bhramcharyadate,$bhramcharyaguru));
+		
+		$sqllocation = "UPDATE muni_location SET lat=?, lng=? WHERE mid='$id'";
+		$q = $db->prepare($sqllocation);
+		$q->execute(array($lat,$lng));
+		
+	} else {
 	
-	<!--  end content wrapper  -->
- 	
-<?php include('footer.php'); ?>
+		echo "Wrong Recaptcha, Please try again!";
+	
+	}
+
+header('location: ./munis.php?id='.$id);
+
+?>
