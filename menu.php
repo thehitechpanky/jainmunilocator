71f06e3da786c58ef794a402ef1230f375b91620
Pankaj Jain <capankajjain@smilyo.com>
2015-07-22 13:28:04 +0530

    Replaced Add info with events
    Since add is already included in search

--- menu.php
+++ menu.php
@@ -1,20 +1,20 @@
-<div id="pgcontainer"><!-- @start #pgcontainer -->
+<div id="pgcontainer">
 	
 	<a href="#" class="menubtn"></a>
     
 	<div id="menu">
         <ul>		            
-			<li><a href="./">Home</a></li>
-			<li><a href="./about.php">About</a></li>
-			<li><a href="./munis.php">Munis</a></li>
-			<li><a href="./meaning-of-108.php">Meaning of 108</a></li>
-			<li><a href="./mahagun.php">Mahagun</a></li>
-			<li><a href="./contact.php">Contact</a></li>
-			<li><a href="./donate.php">Donate</a></li>
-			<li><a href="./add.php">Add Information</a></li>
+			<li><a href="./" title="Home">Home</a></li>
+			<li><a href="./about.php" title="About">About</a></li>
+			<li><a href="./munis.php" title="Munis">Munis</a></li>
+			<li><a href="./events.php"  title="Events">Events</a></li>
+			<li><a href="./meaning-of-108.php" title="Meaning of 108">Meaning of 108</a></li>
+			<li><a href="./mahagun.php" title="Mahagun">Mahagun</a></li>
+			<li><a href="./contact.php" title="Contact">Contact</a></li>
+			<li><a href="./donate.php"  title="Donate">Donate</a></li>
 		</ul>
     </div>
     
     <div class="overlay"></div>
 
-</div><!-- @end #pgcontainer -->
+</div>
