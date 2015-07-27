df8e241f866652f0befc9b0dc624c2c7594c7193
Pankaj Jain <capankajjain@smilyo.com>
2015-07-27 13:38:57 +0530

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -25,13 +25,14 @@
 			<table style="width:100%">
 				<tr align="left"><th colspan="2">Main Menu</th><th colspan="2">Footer Links</th></tr>
 				<tr><td>&#8226;</td><td><a href="/">Home</a></td><td>&#8226;</td><td><a href="team.php">Team</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="about.php">About</a></td><td>&#8226;</td><td><a href="references.php">References</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="munis.php">Munis</a></td><td>&#8226;</td><td><a href="sitemap.php">Sitemap</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="meaning-of-108.php">Meaning of 108</a></td><td>&#8226;</td><td><a href="tos.php">Terms and Conditions</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="mahagun.php">Mahagun</a></td><td>&#8226;</td><td><a href="disclaimer.php">Disclaimer</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="contact.php">Contact</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="donate.php">Donate</a></td></tr>
-				<tr><td>&#8226;</td><td><a href="add.php">Add Information</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="./about.php">About</a></td><td>&#8226;</td><td><a href="references.php">References</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="./munis.php">Munis</a></td><td>&#8226;</td><td><a href="sitemap.php">Sitemap</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="./events.php">Events</a></td><td>&#8226;</td><td><a href="tos.php">Terms and Conditions</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="./chaturmas.php">Chaturmas</a></td><td>&#8226;</td><td><a href="disclaimer.php">Disclaimer</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="./meaning-of-108.php">Meaning of 108</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="./mahagun.php">Mahagun</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="./contact.php">Contact</a></td></tr>
+				<tr><td>&#8226;</td><td><a href="./donate.php">Donate</a></td></tr>
 			</table>
 			<p> </p>
 			<table style="width:100%">
