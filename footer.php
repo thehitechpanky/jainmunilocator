b48b11ebde29237753082012ed1b9225e536a539
Pankaj Jain <capankajjain@smilyo.com>
2015-07-27 13:36:49 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -64,18 +64,7 @@ while($row = mysqli_fetch_assoc($result)){
 	</div>
 	<div class="copyright">&copy; 2014 Developed by <a  target="_blank" href="http://dhruvjain.org" title="Dhruv Jain">Dhruv Jain</a>	</div>
 
-	<!-- start footer-menu -->
-		<div class="footer-menu">
-			<ul class="footer_menu">
-				<li><a href="./team.php" title="Team">Team</a></li>
-				<li><a href="http://github.com/thehitechpanky/jainmunilocator" title="Github Link">Github</a></li>
-				<li><a href="./references.php" title="References">References</a></li>
-				<li><a href="./sitemap.php" title="Sitemap">Sitemap</a></li>
-				<li><a href="./tos.php" title="Terms and Conditions">Terms and Conditions</a></li>
-				<li><a href="./disclaimer.php" title="Disclaimer">Disclaimer</a></li>
-			</ul>
-		</div>
-	<!-- end footer-menu -->
+<?php include("footerMenu.php") ?>
 
 </div>
 <!-- end footer -->
