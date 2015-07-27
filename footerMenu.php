94f4518f8c2c153e0804be743b387d671a5927ad
Pankaj Jain <capankajjain@smilyo.com>
2015-07-27 13:35:55 +0530

    Create footerMenu.php

---
+++ footerMenu.php
@@ -0,0 +1,12 @@
+<!-- start footer-menu -->
+<div class="footer-menu">
+	<ul class="footer_menu">
+		<li><a href="./team.php" title="Team">Team</a></li>
+		<li><a href="http://github.com/thehitechpanky/jainmunilocator" title="Github Link">Github</a></li>
+		<li><a href="./references.php" title="References">References</a></li>
+		<li><a href="./sitemap.php" title="Sitemap">Sitemap</a></li>
+		<li><a href="./tos.php" title="Terms and Conditions">Terms and Conditions</a></li>
+		<li><a href="./disclaimer.php" title="Disclaimer">Disclaimer</a></li>
+	</ul>
+</div>
+<!-- end footer-menu -->
