0bdf27c7836cc7f02caa6378f3ddba868826fc5c
Pankaj Jain <capankajjain@smilyo.com>
2015-07-31 19:24:38 +0530

    Update fbComments.js

--- js/fbComments.js
+++ js/fbComments.js
@@ -2,6 +2,6 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=_________";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=__________";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
