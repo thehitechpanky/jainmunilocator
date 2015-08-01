f407cd69fb25af5b2ba6ae56c662bbdf5871e598
Pankaj Jain <capankajjain@smilyo.com>
2015-08-01 11:53:19 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -10,8 +10,9 @@
 			<li><a href="./events.php"  title="Events">Events</a></li>
 			<li><a href="./chaturmas.php"  title="Chaturmas">Chaturmas</a></li>
 			<li><a href="./meaning-of-108.php" title="Meaning of 108">Meaning of 108</a></li>
-			<li><a href="./mahagun.php" title="Mahagun">Mahagun</a></li>
-			<li><a href="./uttargun.php" title="Uttargun">Uttargun</a></li>
+			<li><a href="./mahagun.php" title="Mahagun">28 Mahagun</a></li>
+			<li><a href="./uttargun.php" title="Uttargun">34 Uttargun</a></li>
+			<li><a href="./ekadash-pratima.php" title="Ekadash Pratima">Ekadash Pratima</a></li>
 			<li><a href="./contact.php" title="Contact">Contact</a></li>
 			<li><a href="./donate.php"  title="Donate">Donate</a></li>
 		</ul>
