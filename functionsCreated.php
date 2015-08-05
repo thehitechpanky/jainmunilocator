381c5c7ac19a3437a097850f885e56b8b473e35b
Pankaj Jain <capankajjain@smilyo.com>
2015-08-05 13:58:00 +0530

    Update functionsCreated.php

--- functionsCreated.php
+++ functionsCreated.php
@@ -107,36 +107,4 @@ function getguru($id)
 	}
 }
 
-//Find Muni Name and Details
-$showmuni = false;
-// When an id is entered in the url
-if(isset($_GET['id'])) {
-	$id = (int)$_GET['id'];
-	$t = $db->prepare('SELECT * FROM munishri, upadhis, kshullika, aryika, bhramcharya, kshullak, ailak, muni, upadhyay, ailacharya, acharya, muni_location, history, contact
-						WHERE id = ? AND approved=1 AND uid=upadhi AND id=kshullikaid AND id=aryikaid AND id=bhramcharyaid AND id=kid AND id=ailakid AND id=muniid AND id=upadhyayid AND id=ailacharyaid AND id=acharyaid AND id=mid AND id=historyid AND id=contactid');
-	$t->execute(array($id));
-	// For the page showing muni details
-	if($t->rowCount() == 1) {
-		$getinfo = $t->fetch(PDO::FETCH_ASSOC);
-		$title = getmuni($id);
-		$titletag = $title.' | Jain Muni Locator';
-		$showmuni = true;
-		$schemaOrg = 'itemprop="about" itemscope itemtype="http://schema.org/Person"';
-	}
-	// For the page where wrong muni id is selected
-	else{
-		$title = 'Jain Muni Locator';
-		$titletag = $title;
-		$schemaOrg = '';
-	}
-}
-// When no id is entered in the url it will show the list of munis.
-else{
-	$title = "Jain Muni Locator";
-	$titletag = $title;
-	$t = $db->prepare('SELECT * FROM munishri, upadhis, aryika, kshullak, ailak, muni, upadhyay, ailacharya, acharya WHERE approved=1 AND uid=upadhi AND id=aryikaid AND id=kid AND id=ailakid AND id=muniid AND id=upadhyayid AND id=ailacharyaid AND id=acharyaid ORDER BY uid, name ASC');
-	$t->execute();
-	$schemaOrg = '';
-}
-
 ?>
