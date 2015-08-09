9572f6b69b3fc9c5873b7ecc7ff90c848b4f427b
Pankaj Jain <capankajjain@smilyo.com>
2015-08-09 11:06:29 +0530

    Create googleScripts.php

---
+++ googleScripts.php
@@ -0,0 +1,5 @@
+<!-- start google scripts -->
+<script type='text/javascript' src='https://www.google.com/recaptcha/api.js' async defer></script>
+<script type='text/javascript' src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDcnIUTozeOU26CWZRSxQRRoTFeZtvzX6Y'></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
+<!-- end google scripts -->
