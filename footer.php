3cc2651f68cc04a39dee85c6900ba5173239656d
Pankaj Jain <capankajjain@smilyo.com>
2015-08-09 11:01:12 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,26 +1,11 @@
 <!-- start footer -->
 
 <!-- Analytics Included -->
-<?php include_once("analyticstracking.php") ?>
-
-<!-- Google Map Markers -->
 <?php
-$array = array();
-$sql = "SELECT * FROM muni_location, munishri WHERE mid=id AND lat<>0 AND dos='0000-00-00' ORDER BY upadhi DESC";
-$result=mysqli_query($link,$sql);
-$i=0;
-while($row = mysqli_fetch_assoc($result)){
-	if(isset($row)){
-		$array[$i][0]='<center><a href="./munis.php?id='.$row['id'].'"><img style="opacity:0.5" width="200px" src="'.$row['img'].'" /><br />'.getmuni($row['id']).'<br />'.$row['location'].'</a></center>';
-	$array[$i][1]=$row['lat'];
-	$array[$i][2]=$row['lng'];
-	$i++;
-	}
-}
+include_once("analyticstracking.php");
+include "markers.php";
+include('javascripts.php');
 ?>
-
-<!-- Load Javascripts -->
-<?php include('javascripts.php'); ?>
 
 <div class="space"></div>
 <div class="sub-footer clearfix">
@@ -66,7 +51,7 @@ while($row = mysqli_fetch_assoc($result)){
 	</div>
 	<!-- end footer social -->
 
-<p><?php echo $metaKeywords; ?></p>
+<div><span itemprop="keywords"><?php echo $metaKeywords; ?></span></div>
 
 </body>
 <!-- End Body -->
