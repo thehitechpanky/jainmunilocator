1e5b99f411109f023f3ace86b3174c202376a4d5
Pankaj Jain <capankajjain@smilyo.com>
2015-10-06 11:47:22 +0530

    Update functionsCreated.php

--- functionsCreated.php
+++ functionsCreated.php
@@ -1,7 +1,7 @@
 <?php
 
 // Load Configuration File
-include('./config.php');
+include 'config.php';
 
 //Function to find muni details
 function getmuni($id)
@@ -107,4 +107,8 @@ function getguru($id)
 	}
 }
 
+//Mobile detection
+function isMobile() {
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
 ?>
