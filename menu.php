b7975800266664c9aa74cc79ddb69d5ac260cc71
Pankaj Jain <capankajjain@smilyo.com>
2015-10-06 11:49:51 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,23 +1,56 @@
+<script type='text/javascript' src='js/sliderMenu.js'></script>
+<!-- Start Page Container -->
 <div id="pgcontainer">
 	
-	<a href="#" class="menubtn"></a>
+	
+	<a href="#" class="menubtn" title="Menu"></a>
     
 	<div id="menu">
+		
         <ul>		            
-			<li><a href="./" title="Home">Home</a></li>
-			<li><a href="./about.php" title="About">About</a></li>
-			<li><a href="./munis.php" title="Munis">Munis</a></li>
-			<li><a href="./events.php"  title="Events">Events</a></li>
-			<li><a href="./chaturmas.php"  title="Chaturmas">Chaturmas</a></li>
-			<li><a href="./meaning-of-108.php" title="Meaning of 108">Meaning of 108</a></li>
-			<li><a href="./mahagun.php" title="Mahagun">28 Mahagun</a></li>
-			<li><a href="./uttargun.php" title="Uttargun">34 Uttargun</a></li>
-			<li><a href="./ekadash-pratima.php" title="Ekadash Pratima">Ekadash Pratima</a></li>
-			<li><a href="./contact.php" title="Contact">Contact</a></li>
-			<li><a href="./donate.php"  title="Donate">Donate</a></li>
+			<li><a target="" href="./" title="Home">Home</a></li>
+			<li><a target="" href="./about.php" title="About">About</a></li>
+			<li><a target="" href="./munis.php" title="Munis">Munis</a></li>
+			<li><a target="" href="./events.php"  title="Events">Events</a></li>
+			<li><a target="" href="./chaturmas.php"  title="Chaturmas">Chaturmas</a></li>
+			<li><a target="" href="./meaning-of-108.php" title="Meaning of 108">Meaning of 108</a></li>
+			<li><a target="" href="./mahagun.php" title="Mahagun">28 Mahagun</a></li>
+			<li><a target="" href="./uttargun.php" title="Uttargun">34 Uttargun</a></li>
+			<li><a target="" href="./ekadash-pratima.php" title="Ekadash Pratima">Ekadash Pratima</a></li>
+			<li><a target="" href="./references.php" title="References">References</a></li>
+			<li><a target="" href="./contact.php" title="Contact">Contact</a></li>
+			<li><a target="" href="./donate.php"  title="Donate">Donate</a></li>
+			<li><a target="_blank" href="https://docs.google.com/forms/d/1sYAbfHaI6MheZWN0EKeVkiqyWD17-0RsSNJ9vEIHRK0/viewform" title="Submit Feedback">Feedback</a></li>
+			<li><a target="_blank" href="http://github.com/thehitechpanky/jainmunilocator" title="Github Link">Github</a></li>
 		</ul>
+		
     </div>
+	
+	<div class="overlay"></div>
     
-    <div class="overlay"></div>
+	
+	<!-- Start Right Menu -->
 
+	<!-- <a href="#" class="menubtnRight"></a>
+		
+	<div id="menuRight">
+			
+		<ul>
+			<li><a target="" href="./team.php" title="Team">Team</a></li>
+			<li><a target="_blank" href="https://docs.google.com/forms/d/1sYAbfHaI6MheZWN0EKeVkiqyWD17-0RsSNJ9vEIHRK0/viewform" title="Submit Feedback">Submit Feedback</a></li>
+			<li><a target="_blank" href="http://github.com/thehitechpanky/jainmunilocator" title="Github Link">Github</a></li>
+			<li><a target="" href="./references.php" title="References">References</a></li>
+			<li><a target="" href="./sitemap.php" title="Sitemap">Sitemap</a></li>
+			<li><a target="" href="./tos.php" title="Terms and Conditions">Terms and Conditions</a></li>
+			<li><a target="" href="./disclaimer.php" title="Disclaimer">Disclaimer</a></li>
+		</ul>
+		
+	</div>
+	
+	<div class="overlay"></div> -->
+	
+	<!-- End Right Menu -->
+	
+	
 </div>
+<!-- Start Page Container -->
