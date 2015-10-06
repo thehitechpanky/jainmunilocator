f744a44c093e5e06cf951b4379960049408da792
Pankaj Jain <capankajjain@smilyo.com>
2015-10-06 19:39:55 +0530

    Create parishaha.php

---
+++ parishaha.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+
+	<!-- Start Head -->
+	<head>
+		<!-- start title - should be 57 characters max as suggested by http://seorch.eu -->
+		<title>Parishaha Jain Muni Locator Details Digambar Jain Sadhus Sadhvis</title>
+		<!-- start meta -->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="description" content="Jain Muni Locator is about building a global database to locate all the Digambar Jain Muni-shris in world, freely accessible to all the Jainism followers.">
+		<!-- Meta Keywords should be 8 only for optimum SEO results as suggested by http://www.seoworkers.com/tools/analyzer.html -->
+		<meta name="keywords" content="Parishaha, Jain Muni Locator, Details Digambar Jain Sadhus Sadhvis, Jainism, Jain Sadhu, Jain Acharya, Jain Guru, Meaning of 108, Mahagun, list of all digamabar jain munis">
+		<meta name="author" content="Jain Muni Locator">
+		<meta name=viewport content="width=device-width, initial-scale=1">
+		
+		<?php
+		include 'alexa.php';
+		include 'css.php';
+		?>
+		
+	</head>
+	<!-- End Head -->
+	
+	<?php include 'jquery.php'; include 'functions/universal.php'; ?>
+	
+	<!-- start body -->
+	<body itemscope itemtype="http://schema.org/WebPage" >
+		
+		<?php
+		include('menu.php');
+		if(isMobile()){}{
+		?>
+		
+		<!-- start content wrapper -->
+		<div class="container" >
+			
+		<?php } ?>
+			
+			<div class="page-title">
+				<h1><span itemprop="name">22 Parishaha</span></h1>
+			</div>
+		
+		<?php if(isMobile()){}{ ?>
+		<div class="divider clear"></div>
+		<?php } ?>
+		
+		<div>
+			
+		</div>
+			
+		<?php if(isMobile()){}{ ?>
+		</div><!-- end content wrapper -->
+		<?php } ?>
+			
+	</body>
