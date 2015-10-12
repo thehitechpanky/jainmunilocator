d242fb7968f5dd4b6c99ae376a0759b95564b04c
Pankaj Jain <capankajjain@smilyo.com>
2015-10-12 11:23:27 +0530

    Create foot.php

---
+++ foot.php
@@ -0,0 +1,63 @@
+<!-- start footer -->
+
+<!-- Analytics Included -->
+<?php
+include_once("analyticstracking.php");
+
+if(isMobile()){}else{
+	include 'markers.php';
+}
+
+include 'javascripts.php';
+?>
+
+<div class="space"></div>
+<div class="sub-footer clearfix">
+	<div id="progress-back" class="load-item">
+		<div id="progress-bar"></div>
+	</div>
+	<div class="copyright">&copy; 2014 Developed by <a  target="_blank" href="http://dhruvjain.org" title="Dhruv Jain">Dhruv Jain</a>	</div>
+
+<?php include 'footerMenu.php'; ?>
+
+</div>
+<!-- end footer -->
+
+	<div id="slidecaption"></div>
+
+	<!-- srart footer social -->
+	<div class="social">
+
+		<a title="Facebook" target="_blank" href="http://www.facebook.com/jainmunilocator">
+			<img width="25" height="25" title="Visit our Facebook Page" alt="Facebook | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/facebook.png" />
+		</a><br />
+
+		<a title="Follow me" target="_blank" href="http://twitter.com/jainmunilocator">
+			<img width="25" height="25" title="Visit our Twitter Handle" alt="Follow Me | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/twitter.png" />
+		</a><br />
+
+		<a title="Google Plus" target="_blank" href="http://plus.google.com/+JainmunilocatorOrg1">
+			<img width="25" height="25" title="Visit our Google Plus Page" alt="Google Plus | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/googleplus.png" />
+		</a><br />
+
+		<!--<a title="Flickr" target="_blank" href="#">
+			<img width="25" height="25" title="Visit our Flickr Link" alt="Flickr | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/flickr.png" />
+		</a><br />
+		
+		<a title="Youtube" target="_blank" href="#">
+			<img width="25" height="25" title="Visit our Youtube Channel" alt="Youtube | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/youtube.png" />
+		</a><br />
+
+		<a title="Linkedin" target="_blank" href="#">
+			<img width="25" height="25" title="Visit our Linkedin Page" alt="Linkedin | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/linkedin.png" />
+		</a><br />-->
+
+	</div>
+	<!-- end footer social -->
+
+<?php include 'keywords.php'; ?>
+
+</body>
+<!-- End Body -->
+
+</html>
