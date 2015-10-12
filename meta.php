7344f446a1c9e8655ab60fc56edd4124d90b6033
Pankaj Jain <capankajjain@smilyo.com>
2015-10-12 11:09:05 +0530

    Delete meta.php

--- meta.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="description" content="Jain Muni Locator is about building a global database to locate all the Digambar Jain Muni-shris in world, freely accessible to all the Jainism followers.">
-<!-- Meta Keywords should be 8 only for optimum SEO results as suggested by http://www.seoworkers.com/tools/analyzer.html -->
-<meta name="keywords" content="<?php echo $metaKeywords; ?>">
-<meta name="author" content="Jain Muni Locator">
-<meta name=viewport content="width=device-width, initial-scale=1">
