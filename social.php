fbd254cc75f4d802fbc8ac44ee6176e3c7c5990a
Pankaj Jain <capankajjain@smilyo.com>
2015-10-12 10:39:51 +0530

    Create social.php

---
+++ social.php
@@ -0,0 +1,26 @@
+<div class="social">
+	
+	<a title="Facebook" target="_blank" href="http://www.facebook.com/jainmunilocator">
+		<img width="25" height="25" title="Visit our Facebook Page" alt="Facebook | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/facebook.png" />
+	</a><br />
+	
+	<a title="Follow me" target="_blank" href="http://twitter.com/jainmunilocator">
+		<img width="25" height="25" title="Visit our Twitter Handle" alt="Follow Me | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/twitter.png" />
+	</a><br />
+	
+	<a title="Google Plus" target="_blank" href="http://plus.google.com/+JainmunilocatorOrg1">
+		<img width="25" height="25" title="Visit our Google Plus Page" alt="Google Plus | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/googleplus.png" />
+	</a><br />
+	
+	<!--<a title="Flickr" target="_blank" href="#">
+<img width="25" height="25" title="Visit our Flickr Link" alt="Flickr | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/flickr.png" />
+</a><br />
+
+<a title="Youtube" target="_blank" href="#">
+<img width="25" height="25" title="Visit our Youtube Channel" alt="Youtube | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/youtube.png" />
+</a><br />
+<a title="Linkedin" target="_blank" href="#">
+<img width="25" height="25" title="Visit our Linkedin Page" alt="Linkedin | Jain Muni Locator" src="./images/icons/20-social-media-icons/57x57/linkedin.png" />
+</a><br />-->
+	
+</div>
