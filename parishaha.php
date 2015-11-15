13fd0d3a64f848adf4a02bb0b7b10838c3261674
Pankaj Jain <capankajjain@smilyo.com>
2015-11-16 00:19:16 +0530

    Update parishaha.php

--- parishaha.php
+++ parishaha.php
@@ -26,7 +26,49 @@ include 'head.php';
 		<?php } ?>
 		
 		<div>
-			Hello
+			Kshut, i.e., Hunger,
+
+Pipasa, i.e., Thirst,
+
+Sita, i.e., Cold.
+
+Ushna, i.e., Heat,
+
+Damsamasaka, i.e. Insect-bite,
+
+Nagnya, i.e., Nakedness,
+
+Arati, i.e., Ennui or disagreeable surroundings,
+
+Stri, i.e., Sex-passion,
+
+Charya, i.e., Walking too much,
+
+Nishadya, i.e., Continuous sitting in one posture,
+
+Sayya, i.e., Resting on hard earth,
+
+Akrosa, i.e., Abuse or unpleasant and insulting language,
+
+Vadha, i.e., Beating or violence inflicted by cruel persons,
+
+Yachana, i.e., Begging or desire to beg for food, medicine, etc.,
+
+Alabha, i.e., Disappointment from not getting what one wants, e.g. food,
+
+Roga, i.e., Diseases and infirmities in the body,
+
+Trnasparsa, i.e., Thorn-pricks or pricks from the sharp grass,
+
+Mala, i.e., Dirt and impurity all over the body,
+
+Satkara-puraskara, i.e. Remaining uninfluenced by praise or reward,
+
+Prajna, i.e., Pride of knowledge,
+
+Ajnana, i.e., feeling of ignorance or non-possession of knowledge, and
+
+Adarsana, i.e., slack-belief or temporary lack of faith.
 		</div>
 			
 		<?php if(isMobile()){}{ ?>
