6a9ca780b123dcf57ab4cbeeceba4a502a6e93a3
Pankaj Jain <capankajjain@smilyo.com>
2016-03-25 15:34:57 +0530

    Update uttargun.php

--- uttargun.php
+++ uttargun.php
@@ -1,175 +1,99 @@
-<?php include('header.php'); ?>
+<?php
+include 'config.php';
+$title = 'Thirty Four Extra Virtues (Uttar Gun)';
+$alias = 'uttargun';
+?>
 
-<!-- start body -->
-<body onunload="" <?php echo $schemaOrgBody; ?>>
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title><?php echo $title; ?></title>
+		
+		<!-- Bootstrap Core CSS -->
+		<?php
+include 'bootstrap.php';
+include 'stylesheets.php';
+		?>
+		
+		
+		<!-- Custom Fonts -->
+		<?php include 'font-awesome.php'; ?>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:100,600' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+		<?php include 'login/login.php'; ?>
+		
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<script>
+document.createElement('video');
+</script>
+<![endif]-->
+		
+	</head>
+	
+	<body id="home">
+		<!-- Navigation -->
+		<?php
+$q = $db->prepare('SELECT * FROM articles WHERE title=?');
+$q->execute(array($title));
+if($q->rowCount() > 0) {
+	$row = $q->fetch(PDO::FETCH_ASSOC);
+	$content = $row['content'];
+}
 
-	<!-- start dotted pattern -->
-	<div class="bg-overlay"></div>
-	<!-- end dotted pattern -->
-	
-	<!-- start navigation -->
-	<?php include('menu.php'); ?>
-	<!-- end navigation -->
-	
-	<!-- start content wrapper -->
-	<div class="content page-content">
-	
-		<div class="page-title">
-			<h1>Thirty Four Extra Virtues (Uttar Gun)</h1>
-		</div>
+$navLinks = '<li><a href="/">Home</a></li>
+<li><a href="#team">'.$title.'</a></li>
+                                <li><a href="#contact">Contact us</a></li>';
+include 'nav.php';
+		?>
 		
-		<div class="divider clear"></div>
 		
-		<!-- start page content -->
-		<div class="">
-			<div>
-				There are a total of Thirty Four Extra Virtues of Digambar Muni, further classified into Six Outward (Bahirang Tap), Six Internal (antarang Tap) and Twenty Two (parishay jai).
-			</div><br /><br />
-			
-			<!-- Start List Items -->
-			<div>
+		<!-- Our team Section -->
+		<section id="team" class="team content-section">
+			<div class="container">
+				<div class="g-signin2" data-onsuccess="onSignIn"></div>
+				<div id="edit" class="hidden"><a class="btn btn-default btn-lg" href="articles/editor.php?title=<?php echo $title; ?>&alias=<?php echo $alias; ?>">Edit</a></div>
+				<div class="row text-center">
+					<div class="col-md-12">
+						<h2><?php echo $title; ?></h2>
+					</div><!-- /.col-md-12 -->
+				</div><!-- /.row -->
+				<div><?php echo $content; ?></div>
 				
-				<h2><u>SIX BAHIRANG TAP</u></h2><br />
-				<img class="floatRight" title="Five Mahavrat" width="300" src="https://jainsquare.files.wordpress.com/2011/03/680px-mahavratas.jpg" />
-				<ol>
-					<li>
-						<h3>ANSHAN</h3>
-						To purify the soul, to weaken the pride and evil thoughts and to control the senses voluntarily stop taking completely all type of food and liquid for a certain period.
-					</li><br /><li>
-						<h3>AVMODRYA</h3>
-						To win laziness, do meditation and study, eat food less than their hunger.
-					</li><br /><li>
-						<h3>VRATI PARISANKHYAN</h3>
-						To think about some condition silently and without telling anybody that it it will happen today then I will accept food otherwise not for example-special type of house or. some definite number of human beings or food or any type of event or thing.
-					</li><br /><li>
-						<h3>RAS TYAG</h3>
-						To have control on liking or thoughts of eating particular food of a particular taste and to limit sensory pride taking a vow not to eat one or two or more type of food belonging to following classes-sweet, salt, oils, Ghee milk and curd. These types are known as six ras.
-					</li><br /><li>
-						<h3>AIKANT SHAYYASAN</h3>
-						For undisturbed studies, meditation thinking and analysis etc. to live in lonely places where there is no man woman or eunuch.
-					</li><br /><li>
-						<h3>KAYA KLAISH</h3>
-						To realize self to sit in padamasan (lotus posture) or not to speak for a particular interval of time (Silence) or live near rivers or in mountains and thus bear the bodily discomforts keeping one self normal.
-					</li><br />
-				</ol>
-				<br class="clear" />
+				<!-- Facebook Comments Started -->
+				<hr>
+				<div id="fb-root"></div>
+				<div class="fb-like" data-href="http://jainmunilocator.org/ekadash-pratima.php" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+				<div class="fb-comments" data-href="http://jainmunilocator.org/ekadash-pratima.php" data-numposts="5"></div>
+				<!-- Facebook Comments Ended -->
 				
-				<h2><u>SIX ANTARANG TAP</u></h2><br />
-				<img class="floatRight" title="Five Mahavrat" width="300" src="https://jainsquare.files.wordpress.com/2011/03/680px-mahavratas.jpg" />
-				<ol>
-					<li>
-						<h3>PRAYASHCHIT</h3>
-						To repent on wrong thoughts and doings.
-					</li><br /><li>
-						<h3>VINAY</h3>
-						To behave respectfully towards virtues and virtuous.
-					</li><br /><li>
-						<h3>VAYA VRAT</h3>
-						To serve saints without any desire.
-					</li><br /><li>
-						<h3>SWADHYAI</h3>
-						To purify the soul. to remove the darkness of ignorance to check the tendency of iII will to self and others and to create the tendency of doing good to others study of true religious literature.
-					</li><br /><li>
-						<h3>VUTSARG</h3>
-						To finish attachment with body and other worldly things.
-					</li><br /><li>
-						<h3>DHYAN</h3>
-						To stabilize the thoughts in the study of inner self and analysis of six substances that is-Jiva, Pudgal [matter] Dharam [medium of motion of soul and matter] Adharam [medium of rest for soul and matter], Kai [time] and Akash [space]. After getting rid of of all ill instincts.
-					</li><br />
-				</ol>
-				<br class="clear" />
-				
-				<h2><u>TWENTY TWO PARISHA JAI (Victories of pinches)</u></h2><br />
-				<img class="floatRight" title="Five Mahavrat" width="300" src="https://jainsquare.files.wordpress.com/2011/03/680px-mahavratas.jpg" />
-				<ol>
-					<li>
-						<h3>SHUDHA JAI</h3>
-						To bear hunger keeping normal thoughts.
-					</li><br /><li>
-						<h3>PIPASA JAI</h3>
-						To bear thirst without uneasiness.
-					</li><br /><li>
-						<h3>SHEET JAI</h3>
-						To bear cold with equanimity.
-					</li><br /><li>
-						<h3>USHAN JAI</h3>
-						To tolerate heat with equanimity.
-					</li><br /><li>
-						<h3>DANSH MASAK JAi</h3>
-						To keep normal if mosquito etc. bite.
-					</li><br /><li>
-						<h3>NAGAN PARISH JAI</h3>
-						Not to have only pinch of evil thoughts or shame by being completely naked.
-					</li><br /><li>
-						<h3>ARATI JAI</h3>
-						Even in the face of dissatisfaction not to have dissatisfaction or aversion from the religion.
-					</li><br /><li>
-						<h3>STRI PARISHY JAI</h3>
-						Not to allow turbulence of' thoughts even by seeing young beautiful ladies.
-					</li><br /><li>
-						<h3>CHARYA PARISHY JAI</h3>
-						To be remor seless while walking on uneven. thorny Stanley ground.
-					</li><br /><li>
-						<h3>NISHIDHA PARISHY JAI</h3>
-						To bear the pain calmly which is caused by sitting or standing for long intervals during meditation or tapasya.
-					</li><br /><li>
-						<h3>SHAYYA PARISHY JAI</h3>
-						To bear pain calmly caused by sleeping on hard ground.
-					</li><br /><li>
-						<h3>AAKROSH PARISHY JAI</h3>
-						To tolerate calmly unparliamantry, fifthly and perching words use by others.
-					</li><br /><li>
-						<h3>BADH PARISHY JAI</h3>
-						To remain calm and quit at the time of murderer attack with sword or other arms etc. by others.
-					</li><br /><li>
-						<h3>YACHNA PARISHY JAI</h3>
-						Not to beg food or medicine even at the death bed.
-					</li><br /><li>
-						<h3>ALABH PARISHY JAI</h3>
-						In condition of not getting food for long intervals to consider this loss better than gain.
-					</li><br /><li>
-						<h3>ROG PARISHY JAI</h3>
-						To remain normal and firm while suffering with dreaded and painful disease.
-					</li><br /><li>
-						<h3>TREN SPARSH JAI</h3>
-						Remain call and normal in his thoughts if he gets pricked with thorn etc.
-					</li><br /><li>
-						<h3>MAL PARISHY JAI</h3>
-						To bear the inconvenience quietly if their body gets dusty etc.
-					</li><br /><li>
-						<h3>SATKAR PURSKAR PARISHY JAI-</h3>
-						Not to feel inferiority complex and let his thoughts be disturbed if others do not praise him though he is very learned austere and meditative.
-					</li><br /><li>
-						<h3>PARGGYA PARISHY JAI</h3>
-						Not to be proud of his intellect.
-					</li><br /><li>
-						<h3>AGAYAN PARISHY JAI</h3>
-						Not to be remorseful if he does not acquire much knowledge inspite of his best efforts.
-					</li><br /><li>
-						<h3>ADARSHAN PARISHY JAI</h3>
-						Even after tough and prolonged Tapasya (austerity) if he does not attain special knowledge and super natural powers then also he does not loose his faith in the path of Nirvana.
-					</li><br />
-				</ol>
-				<br class="clear" />
-				
-			</div>
-			<!-- end list items -->
-			
-			<br />
-			<hr>
-			
-			<!-- Facebook Comments Started -->
-			<div id="fb-root"></div>
-			<div class="fb-like" data-href="http://jainmunilocator.org/uttargun.php" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
-			<div class="fb-comments" data-href="http://jainmunilocator.org/uttargun.php" data-numposts="5"></div>
-			<!-- Facebook Comments Ended -->
-			
-			<br /><br /><br /><br /><br /><br /><br /><br /><br />
-			
-		</div>
-		<!-- end page content -->
+			</div><!-- /.container -->
+		</section><!-- /.our-team -->
 		
-	</div>
-	<!--  end content wrapper  -->
- 	
-<?php include('footer.php'); ?>
+		
+		
+		<!-- Contact section -->
+		<?php include 'contact.php'; ?>			
+		
+		<!-- Footer -->
+		<?php include 'footer2.php'; ?>		
+		
+		<!-- Facebook -->
+		<script type='text/javascript' src='js/facebook.js'></script>
+		
+		<?php
+include 'scripts.php';
+include 'login/loginscripts.php';
+		?>
+		
+	</body>
+	
+</html>
