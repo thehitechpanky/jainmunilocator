f9e52541249ef161bd716e474f76217d222977c4
Pankaj Jain <capankajjain@smilyo.com>
2016-03-25 16:53:35 +0530

    Update edit.php

--- articles/edit.php
+++ articles/edit.php
@@ -2,14 +2,15 @@
 
 include '../config.php';
 
+$id = $_POST['id'];
 $title = $_POST['title'];
 $alias = $_POST['alias'];
 $content = $_POST['content'];
 $author = $_POST['author'];
 
-$sqlarticles = "UPDATE articles SET content=?, author=? WHERE title=?";	
+$sqlarticles = "UPDATE articles SET title=?, content=?, author=? WHERE id=?";	
 $q = $db->prepare($sqlarticles);
-$q->execute(array($content,$author,$title));
+$q->execute(array($title,$content,$author,$id));
 
 header('location: ../'.$alias.'.php');
 
