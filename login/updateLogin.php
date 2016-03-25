4c1b46200c4bb9398b50e090a5ddf58314bcba80
Pankaj Jain <capankajjain@smilyo.com>
2016-03-25 20:32:24 +0530

    Update and rename updateLocation.php to updateLogin.php

--- login/updateLogin.php
+++ login/updateLogin.php
@@ -1,21 +1,15 @@
-<?php 
+<?php
 include '../config.php';
-include '../functions.php';
-
 $email = $_GET['email'];
 $username = $_GET['username'];
 $userimg = $_GET['userimg'];
-$userlat = $_GET['lat'];
-$userlng = $_GET['lng'];
-$userlocation = getaddress($userlat,$userlng);
-
+$userlat = 0;
+$userlng = 0;
+$userlocation = 'N/A';
 // fileds of editlog
-$usertimestamp = date("Y-m-d H:i:s", time());
 $userip = $_SERVER['REMOTE_ADDR'];
-
 $u = $db->prepare("SELECT * FROM user WHERE email=?");
 $u->execute(array($email));
-
 if($u->rowCount() == 1) {
 	$q = $db->prepare("UPDATE user SET username=?, userimg=?, userlat=?, userlng=?, userlocation=?, userip=? WHERE email=?");
 	$q->execute(array($username,$userimg,$userlat,$userlng,$userlocation,$userip,$email));
@@ -23,5 +17,4 @@ if($u->rowCount() == 1) {
 	$q = $db->prepare("INSERT INTO user (email,username,userimg,userlat,userlng,userlocation,userip) VALUES (?,?,?,?,?,?,?)");
 	$q->execute(array($email,$username,$userimg,$userlat,$userlng,$userlocation,$userip));
 }
-
 ?>
