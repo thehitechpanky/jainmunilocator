c1b83489a1726c7d5484e443be2e42d8b352eeb4
Pankaj Jain <capankajjain@smilyo.com>
2016-03-25 15:30:36 +0530

    Update mahagun.php

--- mahagun.php
+++ mahagun.php
@@ -1,174 +1,99 @@
-<?php include('header.php'); ?>
+<?php
+include 'config.php';
+$title = 'Twenty Eight Special Virtues';
+$alias = 'mahagun';
+?>
 
-<!-- start body -->
-<body onunload="" <?php echo $schemaOrgBody; ?>>
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title><?php echo $title; ?></title>
+		
+		<!-- Bootstrap Core CSS -->
+		<?php
+include 'bootstrap.php';
+include 'stylesheets.php';
+		?>
+		
+		
+		<!-- Custom Fonts -->
+		<?php include 'font-awesome.php'; ?>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:100,600' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+		<?php include 'login/login.php'; ?>
+		
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<script>
+document.createElement('video');
+</script>
+<![endif]-->
+		
+	</head>
+	
+	<body id="home">
+		<!-- Navigation -->
+		<?php
+$q = $db->prepare('SELECT * FROM articles WHERE title=?');
+$q->execute(array($title));
+if($q->rowCount() > 0) {
+	$row = $q->fetch(PDO::FETCH_ASSOC);
+	$content = $row['content'];
+}
 
-	<!-- start dotted pattern -->
-	<div class="bg-overlay"></div>
-	<!-- end dotted pattern -->
-	
-	<!-- start navigation -->
-	<?php include('menu.php'); ?>
-	<!-- end navigation -->
-	
-	<!-- start content wrapper -->
-	<div class="content page-content">
-	
-		<div class="page-title">
-			<h1>Twenty Eight Special Virtues (महागुण)</h1>
-		</div>
+$navLinks = '<li><a href="/">Home</a></li>
+<li><a href="#team">'.$title.'</a></li>
+                                <li><a href="#contact">Contact us</a></li>';
+include 'nav.php';
+		?>
 		
-		<div class="divider clear"></div>
 		
-		<!-- start page content -->
-		<div class="">
-			<div>
-				Digambar Jain Sadhus take a vow to observe Twenty Eight Special Virtues (महागुण) at the time of their induction into the saintly order and remain vigilant to observe these without slightest deviation through out their life.
-			</div>
-			<div>
-				These twenty eight special virtues have been divided into Five Great Vows (पंच-महाव्रत), Five Limits / Care (पंच-समिति), Victory over Five Senses (पंच-इन्द्रिय-विजय), Six Essential Duties (षट्-आवश्यक) and Seven Specialties (सप्त विशेष गुण).
-			</div><br /><br />
-			
-			<!-- Start List Items -->
-			<div>
+		<!-- Our team Section -->
+		<section id="team" class="team content-section">
+			<div class="container">
+				<div class="g-signin2" data-onsuccess="onSignIn"></div>
+				<div id="edit" class="hidden"><a class="btn btn-default btn-lg" href="articles/editor.php?title=<?php echo $title; ?>&alias=<?php echo $alias; ?>">Edit</a></div>
+				<div class="row text-center">
+					<div class="col-md-12">
+						<h2><?php echo $title; ?></h2>
+					</div><!-- /.col-md-12 -->
+				</div><!-- /.row -->
+				<div><?php echo $content; ?></div>
 				
-				<h2><u>Five Special Vows (पंच-महाव्रत)</u></h2><br />
-				<img class="floatRight" title="Five Mahavrat" width="300" src="https://jainsquare.files.wordpress.com/2011/03/680px-mahavratas.jpg" />
-				<ol>
-					<li>
-						<h3>Vow of Non Violence (अहिंसा-व्रत)</h3>
-						To cause slightest mental or bodily pain to any microscopic or macro living creature that is a cellular or higher life including plants, insects, human being, animals, birds etc. with their thoughts, speech and body. To have sympathy on all living ones taking them to be equal to his own soul.
-					</li><br /><li>
-						<h3>Vow of Truth (सत्य-व्रत)</h3>
-						Not to speak words which are false, harsh, hurting and against the interest of self and others. To speak sweet, helpful for self and others according to religious sanction.
-					</li><br /><li>
-						<h3>Vow to Never Steal (अचौर्य-व्रत)</h3>
-						Neither to take or help others to take or praise others taking the physical belongings of others without having their clear permission by mind speech and body.
-					</li><br /><li>
-						<h3>Vow of Bramcharya (भ्रमचर्य-व्रत)</h3>
-						Absolutely free from any type of sexual act with any woman, of the world in the three ways that is thoughts, words, physical acts and remain completely engrossed in his own soul.
-					</li><br /><li>
-						<h3>Vow of Renunciation (अपरिग्रह-व्रत)</h3>
-						To completely leave internal parigrah that is wrath, pride, wickedness, greed etc and external parigrah that is each and every type of attachment of physical articles including money etc.
-					</li><br />
-				</ol>
-				<br class="clear" />
+				<!-- Facebook Comments Started -->
+				<hr>
+				<div id="fb-root"></div>
+				<div class="fb-like" data-href="http://jainmunilocator.org/ekadash-pratima.php" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+				<div class="fb-comments" data-href="http://jainmunilocator.org/ekadash-pratima.php" data-numposts="5"></div>
+				<!-- Facebook Comments Ended -->
 				
-				<h2><u>Five Limits / Care (पंच-समिति)</u></h2><br />
-				<img class="floatRight" title="Five Mahavrat" width="300" src="https://jainsquare.files.wordpress.com/2011/03/680px-mahavratas.jpg" />
-				<ol>
-					<li>
-						<h3>Limit in movement (इर्या-समिति)</h3>
-						To walk cautiously seeing the foreground so that no living creature however small it may be killed or hurt.
-					</li><br /><li>
-						<h3>Limit in speech (भाषा-समिति)</h3>
-						To speak sweet simple, soft and helpful only in essential words according to relegious scriptures.
-					</li><br /><li>
-						<h3>Limit of articles (ईशान-समिति)</h3>
-						Without causing any sort of trouble to others cat simple, pure, hygienic, perfectly vegetarian food only once in day time in twenty four hours to tread the path of religion.
-					</li><br /><li>
-						<h3>Limit in respect of receiving and keeping the necessary things (आदान-निक्षेपण-समिति)</h3>
-						Carefully lie down or sit down or remove and put down things keeping defense of any creatures in view.
-					</li><br /><li>
-						<h3>Limit in respect of throwing away articles (परिष्टपणिका-समिति)</h3>
-						To remain vigilant while making excretions (passing out urine, excreta etc). That no insect is killed or troubled.
-					</li><br />
-				</ol>
-				<br class="clear" />
-				
-				<h2><u>Victory over Five Senses (पंच-इन्द्रिय-विजय)</u></h2><br />
-				<img class="floatRight" title="Five Mahavrat" width="300" src="http://ww2.valdosta.edu/~erbrantl/five%20senses.gif" />
-				<ol>
-					<li>
-						<h3>Victory of Sense of Touch (स्पर्शन-इन्द्रिय-विजय)</h3>
-						To discard sense of joy and sorrow created or felt by touch of the body.
-					</li><br /><li>
-						<h3>Victory of Sense of Taste (रसना-इन्द्रिय-विजय)</h3>
-						Not to have liking or disliking for things having different taste like sweet, Sour, salty, spicy  etc.
-					</li><br /><li>
-						<h3>Victory of Sense of Smell (घ्राण-इन्द्रिय-विजय)</h3>
-						Not to have desire for fragrance or dislike for foul odour.
-					</li><br /><li>
-						<h3>Victory of Sense of Sight (चक्षु-इन्द्रिय-विजय)</h3>
-						To leave completely any sense of Joy or sorrow by seeing beautiful or ugly things or scenes.
-					</li><br /><li>
-						<h3>Victory of Sense of Hearing (कर्ण-इन्द्रिय-विजय)</h3>
-						Not to feel pleasure or irritation due to melodious sounds, discordant notes, praise and abusive Language etc.
-					</li><br />
-				</ol>
-				<br class="clear" />
-				
-				<h2><u>Six Essential Duties (षट्-आवश्यक)</u></h2><br />
-				<img class="floatRight" title="Five Mahavrat" width="300" src="https://jainsquare.files.wordpress.com/2011/03/680px-mahavratas.jpg" />
-				<ol>
-					<li>
-						<h3>SAMTA</h3>
-						Without having an emotion good or bad to keep equanimity, that is medium state of thoughts  in all either friend or enemy, praise or insult, comfort or discomfort, gain or loss birth or death, respect or insult etc.
-					</li><br /><li>
-						<h3>VANDANA</h3>
-						To worship and sing in praises of God or one venerable saint.
-					</li><br /><li>
-						<h3>STUTI</h3>
-						To worship and praise collectively for a number of saints or Mahapurshas (Super human being) gods.
-					</li><br /><li>
-						<h3>PRATI KRAMAN</h3>
-						To make efforts and repent over his own past mistakes or acts which are not proper or forbidden.
-					</li><br /><li>
-						<h3>PRATYAKHYAN</h3>
-						To make efforts to save himself from future sins and mistakes.
-					</li><br /><li>
-						<h3>KAYAUTSARG</h3>
-						To leave the love and care of his own body to perform acts of hardship like Tapasya, fast, expose to cold heat and rains etc calmly.
-					</li><br />
-				</ol>
-				<br class="clear" />
-				
-				<h2><u>Seven Specialities (सप्त विशेष गुण)</u></h2><br />
-				<img class="floatRight" title="Five Mahavrat" width="300" src="https://jainsquare.files.wordpress.com/2011/03/680px-mahavratas.jpg" />
-				<ol>
-					<li>
-						<h3>ACHAILAKTAV (NAKEDNESS)</h3>
-						To discard all cloths and coverings to attain simple, natural, emotionless  etc. child like state.
-					</li><br /><li>
-						<h3>ASNAN</h3>
-						Not to bath his body with water, only to have the bath of mantra (spiritual words) vow. air and sun rays etc.
-					</li><br /><li>
-						<h3>DANT DHOVAN</h3>
-						Not to shine his teeth with brush or any powder etc.
-					</li><br /><li>
-						<h3>KESH LONCH</h3>
-						TO pull out hairs of head; board and pair of moustaches with hands in two, three or four months once.
-					</li><br /><li>
-						<h3>BHOOMI SHAYAN</h3>
-						TO sleep for little time on clean that is having no ants or insects etc. earth, stone sleep, mat or dry grass etc. without any aid like pillow. cushion, any cloth or bad etc. for removing fatigue.
-					</li><br /><li>
-						<h3>AK BHUKTI</h3>
-						They eat once In day time only in twenty four hours simple and hygienic food without any of the thirty two antrai (disturbances) mentioned as below-If they find hair, leather (dead skin) dead insect or any other creature, meat bone, Nail etc. in their food while eating then they at once leave the food without any ill feeling and go away. If they happen to saw raw meat, dying creature, wine, raging fire etc, then they take it as antrai and leave food. While they are going to have their food and they happen to see corpse, dead animals having all the five senses excreta of some bird falls on their body, if some carnivorous animal, some drunkard or meat eater man, chandal (executioner), or a Lady having menses touches their body then they return back without taking their food on that day At the time of taking their food if they happen to hear pathetic words or sound of loudly weeping or fighting then they assume it as anrai. Moreover if person offering food to them falls or some utensil falls, either lamp or fire gets extinguished, raw seed (having power of germination) gets into their stomach. appearance of ascaris or pin or hook worms etc. then they take it also as antrai.
-					</li><br /><li>
-						<h3>STITH I BHOJAN</h3>
-						Saints do not eat their food sitting in any utensil but instead they stand keeping a distance of about ten centimeters between their feet, without having evil thought, observing perfect silence, without making any indication of yes or no, without having any complex of begging or humbleness they accept their food solid or liquid in their palms only and eat, They always eat less than their hunger.
-					</li><br />
-				</ol>
-				<br class="clear" />
-				
-			</div>
-			<!-- end list items -->
-			
-			<br />
-			<hr>
-			
-			<!-- Facebook Comments Started -->
-			<div id="fb-root"></div>
-			<div class="fb-like" data-href="http://jainmunilocator.org/mahagun.php" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
-			<div class="fb-comments" data-href="http://jainmunilocator.org/mahagun.php" data-numposts="5"></div>
-			<!-- Facebook Comments Ended -->
-			
-			<br /><br /><br /><br /><br /><br /><br /><br /><br />
-			
-		</div>
-		<!-- end page content -->
+			</div><!-- /.container -->
+		</section><!-- /.our-team -->
 		
-	</div>
-	<!--  end content wrapper  -->
- 	
-<?php include('footer.php'); ?>
+		
+		
+		<!-- Contact section -->
+		<?php include 'contact.php'; ?>			
+		
+		<!-- Footer -->
+		<?php include 'footer2.php'; ?>		
+		
+		<!-- Facebook -->
+		<script type='text/javascript' src='js/facebook.js'></script>
+		
+		<?php
+include 'scripts.php';
+include 'login/loginscripts.php';
+		?>
+		
+	</body>
+	
+</html>
