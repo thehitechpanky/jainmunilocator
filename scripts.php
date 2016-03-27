90e9c8dd1809ebe9a25c24889afffc88091a7010
Pankaj Jain <capankajjain@smilyo.com>
2016-03-27 13:34:41 +0530

    Create scripts.php

---
+++ scripts.php
@@ -0,0 +1,28 @@
+<!-- jQuery -->
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<!-- Plugin JavaScript -->
+<script src="../assets/js/plugins/wow.min.js"></script>
+<script src="../assets/js/plugins/owl.carousel.min.js"></script>
+<script src="../assets/js/plugins/jquery.parallax-1.1.3.js"></script>
+<script src="../assets/js/plugins/jquery.magnific-popup.min.js"></script>
+<script src="../assets/js/plugins/jquery.mb.YTPlayer.min.js"></script>
+<script src="../assets/js/plugins/jquery.countTo.js"></script>
+<script src="../assets/js/plugins/jquery.inview.min.js"></script>
+<script src="../assets/js/plugins/pace.min.js"></script>
+<script src="../assets/js/plugins/jquery.easing.min.js"></script>
+<script src="../assets/js/plugins/jquery.validate.min.js"></script>
+<script src="../assets/js/plugins/additional-methods.min.js"></script> 
+
+<!-- Custom JavaScript -->
+<script src="../assets/js/hallooou.js"></script>
+
+<?php
+include 'analyticstracking.php';
+include 'login/loginscripts.php';
+?>
