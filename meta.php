3231468d7b1e4ec323396ac580ee862fe10771b2
Pankaj Jain <capankajjain@smilyo.com>
2016-03-27 13:32:21 +0530

    Create meta.php

---
+++ meta.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php include 'login/login.php'; ?>
