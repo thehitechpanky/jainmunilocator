343a646951d6509663c49cfcb32992b695284478
Pankaj Jain <capankajjain@smilyo.com>
2016-03-27 13:33:23 +0530

    Create stylesheets.php

---
+++ stylesheets.php
@@ -0,0 +1,29 @@
+<!-- Bootstrap Core CSS -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Main stylesheet -->
+<link href="../assets/css/hallooou.css" rel="stylesheet">
+
+<!-- Color stylesheet -->
+<!-- <link href="assets/css/colors.css" rel="stylesheet"> -->
+<!-- <link href="assets/css/colors_2.css" rel="stylesheet"> -->
+<!-- <link href="assets/css/colors_3.css" rel="stylesheet"> -->
+<!-- <link href="assets/css/colors_4.css" rel="stylesheet"> -->
+
+
+<!-- Plugin stylesheets -->
+<link href="../assets/css/plugins/owl.carousel.css" rel="stylesheet">
+<link href="../assets/css/plugins/owl.theme.css" rel="stylesheet">
+<link href="../assets/css/plugins/owl.transitions.css" rel="stylesheet">
+<link href="../assets/css/plugins/animate.css" rel="stylesheet">
+<link href="../assets/css/plugins/magnific-popup.css" rel="stylesheet">
+<link href="../assets/css/plugins/jquery.mb.YTPlayer.min.css" rel="stylesheet">
+<link href="../css/custom.css" rel="stylesheet" type="text/css" media="screen" />
+
+<!-- Custom Fonts -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link href='http://fonts.googleapis.com/css?family=Raleway:100,600' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
