8460199ea01af0cb65ab09c621600b68c66f568c
Pankaj Jain <capankajjain@smilyo.com>
2016-03-27 12:10:11 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,100 +1,108 @@
-<?php include('header.php'); ?>
-
-<!-- start body -->
-<body onunload="" >
-
-	<!-- start dotted pattern -->
-	<div class="bg-overlay"></div>
-	<!-- end dotted pattern -->
-	
-	<!-- start navigation -->
-	<?php include('menu.php'); ?>
-	<!-- end navigation -->
-	
-	<!-- start content wrapper -->
-	
-	<div class="content page-content">
-	
-		<div class="page-title">
-			<h1>Feedback</h1>
-		</div>
-		
-		<div class="divider clear"></div>
-		
-		<div class="inner-content">	
-			<?php
-			$name = "";
-			$email = "";
-			$msg = "";
-			if(isset($_POST['submit']))
-			{
-			$name = $_POST['name'];
-			$msg = $_POST['message'];
-			$email = $_POST['email'];
-				$url = "https://www.google.com/recaptcha/api/siteverify?secret=6LcXYP8SAAAAAH7WUPMtiHoEiTnev6ofbzsuRY4U&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR'];
-			$text = file_get_contents($url);
-		if(strpos($text,'true') && $name != "" && $email != "" && $msg != "")
-		{
-			echo 'Email has been sent. We will contact you back shortly';
-			mail("capankajjain@smilyo.com","Jain Muni - Email From ".$email,$msg." - ".$name."(".$email.")");
-		}
-		else
-		{
-			echo 'Please check and fill the form again';
-		}
-			}
-			?>
-			<!-- start feedback form -->
-			<form method="post" class="contactForm" id="contactForm" action="">
-				<fieldset>
-					<div>
-						<label>Name*</label>
-						<input type="text" value="<?php echo $name ?>" id="name" name="name" class="name validate[required,custom[onlyLetterNumber],maxSize[20]]">
-					</div>
-					
-					<div>
-						<label>Email*</label>
-						<input type="text" value="<?php echo $email ?>" id="email" name="email" class="email  validate[required,custom[email]]">
-					</div>
-					
-					<div>					
-						<label>Message*</label><textarea id="mess" name="message" class="message validate[requied,length[6,3000]]"><?php echo $msg ?></textarea>
-					</div>
-					<div>
-					<div class="g-recaptcha" data-sitekey="6LcXYP8SAAAAAM8199rOJV8yoCWS4mI5FHb5Q70Q"></div>
-					</div><br />
-					<p class="sent-result" style="display:none;">Your message sent successfully!</p>
-					
-					<div class="send-wrap">
-						<input type="submit" name="submit" value="Send Email">
-					</div>
-				</fieldset>
-				
-				
-			</form>					
-			<!-- end feedback form -->	
-	
-		</div>
-		
-		<!-- start widgets -->		 
-		<div class="sidebar">
-			<div id="text-2" class="widget widget_text">
-				<h3>Contact Us</h3>			
-				<div class="textwidget">Contact us to give us suggestions, report any wrong information or any other thing. Please use the contact form and we will contact you back shortly.
-					<br><br>
-					<strong>
-						Any address we would like to give<br />
-						9818609955 <br />
-						capankajjain@smilyo.com
-					</strong>
-				</div>
-			</div>
+<!-- Counter Section -->
+<section id="counter" class="counter-section content-section">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-md-12">
+				<h2 class="white">Stats so far</h2>
+				<h3 class="caption">The count is as follows and growing</h3>
+			</div><!-- /.col-md-12 -->
 			
-		
-		</div>
-		<!-- end widgets -->
-	</div>
+			<div class="col-sm-3 counter-wrap wow fadeInUp" data-wow-offset="10">
+				<strong><span class="timer">71</span>+</strong>
+				<span class="count-description">Acharya, Ailacharya &amp; Upadhyay</span>
+			</div><!-- /.col-sm-3 -->
+			
+			<div class="col-sm-3 counter-wrap wow fadeInUp" data-wow-offset="10">
+				<strong><span class="timer">172</span>+</strong>
+				<span class="count-description">Muni</span>
+			</div><!-- /.col-sm-3 -->
+			
+			<div class="col-sm-3 counter-wrap wow fadeInUp" data-wow-offset="10">
+				<strong><span class="timer">129</span>+</strong>
+				<span class="count-description">Aryika</span>
+			</div><!-- /.col-sm-3 -->
+			
+			<div class="col-sm-3 counter-wrap wow fadeInUp" data-wow-offset="10">
+				<strong><span class="timer">13</span>+</strong>
+				<span class="count-description">Ailak &amp; Kshullak</span>
+			</div><!-- /.col-sm-3 -->
+			
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</section><!-- /.counter-section -->
+
+
+<!-- Call to action - two section -->
+<section class="cta-two-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-9">
+				<h3>Have an idea?</h3>
+				<p>Let us know what we should do more...</p>
+			</div>
+			<div class="col-sm-3">
+				<a href="https://docs.google.com/forms/d/1sYAbfHaI6MheZWN0EKeVkiqyWD17-0RsSNJ9vEIHRK0/viewform" class="btn btn-overcolor">Get in touch</a>
+			</div>
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</section><!-- /.cta-two-section -->
+
+
+<section id="contact" class="contact content-section no-bottom-pad wow fadeIn" data-wow-offset="10">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-md-12">
+				<h2>Contact</h2>
+				<h3 class="caption gray">Feel free to get in touch with us if you have a new project or simply something awesome</h3>
+			</div><!-- /.col-md-12 -->
+			
+		</div><!-- /.row -->
+	</div><!-- /.container -->
 	
-	<!--  end content wrapper  -->
- 	
-<?php include('footer.php'); ?>
+	<div class="container">
+		<div class="row form-container">
+			
+			<div class="col-md-8 contact-form">
+				<h3>Drop us a line</h3>
+				<form class="ajax-form" id="contactForm" method="post" action="assets/php/contact.php">
+					<div class="form-group">
+						<input type="text" class="form-control" id="name" name="name" placeholder="Your Name..." value="" required>
+					</div>
+					<div class="form-group">
+						<input type="email" class="form-control" id="email" name="email" placeholder="Your email..." value="" required>
+					</div>
+					<div class="form-group">
+						<input type="phone" class="form-control" id="phone" name="phone" placeholder="Your phone..." value="" required>
+					</div>
+					<div class="form-group">
+						<textarea class="form-control" rows="4" name="message" placeholder="Your message..." required></textarea>
+					</div>
+					<div class="form-group">
+						<button type="submit" name="submit" class="btn btn-default"><i class="fa fa-paper-plane fa-fw"></i> Send</button>
+					</div>
+				</form>
+			</div><!-- /.contact-form -->
+			
+			<div class="col-md-4 contact-address">
+				<h3>Administrator</h3>
+				<div class="team-member wow fadeIn" data-wow-offset="10">
+					<figure>
+						<img src="assets/images/pankaj-jain.jpg" alt="Pankaj Jain" class="img-responsive">
+						<figcaption>
+							<center>
+								<h1>Pankaj Jain</h1>
+								<p><i class="fa fa-envelope fa-fw"></i>&nbsp; capankajjain@smilyo.com</p>
+								<p><i class="fa fa-phone fa-fw"></i>&nbsp; +91 9818609955</p>
+								<ul>
+									<li><a href="https://www.facebook.com/thehitechpanky"><i class="fa fa-facebook fa-2x"></i></a></li>
+									<li><a href="https://twitter.com/thehitechpanky"><i class="fa fa-twitter fa-2x"></i></a></li>
+									<li><a href="https://www.linkedin.com/in/thehitechpanky"><i class="fa fa-linkedin fa-2x"></i></a></li>
+								</ul>
+							</center>
+						</figcaption>
+					</figure>
+				</div><!-- /.team-member -->
+			</div><!-- /.contact-address -->
+			
+		</div><!-- /.row -->
+	</div><!-- /.container -->
