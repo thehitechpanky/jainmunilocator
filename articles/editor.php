285cc61670384719ebded27d1d51d4dfc5ceb0f6
Pankaj Jain <capankajjain@smilyo.com>
2016-03-28 10:07:44 +0530

    Update editor.php

--- articles/editor.php
+++ articles/editor.php
@@ -18,32 +18,10 @@ if($q->rowCount() == 1) {
 <html lang="en">
 	
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		
+		<?php include '../meta.php'; ?>
 		<title><?php $title; ?></title>
 		
-		<!-- Bootstrap Core CSS -->
-		<?php include '../bootstrap.php'; ?>
-		
-		<?php include '../stylesheets.php'; ?>		
-		
-		<!-- Custom Fonts -->
-		<?php include '../font-awesome.php'; ?>
-		<link href='http://fonts.googleapis.com/css?family=Raleway:100,600' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
-		<?php include '../login/login.php'; ?>
-		
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<script>
-document.createElement('video');
-</script>
-<![endif]-->
+		<?php include '../stylesheets.php'; ?>
 		
 		<script src="//cdn.ckeditor.com/4.5.7/full/ckeditor.js"></script>
 		
@@ -81,19 +59,11 @@ include '../nav.php';
 			</div><!-- /.container -->
 		</section><!-- /.our-team -->
 		
-		
-		
-		<!-- Contact section -->
+
 		<?php
 include '../contact.php';
 include '../footer2.php';
-		?>		
-		
-		<!-- Facebook -->
-		<script type='text/javascript' src='js/facebook.js'></script>
-		<?php
 include '../scripts.php';
-include '../login/loginscripts.php';
 		?>
 		<script>
 			// Replace the <textarea id="editor1"> with a CKEditor
