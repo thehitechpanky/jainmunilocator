051eb9fb90c4c328cb969c68401dd140d41e1021
Pankaj Jain <capankajjain@smilyo.com>
2016-03-28 10:09:14 +0530

    Update edit.php

--- articles/edit.php
+++ articles/edit.php
@@ -1,6 +1,10 @@
 <?php
 
 include '../config.php';
+include '../meta.php';
+
+//setting character set
+mysql_set_charset('utf8');
 
 $id = $_POST['id'];
 $title = $_POST['title'];
@@ -11,6 +15,10 @@ $author = $_POST['author'];
 $sqlarticles = "UPDATE articles SET title=?, content=?, author=? WHERE id=?";	
 $q = $db->prepare($sqlarticles);
 $q->execute(array($title,$content,$author,$id));
+
+$sqlarticlelog = "INSERT INTO articlelog (id, title, content, author) VALUES (?,?,?,?)";
+$q = $db->prepare($sqlarticlelog);
+$q->execute(array($id,$title,$content,$author));
 
 header('location: ../'.$alias.'.php');
 
