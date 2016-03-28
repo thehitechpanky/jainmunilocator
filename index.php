192fedba722e6515635e150c1d2c439dc2e7efa3
Pankaj Jain <capankajjain@smilyo.com>
2016-03-28 10:10:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,48 +2,13 @@
 <html lang="en">
 	
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php include 'meta.php'; ?>
 		
 		<title>Jain Muni Locator - Home</title>
 		
-		<!-- Bootstrap Core CSS -->
-		<?php include 'bootstrap.php'; ?>
-		
-		<!-- Main stylesheet -->
-		<link href="assets/css/hallooou.css" rel="stylesheet">
-		
-		<!-- Color stylesheet -->
-		<!-- <link href="assets/css/colors.css" rel="stylesheet"> -->
-		<!-- <link href="assets/css/colors_2.css" rel="stylesheet"> -->
-		<!-- <link href="assets/css/colors_3.css" rel="stylesheet"> -->
-		<!-- <link href="assets/css/colors_4.css" rel="stylesheet"> -->
-		
-		
-		<!-- Plugin stylesheets -->
-		<link href="assets/css/plugins/owl.carousel.css" rel="stylesheet">
-		<link href="assets/css/plugins/owl.theme.css" rel="stylesheet">
-		<link href="assets/css/plugins/owl.transitions.css" rel="stylesheet">
-		<link href="assets/css/plugins/animate.css" rel="stylesheet">
-		<link href="assets/css/plugins/magnific-popup.css" rel="stylesheet">
-		<link href="assets/css/plugins/jquery.mb.YTPlayer.min.css" rel="stylesheet">
-		
-		
-		<!-- Custom Fonts -->
-		<?php include 'font-awesome.php'; ?>
-		<link href='http://fonts.googleapis.com/css?family=Raleway:100,600' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
-		
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<script>
-document.createElement('video');
-</script>
-<![endif]-->
+		<?php
+include 'stylesheets.php';
+		?>
 		
 	</head>
 	
@@ -72,7 +37,7 @@ include 'nav.php';
 			<div class="carousel-inner">
 				<div class="item active">
 					<!-- Set the first background image using inline CSS below. -->
-					<div class="fill" style="background-image:url('assets/images/cover-one.jpg');"></div>
+					<div class="fill" style="background-image:url('assets/images/mataji.jpg');"></div>
 					<div class="carousel-caption">
 						<h1 class="wow animated slideInDown">Jain Muni <span class="highlight">Locator</span></h1>
 						<p class="intro-text wow animated slideInUp">One-stop online destination for Digambara Jains. Know about Jainism and its glory.</p>
@@ -83,7 +48,7 @@ include 'nav.php';
 				
 				<div class="item">
 					<!-- Set the second background image using inline CSS below. -->
-					<div class="fill" style="background-image:url('assets/images/cover-two.jpg');"></div>
+					<div class="fill" style="background-image:url('assets/images/vidyasagar.jpg');"></div>
 					<div class="carousel-caption">
 						<h1 class="wow animated slideInDown"><span class="highlight">Monks &amp; Nuns</span> Biography</h1>
 						<p class="intro-text wow animated slideInUp">Find out about the life events and disciples of Digambara Monks &amp; Nuns.</p>
@@ -94,9 +59,9 @@ include 'nav.php';
 				
 				<div class="item">
 					<!-- Set the second background image using inline CSS below. -->
-					<div class="fill" style="background-image:url('assets/images/cover-three.jpg');"></div>
+					<div class="fill" style="background-image:url('assets/images/muni.jpg');"></div>
 					<div class="carousel-caption">
-						<h1 class="wow animated slideInDown">Jain Muni Locator <span class="highlight">Facebook</span> &amp; Page</h1>
+						<h1 class="wow animated slideInDown">Jain Muni Locator <span class="highlight">Facebook</span> Page</h1>
 						<p class="intro-text wow animated slideInUp">Get up to date information about Digambara monks &amp; Nuns and our initiatives.</p>
 						<a href="https://www.facebook.com/jainmunilocator/" class="btn btn-default btn-lg">Go to Facebook</a>
 					</div>
@@ -123,7 +88,7 @@ include 'nav.php';
 					<div class="col-md-6">
 						<h2>Excited about Jain Muni Locator</h2>
 						<h3 class="caption gray">Guess where does our information come from?</h3>
-						<p>That's right, it's "YOU". You are the one who have contributed in building this website. It's you who let us know, where munishri currently is.</p>
+						<p>That's right, it's "YOU". You are the one who have contributed in building this website. It's you who let us know, where monks &amp; currently are.</p>
 						
 						<blockquote>
 							There is no enemy out of your soul. The real enemies live inside yourself, they are anger, proud, curvedness, greed, attachmentes and hate.
@@ -140,6 +105,10 @@ include 'nav.php';
 								<a href="https://www.facebook.com/jainmunilocator">Jain Muni Locator</a>
 								</blockquote></div>
 						</div>
+					</div><!-- /.col-md-6 -->
+					
+					<div class="col-md-6">
+						<?php include 'adsense.php'; ?>
 					</div><!-- /.col-md-6 -->
 				</div><!-- /.row -->
 			</div><!-- /.container -->
@@ -178,7 +147,7 @@ include 'nav.php';
 								</div><!-- /.col-md-4 -->
 							</a>
 							
-							<a href="tirthankara.php">
+							<a href="">
 								<div class="col-md-4">
 									<div class="row services-item text-center wow flipInX" data-wow-offset="10">
 										<i class="fa fa-street-view fa-3x"></i>
@@ -308,261 +277,15 @@ include 'nav.php';
 					
 				</div><!-- /.row -->
 			</div><!-- /.container -->
-		</section><!-- /.products -->		
+		</section><!-- /.products -->
 		
+		<?php
+include 'goodreads.php';		
+include 'contact.php';
+include 'footer2.php';
+include 'scripts.php';
+		?>
 		
-		
-		
-		<!-- Portfolio/Gallery Section -->
-		<section id="portfolio" class="portfolio content-section">
-			<div class="container">
-				<div class="row text-center">
-					<div class="col-md-12">
-						<h2>Good Reads</h2>
-						<h3 class="caption white">We have collected some information and created some articles for you. Worth giving a min! </h3>
-					</div><!-- /.col-md-12 -->
-				</div><!-- /.row -->
-			</div><!-- /.container -->
-			
-			<div class="container project-container text-center">
-				<div class="recent-project-carousel owl-carousel owl-theme popup-gallery">
-					<div class="item recent-project wow fadeIn" data-wow-offset="10">
-						<img src="assets/images/gallery/project-one.jpg" alt="">
-						<div class="project-info">
-							<h3>Meaning of 108</h3>
-							<ul class="project-meta">
-								<li><a href="meaning-of-108.php">Read More</a></li>
-							</ul>
-						</div><!-- /.project-info -->
-						<div class="full-project">
-							<a href="meaning-of-108.php" title="Meaning of 108">Read more <i class="fa fa-chevron-right"></i></a>
-						</div><!-- /.full-project -->
-					</div><!-- /.item -->
-					
-					<div class="item recent-project wow fadeIn" data-wow-offset="10">
-						<img src="assets/images/gallery/project-two.jpg" alt="">
-						<div class="project-info">
-							<h3>28 Mahagun</h3>
-							<ul class="project-meta">
-								<li><a href="mahagun.php">Read more</a></li>
-							</ul>
-						</div><!-- /.project-info -->
-						<div class="full-project">
-							<a href="mahagun.php" title="28 Mahagun">Read more <i class="fa fa-chevron-right"></i></a>
-						</div><!-- /.full-project -->
-					</div><!-- /.item -->
-					
-					<div class="item recent-project wow fadeIn" data-wow-offset="10">
-						<img src="assets/images/gallery/project-three.jpg" alt="">
-						<div class="project-info">
-							<h3>34 Uttargun</h3>
-							<ul class="project-meta">
-								<li><a href="uttargun.php">Read more</a></li>
-							</ul>
-						</div><!-- /.project-info -->
-						<div class="full-project">
-							<a href="uttargun.php" title="34 Uttargun">Read more <i class="fa fa-chevron-right"></i></a>
-						</div><!-- /.full-project -->
-					</div><!-- /.item -->
-					
-					<div class="item recent-project wow fadeIn" data-wow-offset="10">
-						<img src="assets/images/gallery/project-four.jpg" alt="">
-						<div class="project-info">
-							<h3>11 Pratima</h3>
-							<ul class="project-meta">
-								<li><a href="ekadash-pratima.php">Read more</a></li>
-							</ul>
-						</div><!-- /.project-info -->
-						<div class="full-project">
-							<a href="ekadash-pratima.php" title="11 Pratima">Read more <i class="fa fa-chevron-right"></i></a>
-						</div><!-- /.full-project -->
-					</div><!-- /.item -->
-					
-				</div><!-- /.recent-project-carousel -->
-				
-				<div class="customNavigation project-navigation text-center">
-					<a class="btn-prev"><i class="fa fa-angle-left fa-2x"></i></a>
-					<a class="btn-next"><i class="fa fa-angle-right fa-2x"></i></a>
-				</div><!-- /.project-navigation -->
-				
-			</div><!-- /.container -->
-		</section><!-- /.portfolio -->
-		
-		
-		
-		
-		<!-- Testimonials Section -->
-		<section class="testimonials content-section wow fadeIn" data-wow-offset="10">
-			<div class="container">
-				<div class="row text-center">
-					<div class="col-md-12">
-						<h2>Rated 4.8 out of 5</h2>
-						<h3 class="caption gray">Here's what our clients have to say</h3>
-					</div><!-- /.col-md-12 -->
-					
-					<div class="container">
-						<div class="row">
-							<div class="col-md-8 col-md-offset-2">
-								<div class="client-testimonials owl-carousel owl-theme">
-									
-									<div class="item">
-										<p class="speech">Hallooou is the best HTML5 template we have EVER purchased. It's the perfect solution for our business. The unlimited customization helps us get things done quickly and efficiently.</p>
-										<div class="client-info">
-											<img src="assets/images/gina_stone-client_pic.jpg" />
-											<h4>Gina Stone</h4>
-											<span>Adam Productions</span>
-										</div>
-									</div><!-- /.item -->
-									
-									<div class="item">
-										<p class="speech">This is the real deal! Very easy to use. Thanks to Hallooou HTML5 template, we've just launched our new website! I couldn't have asked for more than this. Definitely worth the investment.</p>
-										<div class="client-info">
-											<img src="assets/images/john_doe-client_pic.jpg" />
-											<h4>John Doe</h4>
-											<span>Doe Associates</span>
-										</div>
-									</div><!-- /.item -->
-									
-									<div class="item">
-										<p class="speech">We're loving it. The best on the net! I strongly recommend Hallooou HTML5 template to everyone interested in running a successful business! Hallooou HTML5 template has really helped our business.</p>
-										<div class="client-info">
-											<img src="assets/images/dana_hill-client_pic.jpg" />
-											<h4>Dana Hill</h4>
-											<span>Lakeview Inc.</span>
-										</div>
-									</div><!-- /.item -->
-									
-								</div><!-- /.client-testimonials -->
-							</div><!-- /.col-md-8 -->
-							
-						</div><!-- /.row -->
-					</div><!-- /.container -->
-					
-				</div><!-- /.row -->
-			</div><!-- /.container -->
-		</section><!-- /#testimonials -->
-		
-		
-		
-		<!-- Counter Section -->
-		<section id="counter" class="counter-section content-section">
-			<div class="container">
-				<div class="row text-center">
-					<div class="col-md-12">
-						<h2 class="white">Stats so far</h2>
-						<h3 class="caption">The count is as follows and growing</h3>
-					</div><!-- /.col-md-12 -->
-					
-					<div class="col-sm-3 counter-wrap wow fadeInUp" data-wow-offset="10">
-						<strong><span class="timer">71</span>+</strong>
-						<span class="count-description">Acharya, Ailacharya &amp; Upadhyay</span>
-					</div><!-- /.col-sm-3 -->
-					
-					<div class="col-sm-3 counter-wrap wow fadeInUp" data-wow-offset="10">
-						<strong><span class="timer">172</span>+</strong>
-						<span class="count-description">Muni</span>
-					</div><!-- /.col-sm-3 -->
-					
-					<div class="col-sm-3 counter-wrap wow fadeInUp" data-wow-offset="10">
-						<strong><span class="timer">129</span>+</strong>
-						<span class="count-description">Aryika</span>
-					</div><!-- /.col-sm-3 -->
-					
-					<div class="col-sm-3 counter-wrap wow fadeInUp" data-wow-offset="10">
-						<strong><span class="timer">13</span>+</strong>
-						<span class="count-description">Ailak &amp; Kshullak</span>
-					</div><!-- /.col-sm-3 -->
-					
-				</div><!-- /.row -->
-			</div><!-- /.container -->
-		</section><!-- /.counter-section -->
-		
-		
-		
-		<!-- Call to action - two section -->
-		<section class="cta-two-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-9">
-						<h3>Have an idea?</h3>
-						<p>We’re here to help you manage your work</p>
-					</div>
-					<div class="col-sm-3">
-						<a href="#" class="btn btn-overcolor">Get in touch</a>
-					</div>
-				</div><!-- /.row -->
-			</div><!-- /.container -->
-		</section><!-- /.cta-two-section -->
-		
-		
-		<!-- Contact section -->
-		<?php include 'contact.php'; ?>			
-		
-		<!-- Footer -->
-		<footer>
-			<div class="container">
-				<div class="row text-center">
-					<div class="col-md-12 segment">
-						<a href="./" title="Jain Muni Locator">
-							<h2>Jain Muni Locator</h2>
-						</a>
-						<p class="white">The source of our content is our users. For online sources, <a href="references.php">click here</a>.</p>
-					</div><!-- /.col-md-12 -->
-				</div><!-- /.row -->
-				
-				<div class="row text-center">
-					<div class="col-md-12 social segment">
-						<h4>Stay Connected</h4>
-						<a href="https://www.facebook.com/jainmunilocator"><i class="fa fa-facebook fa-3x"></i></a>
-						<a href="https://twitter.com/jainmunilocator"><i class="fa fa-twitter fa-3x"></i></a>
-						<a href="https://github.com/thehitechpanky/jainmunilocator"><i class="fa fa-github fa-3x"></i></a>
-					</div><!-- /.col-md-12 -->
-				</div><!-- /.row -->
-				
-			</div><!-- /.container -->
-			
-			<div class="copynote">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12 text-center">
-							&copy; 2014-2016. Jain Muni Locator. All rights reserved.
-						</div><!-- /.col-md-12 -->
-						
-					</div><!-- /.row -->
-				</div><!-- /.container -->
-			</div><!-- /.copynote -->
-			
-			<div class="nav pull-right scroll-top">
-				<a href="#home" title="Scroll to top"><i class="fa fa-angle-up"></i></a>
-			</div>
-			
-		</footer><!-- /.footer -->
-		
-		
-		<!-- Facebook -->
-		<script type='text/javascript' src='js/facebook.js'></script>
-		
-		<!-- jQuery -->
-		<?php include 'jquery.php'; ?>
-		
-		<!-- Bootstrap Core JavaScript -->
-		<?php include 'bootstrap-script.php'; ?>
-		
-		<!-- Plugin JavaScript -->
-		<script src="assets/js/plugins/wow.min.js"></script>
-		<script src="assets/js/plugins/owl.carousel.min.js"></script>
-		<script src="assets/js/plugins/jquery.parallax-1.1.3.js"></script>
-		<script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
-		<script src="assets/js/plugins/jquery.mb.YTPlayer.min.js"></script>
-		<script src="assets/js/plugins/jquery.countTo.js"></script>
-		<script src="assets/js/plugins/jquery.inview.min.js"></script>
-		<script src="assets/js/plugins/pace.min.js"></script>
-		<script src="assets/js/plugins/jquery.easing.min.js"></script>
-		<script src="assets/js/plugins/jquery.validate.min.js"></script>
-		<script src="assets/js/plugins/additional-methods.min.js"></script> 
-		
-		<!-- Custom JavaScript -->
-		<script src="assets/js/hallooou.js"></script>
 	</body>
 	
 </html>
