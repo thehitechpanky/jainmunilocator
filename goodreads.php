cfff89f437d919cf18f746f352cfc51e83d7fb6e
Pankaj Jain <capankajjain@smilyo.com>
2016-03-30 10:47:12 +0530

    Create goodreads.php

---
+++ goodreads.php
@@ -0,0 +1,113 @@
+<!-- Portfolio/Gallery Section -->
+<section id="portfolio" class="portfolio content-section">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-md-12">
+				<h2>Good Reads</h2>
+				<h3 class="caption white">We have collected some information and created some articles for you. Worth giving a min! </h3>
+			</div><!-- /.col-md-12 -->
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+	
+	<div class="container project-container text-center">
+		<div class="recent-project-carousel owl-carousel owl-theme popup-gallery">
+			<div class="item recent-project wow fadeIn" data-wow-offset="10">
+				<img src="assets/images/gallery/project-one.jpg" alt="">
+				<div class="project-info">
+					<h3><span itemprop="keywords">Meaning of 108</span></h3>
+					<ul class="project-meta">
+						<li><a href="meaning-of-108.php">Read More</a></li>
+					</ul>
+				</div><!-- /.project-info -->
+				<div class="full-project">
+					<a href="meaning-of-108.php" title="Meaning of 108">Read more <i class="fa fa-chevron-right"></i></a>
+				</div><!-- /.full-project -->
+			</div><!-- /.item -->
+			
+			<div class="item recent-project wow fadeIn" data-wow-offset="10">
+				<img src="assets/images/gallery/project-two.jpg" alt="">
+				<div class="project-info">
+					<h3><span itemprop="keywords">28 Mahagun</span></h3>
+					<ul class="project-meta">
+						<li><a href="mahagun.php">Read more</a></li>
+					</ul>
+				</div><!-- /.project-info -->
+				<div class="full-project">
+					<a href="mahagun.php" title="28 Mahagun">Read more <i class="fa fa-chevron-right"></i></a>
+				</div><!-- /.full-project -->
+			</div><!-- /.item -->
+			
+			<div class="item recent-project wow fadeIn" data-wow-offset="10">
+				<img src="assets/images/gallery/project-three.jpg" alt="">
+				<div class="project-info">
+					<h3><span itemprop="keywords">34 Uttargun</span></h3>
+					<ul class="project-meta">
+						<li><a href="uttargun.php">Read more</a></li>
+					</ul>
+				</div><!-- /.project-info -->
+				<div class="full-project">
+					<a href="uttargun.php" title="34 Uttargun">Read more <i class="fa fa-chevron-right"></i></a>
+				</div><!-- /.full-project -->
+			</div><!-- /.item -->
+			
+			<div class="item recent-project wow fadeIn" data-wow-offset="10">
+				<img src="assets/images/gallery/project-four.jpg" alt="">
+				<div class="project-info">
+					<h3><span itemprop="keywords">11 Pratima</span></h3>
+					<ul class="project-meta">
+						<li><a href="ekadash-pratima.php">Read more</a></li>
+					</ul>
+				</div><!-- /.project-info -->
+				<div class="full-project">
+					<a href="ekadash-pratima.php" title="11 Pratima">Read more <i class="fa fa-chevron-right"></i></a>
+				</div><!-- /.full-project -->
+			</div><!-- /.item -->
+			
+			<div class="item recent-project wow fadeIn" data-wow-offset="10">
+				<img src="assets/images/gallery/project-one.jpg" alt="">
+				<div class="project-info">
+					<h3><span itemprop="keywords">Parishaha</span></h3>
+					<ul class="project-meta">
+						<li><a href="parishaha.php">Read more</a></li>
+					</ul>
+				</div><!-- /.project-info -->
+				<div class="full-project">
+					<a href="parishaha.php" title="Parishaha">Read more <i class="fa fa-chevron-right"></i></a>
+				</div><!-- /.full-project -->
+			</div><!-- /.item -->
+			
+			<div class="item recent-project wow fadeIn" data-wow-offset="10">
+				<img src="assets/images/gallery/project-two.jpg" alt="">
+				<div class="project-info">
+					<h3><span itemprop="keywords">Anekantvada</span></h3>
+					<ul class="project-meta">
+						<li><a href="anekantvada.php">Read more</a></li>
+					</ul>
+				</div><!-- /.project-info -->
+				<div class="full-project">
+					<a href="anekantvada.php" title="Anekantvada">Read more <i class="fa fa-chevron-right"></i></a>
+				</div><!-- /.full-project -->
+			</div><!-- /.item -->
+			
+			<div class="item recent-project wow fadeIn" data-wow-offset="10">
+				<img src="assets/images/gallery/project-three.jpg" alt="">
+				<div class="project-info">
+					<h3><span itemprop="keywords">Dincharya</span></h3>
+					<ul class="project-meta">
+						<li><a href="dincharya.php">Read more</a></li>
+					</ul>
+				</div><!-- /.project-info -->
+				<div class="full-project">
+					<a href="dincharya.php" title="Dincharya">Read more <i class="fa fa-chevron-right"></i></a>
+				</div><!-- /.full-project -->
+			</div><!-- /.item -->
+			
+		</div><!-- /.recent-project-carousel -->
+		
+		<div class="customNavigation project-navigation text-center">
+			<a class="btn-prev"><i class="fa fa-angle-left fa-2x"></i></a>
+			<a class="btn-next"><i class="fa fa-angle-right fa-2x"></i></a>
+		</div><!-- /.project-navigation -->
+		
+	</div><!-- /.container -->
+</section><!-- /.portfolio -->
