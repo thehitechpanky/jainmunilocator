66686046dd27257fdb92c5f89ced3a7b6e672d09
Pankaj Jain <capankajjain@smilyo.com>
2016-03-30 11:43:01 +0530

    Update meta.php

--- meta.php
+++ meta.php
@@ -2,4 +2,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="Jain Muni Locator">
+<meta name="google-signin-scope" content="profile email">
 <?php include 'login/login.php'; ?>
