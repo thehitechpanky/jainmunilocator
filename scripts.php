9a6099a421272303f850ec02b4f22f4179d474a8
Pankaj Jain <capankajjain@smilyo.com>
2016-03-30 10:51:58 +0530

    Update scripts.php

--- scripts.php
+++ scripts.php
@@ -1,6 +1,5 @@
 <!-- jQuery -->
-<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
 <!-- Latest compiled and minified JavaScript -->
