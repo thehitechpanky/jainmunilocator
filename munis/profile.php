6f79f9a5914c1d8b10b38f228e157ca904672710
Pankaj Jain <capankajjain@smilyo.com>
2016-03-31 21:04:39 +0530

    Update profile.php

--- munis/profile.php
+++ munis/profile.php
@@ -10,12 +10,15 @@ $q->execute(array($id));
 
 if ($q->rowCount() == 1) {
 	// When an id is entered in the url
-	$row = $q->fetch(PDO::FETCH_ASSOC);
-	$dos = $row['dos'];
-	$website = $row['website'];
+	$getinfo = $q->fetch(PDO::FETCH_ASSOC);
+	$dos = $getinfo['dos'];
+	$website = $getinfo['website'];
 	
 	include 'munis/getMuni.php';
+	include 'munis/getImg.php';
+	
 	$title = getmuni($id);
+	
 ?>
 
 
@@ -51,18 +54,174 @@ if ($q->rowCount() == 1) {
 						<table style="width:100%">
 							<?php if($dos=="0000-00-00") { ?>
 							<tr><th>Current Location</th><td>
-								<div itemscope itemtype="http://schema.org/Place"><a href="../map.php"><span itemprop="address"><?php echo $row['location']; ?></span></a></div>
+								<div itemscope itemtype="http://schema.org/Place"><a href="../map.php"><span itemprop="address"><?php echo $getinfo['location']; ?></span></a></div>
 								</td></tr>
 							<?php } if ($website != "#") { ?>
-							<tr><th>Website</th>
+							<tr><td>Website</td>
 								<td><a href="<?php echo $website; ?>" target="_blank" itemprop="url"><?php echo $website; ?></a></td></tr>
+							<?php } if($dos != "0000-00-00") { ?>
+							<tr><td colspan="2"></td></tr>
+							<tr><th colspan="2" align="left">Samadhi Details</th></tr>
+							<tr><td>Date</td><td><span itemprop="deathDate"><?php echo $dos; ?></span></td></tr>
+							<tr><td>Place</td><td><span itemprop="deathPlace"><?php echo $getinfo['samadhiplace']; ?></span></td></tr>
 							<?php } ?>
+							<tr><td colspan="2"></td></tr>
+							<tr><th colspan="2" align="left">Books</th></tr>
+							<?php
+	$b = $db->prepare("SELECT * FROM books WHERE bguru = '$id'");
+	$b->execute();
+	while($brow = $b->fetch(PDO::FETCH_ASSOC)) {
+		$i++;
+		echo '<tr><td colspan="2"><div class="hoverImg" itemscope itemtype="http://schema.org/Book">
+					<a href="'.$brow['blink'].'" itemprop="url">'.$i.' <span itemprop="name">'.$brow['bname'].'</span>
+					<img class="smallLight" src="'.$brow['bimg'].'" alt="'.$brow['bname'].'" itemprop="image" />
+					</a></div></td></tr>';
+	}
+							?>
+							<tr><td colspan="2"></td></tr>
+							<tr><th colspan="2" align="left">Chaturmas</th></tr>
+							<?php
+	$c = $db->prepare("SELECT * FROM chaturmas WHERE chaturmasmuni='$id' AND chaturmasplace!='N/A'");
+	$c->execute();
+	while($crow = $c->fetch(PDO::FETCH_ASSOC)) {
+		echo '<tr><td>'.$crow['chaturmasyear'].'</td><td>
+					<div itemscope itemtype="http://schema.org/Place"><span itemprop="address">
+					'.$crow['chaturmasplace'].'</span></div></td></tr>';
+	}
+	
+	if($getinfo['upadhi']=="1") {echo
+		'<tr><td colspan="2"></td></tr>
+					<tr><th colspan="2" align="left">Acharya Pad Details</th></tr>
+					<tr><td>Date</td><td><time datetime="'.$getinfo['adate'].'">'.$getinfo['adate'].'</time></td></tr>
+					<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['aguru'].'">'.getmuni($getinfo['aguru']).'
+					<img class="smallLight" src="'.getImg($getinfo['aguru']).'" alt="'.getmuni($getinfo['aguru']).'" />
+					</a></div></td></tr>
+					<tr><td>Place</td><td>'.$getinfo['aplace'].'</td></tr>'
+		;}
+	
+	if($getinfo['ailacharyaguru']>0) {echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">Elacharya Pad Details</th></tr>';
+									  if($getinfo['ailacharyaname']!="") {echo '<tr><td>Name</td><td>'.$getinfo['ailacharyaname'].'</td></tr>';}
+									  echo '<tr><td>Date</td><td>'.$getinfo['ailacharyadate'].'</td></tr>
+				<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['ailacharyaguru'].'">'.getmuni($getinfo['ailacharyaguru']).'
+				<img class="smallLight" src="'.getImg($getinfo['ailacharyaguru']).'" alt="'.getmuni($getinfo['ailacharyaguru']).'" />
+				</a></div></td></tr>
+				<tr><td>Place</td><td>'.$getinfo['ailacharyaplace'].'</td></tr>'
+										  ;}
+	
+	if($getinfo['upadhyayguru']>0) {echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">Upadhyay Pad Details</th></tr>';
+									if($getinfo['upadhyayname']!="") {echo '<tr><td>Name</td><td>'.$getinfo['upadhyayname'].'</td></tr>';}
+									echo '<tr><td>Date</td><td>'.$getinfo['upadhyaydate'].'</td></tr>
+				<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['upadhyayguru'].'">'.getmuni($getinfo['upadhyayguru']).'
+				<img class="smallLight" src="'.getImg($getinfo['upadhyayguru']).'" alt="'.getmuni($getinfo['upadhyayguru']).'" />
+				</a></div></td></tr>
+				<tr><td>Place</td><td>'.$getinfo['upadhyayplace'].'</td></tr>'
+										;}
+	
+	if($getinfo['upadhi']<5) {echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">Muni Deeksha Details</th></tr>';
+							  if($getinfo['muniname']!="") {echo '<tr><td>Name</td><td>'.$getinfo['muniname'].'</td></tr>';}
+							  echo '<tr><td>Date</td><td>'.$getinfo['munidate'].'</td></tr>
+				<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['muniguru'].'">'.getmuni($getinfo['muniguru']).'
+				<img class="smallLight" src="'.getImg($getinfo['muniguru']).'" alt="'.getmuni($getinfo['muniguru']).'" />
+				</a></div></td></tr>
+				<tr><td>Place</td><td>'.$getinfo['muniplace'].'</td></tr>'
+								  ;}
+	
+	if($getinfo['ailakguru']>0) {echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">Ailak Deeksha Details</th></tr>';
+								 if($getinfo['ailakname']!="") {echo '<tr><td>Name</td><td>'.$getinfo['ailakname'].'</td></tr>';}
+								 echo '<tr><td>Date</td><td>'.$getinfo['ailakdate'].'</td></tr>
+				<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['ailakguru'].'">'.getmuni($getinfo['ailakguru']).'
+				<img class="smallLight" src="'.getImg($getinfo['ailakguru']).'" alt="'.getmuni($getinfo['ailakguru']).'" />
+				</a></div></td></tr>
+				<tr><td>Place</td><td>'.$getinfo['ailakplace'].'</td></tr>'
+									 ;}
+	
+	if($getinfo['kguru']>0) {echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">Kshullak Deeksha Details</th></tr>';
+							 if($getinfo['ailacharyaname']!="") {echo '<tr><td>Name</td><td>'.$getinfo['kname'].'</td></tr>';}
+							 echo '<tr><td>Date</td><td>'.$getinfo['kdate'].'</td></tr>
+				<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['kguru'].'">'.getmuni($getinfo['kguru']).'
+				<img class="smallLight" src="'.getImg($getinfo['kguru']).'" alt="'.getmuni($getinfo['kguru']).'" />
+				</a></div></td></tr>
+				<tr><td>Place</td><td>'.$getinfo['kplace'].'</td></tr>'
+								 ;}
+	
+	if($getinfo['upadhi']==7) {echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">Aryika Deeksha Details</th></tr>
+				<tr><td>Date</td><td>'.$getinfo['aryikadate'].'</td></tr>
+				<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['aryikaguru'].'">'.getmuni($getinfo['aryikaguru']).'
+				<img class="smallLight" src="'.getImg($getinfo['aryikaguru']).'" alt="'.getmuni($getinfo['aryikaguru']).'" />
+				</a></div></td></tr>
+				<tr><td>Place</td><td>'.$getinfo['aryikaplace'].'</td></tr>'
+		;}
+	
+	if($getinfo['kshullikaguru']>0) {echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">Kshullika Deeksha Details</th></tr>
+				<tr><td>Date</td><td>'.$getinfo['kshullikadate'].'</td></tr>
+				<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['kshullikaguru'].'">'.getmuni($getinfo['kshullikaguru']).'
+				<img class="smallLight" src="'.getImg($getinfo['kshullikaguru']).'" alt="'.getmuni($getinfo['kshullikaguru']).'" /></a><div></td></tr>
+				<tr><td>Place</td><td>'.$getinfo['kshullikaplace'].'</td></tr>'
+		;}
+	
+	if($getinfo['bhramcharyadate']!="0000-00-00" or $getinfo['bhramcharyaguru']>0 or $getinfo['bhramcharyaplace']!="N/A") {echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">Bhramcharya (Seventh Pratima) Vrat Details</th></tr>
+				<tr><td>Date</td><td>'.$getinfo['bhramcharyadate'].'</td></tr>'
+		;}
+	
+	if($getinfo['bhramcharyaguru']>0) {echo
+		'<tr><td>Guru</td><td><div class="hoverImg"><a href ="munis.php?id='.$getinfo['bhramcharyaguru'].'">'.getmuni($getinfo['bhramcharyaguru']).'
+					<img class="smallLight" src="'.getImg($getinfo['bhramcharyaguru']).'" alt="'.getmuni($getinfo['bhramcharyaguru']).'" />
+					</a></div></td></tr>'
+		;}
+	
+	if(($getinfo['bhramcharyadate']!="0000-00-00" or $getinfo['bhramcharyaplace']!="N/A") && ($getinfo['bhramcharyaguru']==0)) {echo
+		'<tr><td>Taken By</td><td>Self</td></tr>'
+		;}
+	
+	if($getinfo['bhramcharyadate']!="0000-00-00" or $getinfo['bhramcharyaguru']>0 or $getinfo['bhramcharyaplace']!="N/A") {echo
+		'<tr><td>Place</td><td>'.$getinfo['bhramcharyaplace'].'</td></tr>'
+		;}
+	
+	if($getinfo['vairagya']!="0000-00-00" or $getinfo['grahtyag']!="0000-00-00") {echo
+		'<tr><td colspan="2"></td></tr>'
+		;}
+	
+	if($getinfo['vairagya']!="0000-00-00") {echo
+		'<tr><td>Vairagya</td><td>'.$getinfo['vairagya'].'</td></tr>'
+		;}
+	
+	if($getinfo['grahtyag']!="0000-00-00") {echo
+		'<tr><td>Grah Tyag</td><td>'.$getinfo['grahtyag'].'</td></tr>'
+		;}
+	
+	echo
+		'<tr><td colspan="2"></td></tr>
+				<tr><th colspan="2" align="left">History</th></tr>
+				<tr><td>Birthname</td><td><span itemprop="name">'.$getinfo['birthname'].'</span></td></tr>
+				<tr><td>Date of Birth</td><td><span itemprop="birthDate">'.$getinfo['dob'].'</span></td></tr>
+				<tr><td>Birth Place</td><td><span itemprop="birthPlace">'.$getinfo['birthplace'].'</span></td></tr>
+				<tr><td>Father</td><td>'.$getinfo['father'].'</td></tr>
+				<tr><td>Mother</td><td>'.$getinfo['mother'].'</td></tr>
+				<tr><td>Education</td><td>'.$getinfo['education'].'</td></tr>';
+	
+							?>
 						</table>
 					</div><!-- /.col-md-6 -->
 					
 					<div class="col-md-6">
 						<a class="btn btn-default btn-lg" href="editform.php?id=<?php echo $id; ?>">UPDATE LOCATION / DETAILS</a>
-						<img alt="Photo of <?php echo $title; ?>" width="315px" src="<?php echo $row['img']; ?>" itemprop="image" />
+						<img alt="Photo of <?php echo $title; ?>" width="315px" src="<?php echo $getinfo['img']; ?>" itemprop="image" />
 						<div class="fb-page sidebar" data-href="https://www.facebook.com/jainmunilocator" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
 							<div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/jainmunilocator">
 								<a href="https://www.facebook.com/jainmunilocator">Jain Muni Locator</a>
