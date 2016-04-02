d23c77836d2e256550bdea38630d2719142218c4
Pankaj Jain <capankajjain@smilyo.com>
2016-04-02 08:36:39 +0530

    Update edit.php

--- articles/edit.php
+++ articles/edit.php
@@ -10,15 +10,16 @@ $id = $_POST['id'];
 $title = $_POST['title'];
 $alias = $_POST['alias'];
 $content = $_POST['content'];
+$keywords = $_POST['keywords'];
 $author = $_POST['author'];
 
-$sqlarticles = "UPDATE articles SET title=?, content=?, author=? WHERE id=?";	
+$sqlarticles = "UPDATE articles SET title=?, content=?, keywords=?, author=? WHERE id=?";	
 $q = $db->prepare($sqlarticles);
-$q->execute(array($title,$content,$author,$id));
+$q->execute(array($title,$content,$keywords,$author,$id));
 
-$sqlarticlelog = "INSERT INTO articlelog (id, title, content, author) VALUES (?,?,?,?)";
+$sqlarticlelog = "INSERT INTO articlelog (id, title, alias, content, keywords, author) VALUES (?,?,?,?,?,?)";
 $q = $db->prepare($sqlarticlelog);
-$q->execute(array($id,$title,$content,$author));
+$q->execute(array($id,$title,$alias,$content,$keywords,$author));
 
 header('location: ../'.$alias.'.php');
 
