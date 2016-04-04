dd5afa0465bc00c6f60da290c76510cbd5dda741
Pankaj Jain <capankajjain@smilyo.com>
2016-04-04 18:36:54 +0530

    Update updateLogin.php

--- login/updateLogin.php
+++ login/updateLogin.php
@@ -1,4 +1,5 @@
 <?php
+
 include '../config.php';
 $email = $_GET['email'];
 $username = $_GET['username'];
@@ -11,9 +12,11 @@ $userlocality = 'N/A';
 $userip = $_SERVER['REMOTE_ADDR'];
 $u = $db->prepare("SELECT * FROM user WHERE email=?");
 $u->execute(array($email));
+
 if($u->rowCount() == 1) {
-	$q = $db->prepare("UPDATE user SET username=?, userimg=?, userlat=?, userlng=?, userlocation=?, userlocality=? userip=? WHERE email=?");
-	$q->execute(array($username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$userip,$email));
+	$lastlogin = $u->fetch(PDO::FETCH_ASSOC);
+	$q = $db->prepare("UPDATE user SET username=?, userimg=?, userlat=?, userlng=?, userlocation=?, userlocality=?, lastlogin=?, userip=? WHERE email=?");
+	$q->execute(array($username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$userip,$lastlogin,$email));
 } else {
 	$q = $db->prepare("INSERT INTO user (email,username,userimg,userlat,userlng,userlocation,userlocality,userip) VALUES (?,?,?,?,?,?,?,?)");
 	$q->execute(array($email,$username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$userip));
@@ -26,4 +29,5 @@ if (strlen($email) == 0 || $email == 'capankajjain@smilyo.com' || $email == 'rac
 	$msg = 'I just logged in to Jain Muni Locator.<br /><br />Thanks &amp; Regards<br />'.$username;
 	include '../email.php';
 }
+
 ?>
