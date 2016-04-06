71d20279247b2c3adb7233aff91b9b8811925e67
Pankaj Jain <capankajjain@smilyo.com>
2016-04-06 07:59:56 +0530

    Update events.php

--- events.php
+++ events.php
@@ -1,8 +1,8 @@
 <?php include('header.php'); ?>
 
 <!-- start body -->
-<body onunload="" >
-
+<body onunload="" <?php echo $schemaOrgBody; ?>>
+	
 	<!-- start dotted pattern -->
 	<div class="bg-overlay"></div>
 	<!-- end dotted pattern -->
@@ -14,7 +14,7 @@
 	<!-- start content wrapper -->	
 	
 	<div class="content page-content">
-	
+		
 		<div class="page-title">
 			<h1>Events</h1>
 		</div>
@@ -22,8 +22,8 @@
 		<div class="divider clear"></div>
 		
 		<div class="inner-content">	
-		
-<?php
+			
+			<?php
 
 echo "<h3>Today's Events</h3>
 <table class='sub'>
@@ -35,16 +35,16 @@ while($row = $t->fetch(PDO::FETCH_ASSOC)) {
 	$i++;
 	echo '<tr><td>'.$i.'.</td><td><a href="./munis.php?id='.$row['id'].'">'.getmuni($row['id']).'</a></td></tr>';
 }
-				
+
 echo "<tr><th colspan='2' align='left'>Aryika Deeksha Diwas</th></tr>";
 $i = 0;
 $t = $db->prepare("SELECT * FROM munishri, aryika WHERE id=aryikaid AND DAY(aryikadate)='$currentDay' AND MONTH(aryikadate)='$currentMonth' ORDER BY upadhi");
 $t->execute();
 while($row = $t->fetch(PDO::FETCH_ASSOC)) {
-$i++;
-echo '<tr><td><a href="./munis.php?id='.$row['id'].'">'.$i.': '.getmuni($row['id']).'</a></td></tr>';
+	$i++;
+	echo '<tr><td><a href="./munis.php?id='.$row['id'].'">'.$i.': '.getmuni($row['id']).'</a></td></tr>';
 }
-				
+
 echo "</table>
 <div>&nbsp;</div>
 <h3>This Month's Events</h3>
@@ -59,23 +59,41 @@ while($row = $t->fetch(PDO::FETCH_ASSOC)) {
 	$muniDate = $row['munidate'];
 	echo '<tr><td>'.$i.'</td><td>'.date('d', strtotime($muniDate)).'</td><td><a href="./munis.php?id='.$row['id'].'">'.getmuni($row['id']).'</a></td></tr>';
 }
-	
+
 echo "</table>";
 
-?>
-		
+			?>
+			
+			<br />
+			<hr>
+			
+			<!-- Facebook Comments Started -->
+			<div id="fb-root"></div>
+			<div class="fb-like" data-href="http://jainmunilocator.org/events.php" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+			<div class="fb-comments" data-href="http://jainmunilocator.org/events.php" data-numposts="5"></div>
+			<!-- Facebook Comments Ended -->
+			
+			<br /><br /><br /><br /><br /><br /><br /><br /><br />
+			
 		</div>
+		
 		<div class="sidebar">
-			<div class="today">
-				Current Date: 
-				<?php echo $today; ?>
+			
+			<div class="today">Current Date: <?php echo $today; ?></div><br />
+			
+			<div class="fb-page sidebar" data-href="https://www.facebook.com/jainmunilocator" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
+				<div class="fb-xfbml-parse-ignore">
+					<blockquote cite="https://www.facebook.com/jainmunilocator">
+						<a href="https://www.facebook.com/jainmunilocator">Jain Muni Locator</a>
+					</blockquote>
+				</div>
 			</div>
-			<div>&nbsp;</div>
-		<img alt="Acharya Kundkund | Jain Muni Locator" src="http://www.vitragvani.com/m/jeevan_parichay/pics/Aarcharya_kundkund.jpg" class="sidebar">
+			
 		</div>
 		<!-- end widgets -->
+		
 	</div>
 	
 	<!--  end content wrapper  -->
- 	
-<?php include('footer.php'); ?>
+	
+	<?php include('footer.php'); ?>
