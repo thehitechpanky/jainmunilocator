174e218cd6d675879be8e1ef06d1545147e900ae
Pankaj Jain <capankajjain@smilyo.com>
2016-04-11 17:38:56 +0530

    Create getPrefix.php

---
+++ munis/getPrefix.php
@@ -0,0 +1,15 @@
+<?php
+// configuration
+include '../config.php';
+
+$q = intval($_GET['q']);
+
+mysqli_select_db($link,"ajax_demo");
+$sql="SELECT * FROM upadhis WHERE uid = '".$q."'";
+$result = mysqli_query($link,$sql);
+
+while($row = mysqli_fetch_array($result)) {
+	echo $row['prefix'];
+}
+mysqli_close($link);
+?>
