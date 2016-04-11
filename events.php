f7c83309f39d9ebe8905668401ddab0fa0ace96a
Pankaj Jain <capankajjain@smilyo.com>
2016-04-11 17:45:23 +0530

    Update events.php

--- events.php
+++ events.php
@@ -1,30 +1,43 @@
-<?php include('header.php'); ?>
+<?php
+include 'config.php';
+include 'munis/getMuni.php';
+?>
 
-<!-- start body -->
-<body onunload="" <?php echo $schemaOrgBody; ?>>
+<!DOCTYPE html>
+<html lang="en">
 	
-	<!-- start dotted pattern -->
-	<div class="bg-overlay"></div>
-	<!-- end dotted pattern -->
-	
-	<!-- start navigation -->
-	<?php include('menu.php'); ?>
-	<!-- end navigation -->
-	
-	<!-- start content wrapper -->	
-	
-	<div class="content page-content">
+	<head>
+		<?php include 'meta.php'; ?>
+		<meta name="description" content="Jain Muni Locator is about building a global database to locate all the Digambar Jain Muni-shris in world, freely accessible to all the Jainism followers.">
+		<!-- Meta Keywords should be 8 only for optimum SEO results as suggested by http://www.seoworkers.com/tools/analyzer.html -->
+		<meta name="keywords" content="Jain Muni Locator, Jainism, Jain Sadhu, Jain Acharya, Jain Guru, Meaning of 108, Mahagun, list of all digamabar jain munis">
 		
-		<div class="page-title">
-			<h1>Events</h1>
-		</div>
+		<title>Events | Jain Muni Locator</title>
 		
-		<div class="divider clear"></div>
+		<?php include 'stylesheets.php'; ?>
 		
-		<div class="inner-content">	
-			
-			<?php
-
+	</head>
+	
+	<body id="home" itemscope itemtype="http://schema.org/WebPage">
+		
+		<!-- Navigation -->
+		<?php
+$navLinks = '<li><a href="/">Home</a></li>
+<li><a href="map.php">Map</a></li>
+<li><a href="munis.php">Digambara Monks &amp; Nuns</a></li>
+<li><a href="#about">Events</a></li>
+                                <li><a href="#contact">Contact us</a></li>';
+include 'nav.php';
+		?>
+		
+		
+		<!-- About Section -->
+		<section id="about" class="about content-section alt-bg-light wow fadeInUp" data-wow-offset="10">
+			<div class="container">
+				<div class="row">
+					<h1>Events</h1>
+					<div class="col-md-6">
+						<?php
 echo "<h3>Today's Events</h3>
 <table class='sub'>
 <tr><th colspan='2' align='left'>Muni Deeksha Diwas</th></tr>";
@@ -35,7 +48,6 @@ while($row = $t->fetch(PDO::FETCH_ASSOC)) {
 	$i++;
 	echo '<tr><td>'.$i.'.</td><td><a href="./munis.php?id='.$row['id'].'">'.getmuni($row['id']).'</a></td></tr>';
 }
-
 echo "<tr><th colspan='2' align='left'>Aryika Deeksha Diwas</th></tr>";
 $i = 0;
 $t = $db->prepare("SELECT * FROM munishri, aryika WHERE id=aryikaid AND DAY(aryikadate)='$currentDay' AND MONTH(aryikadate)='$currentMonth' ORDER BY upadhi");
@@ -44,7 +56,6 @@ while($row = $t->fetch(PDO::FETCH_ASSOC)) {
 	$i++;
 	echo '<tr><td><a href="./munis.php?id='.$row['id'].'">'.$i.': '.getmuni($row['id']).'</a></td></tr>';
 }
-
 echo "</table>
 <div>&nbsp;</div>
 <h3>This Month's Events</h3>
@@ -59,41 +70,38 @@ while($row = $t->fetch(PDO::FETCH_ASSOC)) {
 	$muniDate = $row['munidate'];
 	echo '<tr><td>'.$i.'</td><td>'.date('d', strtotime($muniDate)).'</td><td><a href="./munis.php?id='.$row['id'].'">'.getmuni($row['id']).'</a></td></tr>';
 }
-
 echo "</table>";
-
-			?>
-			
-			<br />
-			<hr>
-			
-			<!-- Facebook Comments Started -->
-			<div id="fb-root"></div>
-			<div class="fb-like" data-href="http://jainmunilocator.org/events.php" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
-			<div class="fb-comments" data-href="http://jainmunilocator.org/events.php" data-numposts="5"></div>
-			<!-- Facebook Comments Ended -->
-			
-			<br /><br /><br /><br /><br /><br /><br /><br /><br />
-			
-		</div>
+						?>			
+						
+						<hr>
+						
+						<!-- Facebook Comments Started -->
+						<div id="fb-root"></div>
+						<div class="fb-like" data-href="http://jainmunilocator.org/events.php" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+						<div class="fb-comments" data-href="http://jainmunilocator.org/events.php" data-numposts="5"></div>
+						<!-- Facebook Comments Ended -->
+						
+					</div><!-- /.col-md-6 -->
+					
+					<div class="col-md-6">
+						<div class="fb-page sidebar" data-href="https://www.facebook.com/jainmunilocator" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
+							<div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/jainmunilocator">
+								<a href="https://www.facebook.com/jainmunilocator">Jain Muni Locator</a>
+								</blockquote></div>
+						</div>
+						<?php include 'adsense.php'; ?>
+					</div><!-- /.col-md-6 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /.section -->
 		
-		<div class="sidebar">
-			
-			<div class="today">Current Date: <?php echo $today; ?></div><br />
-			
-			<div class="fb-page sidebar" data-href="https://www.facebook.com/jainmunilocator" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
-				<div class="fb-xfbml-parse-ignore">
-					<blockquote cite="https://www.facebook.com/jainmunilocator">
-						<a href="https://www.facebook.com/jainmunilocator">Jain Muni Locator</a>
-					</blockquote>
-				</div>
-			</div>
-			
-		</div>
-		<!-- end widgets -->
 		
-	</div>
+		<?php
+include 'contact.php';
+include 'footer2.php';
+include 'scripts.php';
+		?>
+		
+	</body>
 	
-	<!--  end content wrapper  -->
-	
-	<?php include('footer.php'); ?>
+</html>
