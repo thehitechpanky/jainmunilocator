6768b7f537204a01ab4761bc80304b28ddf1e1bf
Pankaj Jain <capankajjain@smilyo.com>
2016-04-12 22:59:34 +0530

    Delete jquery.php

--- jquery.php
+++
@@ -1,2 +0,0 @@
-<script type='text/javascript' src='http://code.jquery.com/jquery-1.11.3.min.js'></script>
-<script type='text/javascript' src='http://code.jquery.com/jquery-migrate-1.2.1.min.js'></script>
