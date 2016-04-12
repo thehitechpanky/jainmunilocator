5d69fc2d407c4a7e7bef31b7b60ab0612d9494ff
Pankaj Jain <capankajjain@smilyo.com>
2016-04-12 23:00:24 +0530

    Delete googleScripts.php

--- googleScripts.php
+++
@@ -1,5 +0,0 @@
-<!-- start google scripts -->
-<script type='text/javascript' src='https://www.google.com/recaptcha/api.js' async defer></script>
-<script type='text/javascript' src='https://maps.googleapis.com/maps/api/js?key=__________________'></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
-<!-- end google scripts -->
