87a9748bbcd5d763724eb0a69c51f2455ea66820
Pankaj Jain <capankajjain@smilyo.com>
2016-05-22 23:19:19 +0530

    Update updateLogin.php

--- login/updateLogin.php
+++ login/updateLogin.php
@@ -13,15 +13,25 @@ $userip = $_SERVER['REMOTE_ADDR'];
 $u = $db->prepare("SELECT * FROM user WHERE email=?");
 $u->execute(array($email));
 
-if($u->rowCount() == 1) {
-	$row = $u->fetch(PDO::FETCH_ASSOC);
-	$lastlogin = $row['usertimestamp'];
-	$q = $db->prepare("UPDATE user SET username=?, userimg=?, userlat=?, userlng=?, userlocation=?, userlocality=?, lastlogin=?, userip=? WHERE email=?");
-	$q->execute(array($username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$userip,$lastlogin,$email));
+if ($email == '') {
+	$result = 'Error: email not provided.';
 } else {
-	$q = $db->prepare("INSERT INTO user (email,username,userimg,userlat,userlng,userlocation,userlocality,userip) VALUES (?,?,?,?,?,?,?,?)");
-	$q->execute(array($email,$username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$userip));
+	if($u->rowCount() == 1) {
+		$row = $u->fetch(PDO::FETCH_ASSOC);
+		$lastlogin = $row['usertimestamp'];
+		//echo $lastlogin;
+		$q = $db->prepare("UPDATE user SET username=?, userimg=?, userlat=?, userlng=?, userlocation=?, userlocality=?, lastlogin=?, userip=? WHERE email=?");
+		$q->execute(array($username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$lastlogin,$userip,$email));
+		$result = 'Updated';
+	} else {
+		$q = $db->prepare("INSERT INTO user (email,username,userimg,userlat,userlng,userlocation,userlocality,userip) VALUES (?,?,?,?,?,?,?,?)");
+		$q->execute(array($email,$username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$userip));
+		$result = 'Inserted';
+	}
 }
+
+echo 'Email: '.$email.'<br />Username: '.$username.'<br />Userimg: '.$userimg.'<br />Result: '.$result;
+
 
 if (strlen($email) == 0 || $email == 'capankajjain@smilyo.com' || $email == 'rachna424.rj@gmail.com') {} else {
 	$to = 'capankajjain@smilyo.com';
