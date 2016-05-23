cd3cc57f7e940e46de965992a05dccf2fb1ef59d
Pankaj Jain <capankajjain@smilyo.com>
2016-05-24 00:04:46 +0530

    Update updateLocation.php

--- map/updateLocation.php
+++ map/updateLocation.php
@@ -3,32 +3,27 @@ include '../config.php';
 include 'getAddress.php';
 include 'locality.php';
 $useremail = $_GET['email'];
-$username = $_GET['username'];
-$userimg = $_GET['userimg'];
 $userlat = $_GET['userlat'];
 $userlng = $_GET['userlng'];
 $userlocation = getaddress($userlat,$userlng);
 $userlocality = getlocality($userlat,$userlng);
 
 // fileds of editlog
-$userip = $_SERVER['REMOTE_ADDR'];
-$u = $db->prepare("SELECT * FROM user WHERE email=?");
+$u = $db->prepare("SELECT * FROM userlocation WHERE email=?");
 $u->execute(array($useremail));
 if($u->rowCount() == 1) {
-	$q = $db->prepare("UPDATE user SET username=?, userimg=?, userlat=?, userlng=?, userlocation=?, userlocality=?, userip=? WHERE email=?");
-	$q->execute(array($username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$userip,$useremail));
+	$q = $db->prepare("UPDATE userlocation SET userlat=?, userlng=?, userlocation=?, userlocality=? WHERE email=?");
+	$q->execute(array($userlat,$userlng,$userlocation,$userlocality,$useremail));
 } else {
-	$q = $db->prepare("INSERT INTO user (email,username,userimg,userlat,userlng,userlocation,userlocality,userip) VALUES (?,?,?,?,?,?,?,?)");
-	$q->execute(array($useremail,$username,$userimg,$userlat,$userlng,$userlocation,$userlocality,$userip));
+	$q = $db->prepare("INSERT INTO userlocation (email,userlat,userlng,userlocation,userlocality) VALUES (?,?,?,?,?)");
+	$q->execute(array($useremail,$userlat,$userlng,$userlocation,$userlocality));
 }
-
-echo $username;
 
 if (strlen($useremail) == 0 || $useremail = 'capankajjain@smilyo.com' || $useremail == 'rachna424.rj@gmail.com') {} else {
 	$to = 'capankajjain@smilyo.com';
 	$from = $useremail;
 	$subject = 'New Login to Jain Muni Locator with location';
-	$msg = 'I just logged in to Jain Muni Locator from '.$userlocation.'.<br /><br />Thanks &amp; Regards<br />'.$username;
+	$msg = 'I just logged in to Jain Muni Locator from '.$userlocation.'.<br /><br />Thanks &amp; Regards<br />';
 	require("../sendgrid-php/sendgrid-php.php");
 	include '../sendgridkey.php';
 	$email = new SendGrid\Email();
