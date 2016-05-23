6d0fa8432a0c897a56212afc32b765bdda1ba1a3
Pankaj Jain <capankajjain@smilyo.com>
2016-05-24 01:25:13 +0530

    Create muniapi.php

---
+++ munis/muniapi.php
@@ -0,0 +1,22 @@
+<?php
+
+include '../config.php';
+
+$id = $_GET['id'];
+
+$q = $db->prepare('SELECT * FROM munishri, upadhis, acharya, ailacharya, upadhyay, muni, ailak, kshullak, aryika, kshullika, muni_location, contact
+WHERE id=? AND acharyaid=? AND ailacharyaid=? AND upadhyayid=? AND muniid=? AND ailakid=? AND kid=?
+AND aryikaid=? AND kshullikaid=? AND bhramcharyaid=? AND mid=? AND contactid=? AND uid=upadhi');
+$q->execute(array($id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id));
+
+$muni = array();
+$info["muni"] = array();
+
+if ($q->rowCount() == 1) {
+	$row = $q->fetch(PDO::FETCH_ASSOC);
+	$result[]=$row;
+}
+
+print(json_encode($result));
+
+?>
