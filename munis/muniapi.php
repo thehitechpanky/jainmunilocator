7d9f34c16135efed9e2fde7a1a74638ea2d9eba5
Pankaj Jain <capankajjain@smilyo.com>
2016-05-29 02:58:48 +0530

    Update muniapi.php

--- munis/muniapi.php
+++ munis/muniapi.php
@@ -1,16 +1,15 @@
 <?php
 
+header('Content-Type: application/json');
+
 include '../config.php';
 
 $id = $_GET['id'];
 
-$q = $db->prepare('SELECT * FROM munishri, upadhis, acharya, ailacharya, upadhyay, muni, ailak, kshullak, aryika, kshullika, muni_location, contact
+$q = $db->prepare('SELECT * FROM munishri, upadhis, acharya, ailacharya, upadhyay, muni, ailak, kshullak, aryika, kshullika, bhramcharya, muni_location, contact
 WHERE id=? AND acharyaid=? AND ailacharyaid=? AND upadhyayid=? AND muniid=? AND ailakid=? AND kid=?
 AND aryikaid=? AND kshullikaid=? AND bhramcharyaid=? AND mid=? AND contactid=? AND uid=upadhi');
 $q->execute(array($id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id));
-
-$muni = array();
-$info["muni"] = array();
 
 if ($q->rowCount() == 1) {
 	$row = $q->fetch(PDO::FETCH_ASSOC);
