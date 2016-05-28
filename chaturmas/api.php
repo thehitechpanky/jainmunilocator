cd2fcbfec5a67ff4b02bd70fe0378a1800b083a6
Pankaj Jain <capankajjain@smilyo.com>
2016-05-29 02:58:05 +0530

    Update api.php

--- chaturmas/api.php
+++ chaturmas/api.php
@@ -1,11 +1,15 @@
 <?php
 
+header('Content-Type: application/json');
+
 include '../config.php';
 
 $id = $_GET['id'];
 
 $q = $db->prepare('SELECT * FROM chaturmas WHERE chaturmasmuni=? ORDER BY chaturmasyear DESC');
 $q->execute(array($id));
+
+$result = '';
 
 while ($row = $q->fetch(PDO::FETCH_ASSOC)) { $result[]=$row; }
 
