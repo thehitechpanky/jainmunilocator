8511e9eb6d3fc4551a08eac69e03b3eccacd64c2
Pankaj Jain <capankajjain@smilyo.com>
2016-05-29 03:01:00 +0530

    Update muniapi.php

--- munis/muniapi.php
+++ munis/muniapi.php
@@ -6,10 +6,10 @@ include '../config.php';
 
 $id = $_GET['id'];
 
-$q = $db->prepare('SELECT * FROM munishri, upadhis, acharya, ailacharya, upadhyay, muni, ailak, kshullak, aryika, kshullika, bhramcharya, muni_location, contact
-WHERE id=? AND acharyaid=? AND ailacharyaid=? AND upadhyayid=? AND muniid=? AND ailakid=? AND kid=?
-AND aryikaid=? AND kshullikaid=? AND bhramcharyaid=? AND mid=? AND contactid=? AND uid=upadhi');
-$q->execute(array($id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id));
+$q = $db->prepare('SELECT * FROM munishri, upadhis, acharya, ailacharya, upadhyay, muni, ailak, kshullak, ganini, aryika, kshullika, bhramcharya, muni_location,
+contact WHERE id=? AND acharyaid=? AND ailacharyaid=? AND upadhyayid=? AND muniid=? AND ailakid=? AND kid=? AND ganiniid=? AND aryikaid=?
+AND kshullikaid=? AND bhramcharyaid=? AND mid=? AND contactid=? AND uid=upadhi');
+$q->execute(array($id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id,$id));
 
 if ($q->rowCount() == 1) {
 	$row = $q->fetch(PDO::FETCH_ASSOC);
