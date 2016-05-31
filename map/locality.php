b9e370e251f19ca29f57679f2ec385daf74d03c6
Pankaj Jain <capankajjain@smilyo.com>
2016-06-01 02:13:30 +0530

    Update locality.php

--- map/locality.php
+++ map/locality.php
@@ -1,18 +1,19 @@
 <?php
 
 function getlocality($lat,$lng) {
-	
-	$geocode=file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?latlng='.$lat.','.$lng.'&sensor=false');
-	
-	$output= json_decode($geocode);
-	
-	for($j=0;$j<count($output->results[0]->address_components);$j++){
-		$cn=array($output->results[0]->address_components[$j]->types[0]);
-		if(in_array("locality", $cn))
-		{
-			$locality= $output->results[0]->address_components[$j]->long_name;
-			return $locality;
+	global $serverkey;
+	$geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$lat.','.$lng.'&sensor=false&key='.$serverkey);
+	$output = json_decode($geocode);
+	//var_dump($output); //This will explain the error
+	if ($lat == 0 || $lng == 0) { return 'N/A'; } else {
+		for($j=0; $j<count($output->results[0]->address_components); $j++) {
+			$cn = array($output->results[0]->address_components[$j]->types[0]);
+			if (in_array("locality", $cn)) {
+				$locality= $output->results[0]->address_components[$j]->long_name;
+				return $locality;
+			}
 		}
 	}
 }
+
 ?>
