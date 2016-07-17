c5003fc218df85b98be64828b90fac8a60e3cf74
Pankaj Jain <capankajjain@smilyo.com>
2016-07-17 18:08:19 +0530

    Update yearly.php

--- chaturmas/yearly.php
+++ chaturmas/yearly.php
@@ -2,9 +2,6 @@
 $year = (int)$_GET['year'];
 include 'config.php';
 include 'munis/getMuni.php';
-
-$q = $db->prepare("SELECT * FROM chaturmas WHERE chaturmasyear=? AND NOT (chaturmasplace='N/A') ORDER BY chaturmasplace");
-$q->execute(array($year));
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +11,7 @@ $q->execute(array($year));
 		<?php include 'meta.php'; ?>
 		<meta name="description" content="Jain Muni Locator is about building a global database to locate all the Digambar Jain Muni-shris in world, freely accessible to all the Jainism followers.">
 		<!-- Meta Keywords should be 8 only for optimum SEO results as suggested by http://www.seoworkers.com/tools/analyzer.html -->
-		<meta name="keywords" content="Jain Muni Locator, Jainism, Jain Sadhu, Jain Acharya, Jain Guru, Meaning of 108, Mahagun, list of all digamabar jain munis">
+		<meta name="keywords" content="Jain Muni Locator, Jainism, Jain Sadhu, Jain Acharya, Jain Guru, Chaturmas, Varshayog, list of all digamabar jain munis">
 		
 		<title>Chaturmas <?php echo $year; ?> | Jain Muni Locator</title>
 		
@@ -40,25 +37,44 @@ include 'nav.php';
 		<section id="about" class="about content-section alt-bg-light wow fadeInUp" data-wow-offset="10">
 			<div class="container">
 				<div class="row">
-					<h1>Chaturmas</h1>
+					<h1 itemprop="name">Chaturmas / Varshayog <?php echo $year; ?></h1>
 					<div class="col-md-6">
-						<h2><?php echo $year; ?></h2>
 						<?php
-$c = "N/A";
-while($row = $q->fetch(PDO::FETCH_ASSOC)) {
-	$d = $row['chaturmasplace'];
-	if ($c == $d) {
-		//This is intentionally left blank because only unique records are to fetched
-	} else {
-		$c = $d;
+$q = $db->prepare("SELECT * FROM chaturmas WHERE chaturmasyear=? AND NOT (chaturmasstate=?) ORDER BY chaturmasstate");
+$q->execute(array($year,'N/A'));
+$i = 1;
+$oldstate='N/A';
+while($row=$q->fetch(PDO::FETCH_ASSOC)){
+	$state=$row['chaturmasstate'];
+	if($state==$oldstate){}else{
+		echo '<br /><h3 itemscope itemtype="http://schema.org/State">'.$i.'. <span itemprop="name">'.$state.'</span></h3>';
+		$oldstate=$state;//change oldstate to next
 		$i++;
-		echo '<br /><h3>'.$i.'. '.$c.'</h3>';
-		$q2 = $db->prepare("SELECT * FROM munishri, chaturmas WHERE approved=1 AND id=chaturmasmuni AND chaturmasplace='$c' AND chaturmasyear=? ORDER BY upadhi, name");
-		$q2->execute(array($year));
+		//display location
+		$q2 = $db->prepare("SELECT * FROM chaturmas WHERE chaturmasyear=? AND chaturmasstate=? AND NOT (chaturmasplace=?) AND NOT (chaturmasplace=?) ORDER BY chaturmasplace");
+		$q2->execute(array($year,$state,'','N/A'));
 		$j = 0;
+		$c = "N/A";
 		while($row2 = $q2->fetch(PDO::FETCH_ASSOC)) {
-			$j++;
-			echo '<div class="hoverImg"><a href="./munis.php?id='.$row2["id"].'">'.$j.'. '.getmuni($row2["id"]).'<span><img class="smallLight" src="'.$row2["img"].'" alt="'.getmuni($row2["id"]).'" /></span></a></div>';
+			$d = $row2['chaturmasplace'];
+			if ($c == $d) {
+				//This is intentionally left blank because only unique records are to fetched
+			} else {
+				$c = $d;
+				$j++;
+				echo '<div itemscope itemtype="http://schema.org/Place">&nbsp;&nbsp;&nbsp;&nbsp;<strong>'.$j.'. <span itemprop="address">'.$c.'</span></strong></div>';
+				// display monks & nuns
+				$q3 = $db->prepare("SELECT * FROM munishri, chaturmas WHERE approved=1 AND id=chaturmasmuni AND chaturmasplace=? AND chaturmasyear=? ORDER BY upadhi, name");
+				$q3->execute(array($c,$year));
+				$k = 0;
+				while($row3 = $q3->fetch(PDO::FETCH_ASSOC)) {
+					$k++;
+					echo '<div class="hoverImg" itemscope itemtype="http://schema.org/Person">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="./munis.php?id='.$row3["id"].'">
+					'.$k.'. <span itemprop="name">'.getmuni($row3["id"]).'</span><span><img class="smallLight" src="'.$row3["img"].'" alt="'.getmuni($row3["id"]).'" /></span>
+					</a></div>';
+				}
+			}
 		}
 	}
 }
@@ -75,10 +91,10 @@ while($row = $q->fetch(PDO::FETCH_ASSOC)) {
 					</div><!-- /.col-md-6 -->
 					
 					<div class="col-md-6">
-						<div class="fb-page sidebar" data-href="https://www.facebook.com/jainmunilocator" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
-							<div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/jainmunilocator">
+						<div class="fb-page sidebar" data-href="https://www.facebook.com/jainmunilocator" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+							<blockquote cite="https://www.facebook.com/jainmunilocator" class="fb-xfbml-parse-ignore">
 								<a href="https://www.facebook.com/jainmunilocator">Jain Muni Locator</a>
-								</blockquote></div>
+							</blockquote>
 						</div>
 						<?php include 'adsense.php'; ?>
 					</div><!-- /.col-md-6 -->
